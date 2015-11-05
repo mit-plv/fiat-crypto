78382dea5eec746c5359e103462d9974e5bc64c7
Jade Philipoom <jadep@mit.edu>
2015-11-05 13:48:27 -0500

    ModularBaseSystem: Implemented reduce and proved reduce_rep, currently working on admits (extended_shiftadd and reduce_defn).

--- src/Galois/ModularBaseSystem.v
+++ src/Galois/ModularBaseSystem.v
@@ -103,8 +103,77 @@ Module GFPseudoMersenneBase (BC:BaseCoefs) (M:Modulus) (P:PseudoMersenneBasePara
       f_equal; rewrite decode_short; auto.
   Qed.
 
+  (* Converts from length of E.base to length of B.base by reduction modulo M.*)
+  Fixpoint reduce (us : E.digits) : B.digits :=
+    let high := skipn (length BC.base) us in
+    let low := firstn (length BC.base) us in
+    let wrap := map (Z.mul P.c) high in
+    B.add low wrap.
+
+  (* TODO: move this somewhere more appropriate/improve strategy? *)
+  Lemma Prime_nonzero: forall (p : Prime), primeToZ p <> 0.
+  Admitted.
+
+  Lemma mod_mult_plus: forall a b c, (b <> 0) -> (a * b + c) mod b = c mod b.
+  Proof.
+    intros.
+    rewrite Zplus_mod.
+    rewrite Z.mod_mul; auto; simpl.
+    rewrite Zmod_mod; auto.
+  Qed.
+
   (* a = r + s(2^k) = r + s(2^k - c + c) = r + s(2^k - c) + cs = r + cs *) 
-  (* Fixpoint reduce (us : unreduced_digits) : digits := *)
-      
+  Lemma pseudomersenne_add: forall x y, (x + ((2^P.k) * y)) mod modulus = (x + (P.c * y)) mod modulus.
+  Proof.
+    intros.
+    replace (2^P.k) with (((2^P.k) - P.c) + P.c) by auto.
+    rewrite Z.mul_add_distr_r.
+    rewrite Zplus_mod.
+    rewrite <- P.modulus_pseudomersenne.
+    rewrite Z.mul_comm.
+    rewrite mod_mult_plus; try apply Prime_nonzero.
+    rewrite <- Zplus_mod; auto.
+  Qed.
+
+  Lemma extended_shiftadd: forall (us : E.digits), E.decode us =
+      B.decode (firstn (length BC.base) us) +
+      (2^P.k * B.decode (skipn (length BC.base) us)).
+  Proof.
+    intros.
+    rewrite decode_short; try (rewrite firstn_length; rewrite Min.le_min_l; auto).
+  Admitted.
+
+  (* TODO: definition of reduce, but isn't unfolding and solving automatically. *)
+  Lemma reduce_defn : forall (us : E.digits), reduce us =
+      B.add (firstn (length BC.base) us) (map (Z.mul P.c) (skipn (length BC.base) us)).
+  Proof.
+  Admitted.
+
+  (* TODO: add in preconditions for B *)
+  Lemma base_bounds : forall us, 0 <= B.decode us < modulus.
+  Admitted.
+
+  Lemma decode_mod : forall us, B.decode us = B.decode us mod modulus.
+  Proof.
+      intros; rewrite Zmod_small; auto.
+      apply base_bounds.
+  Qed.
+
+  Lemma reduce_rep : forall us, B.decode (reduce us) = (E.decode us) mod modulus.
+  Proof.
+    intros.
+    rewrite decode_mod.
+    rewrite extended_shiftadd.
+    rewrite pseudomersenne_add.
+    remember (firstn (length BC.base) us) as low.
+    remember (skipn (length BC.base) us) as high.
+    replace (reduce us) with (B.add low (map (Z.mul P.c) high)) by (rewrite reduce_defn; rewrite Heqlow; rewrite Heqhigh; auto).
+    unfold B.decode.
+    rewrite B.add_rep.
+    replace (map (Z.mul P.c) high) with (B.mul_each P.c high) by auto.
+    rewrite B.mul_each_rep; auto.
+  Qed.
+
+Print Assumptions reduce_rep.
 
 End GFPseudoMersenneBase.
