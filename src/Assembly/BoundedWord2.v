4341e7432963d57ec0f42f8ccf5cc88da2aacb5c
Andres Erbsen <andreser@mit.edu>
2016-02-17 18:10:32 -0500

    BoundedWord2: add test cases

--- src/Assembly/BoundedWord2.v
+++ src/Assembly/BoundedWord2.v
@@ -97,6 +97,7 @@ Section BoundedWord.
     unfold wmultN, wordBinN.
     rewrite wordToNat_natToWord_idempotent.
     ge_to_le_nat.
+
     apply Mult.mult_le_compat; nomega.
     pre_nomega.
     apply Lt.le_lt_trans with (N.to_nat b1 * N.to_nat b2); auto.
@@ -153,4 +154,124 @@ Section BoundedWord.
                     (H1 : w1 <= _) (H2 : w2 <= _) (H3 : w3 <= _) (H4 : w4 <= _) =>
                   projT1 (example3 H1 H2 H3 H4).
 
+
+  Section MulmodExamples.
+
+    Lemma example_and : forall x : word 32, wand x (NToWord 32 (N.ones 10)) <= 1023.
+    Abort.
+    
+    Variable shiftrl : forall {l}, word l -> nat -> word l. (* "logical" aka "unsigned" bitshift *)
+    Hypothesis shiftrl_spec : forall {l} (w : word l) (n:nat), wordToN (shiftrl w n) = N.shiftr (wordToN w) (N.of_nat n).
+
+    Lemma example_shiftrl : forall x : word 32, shiftrl x 30 <= 3.
+    Abort.
+
+    Lemma example_shiftrl2 : forall x : word 32, x <= 1023 -> shiftrl x 5 <= 31.
+    Abort.
+    
+    Variable f0 f1 f2 f3 f4 f5 f6 f7 f8 f9 : word 32.
+    Variable g0 g1 g2 g3 g4 g5 g6 g7 g8 g9 : word 32.
+    Hypothesis Hf0 : f0 <= 2^26.
+    Hypothesis Hf1 : f1 <= 2^25.
+    Hypothesis Hf2 : f2 <= 2^26.
+    Hypothesis Hf3 : f3 <= 2^25.
+    Hypothesis Hf4 : f4 <= 2^26.
+    Hypothesis Hf5 : f5 <= 2^25.
+    Hypothesis Hf6 : f6 <= 2^26.
+    Hypothesis Hf7 : f7 <= 2^25.
+    Hypothesis Hf8 : f8 <= 2^26.
+    Hypothesis Hf9 : f9 <= 2^25.
+    Hypothesis Hg0 : g0 <= 2^26.
+    Hypothesis Hg1 : g1 <= 2^25.
+    Hypothesis Hg2 : g2 <= 2^26.
+    Hypothesis Hg3 : g3 <= 2^25.
+    Hypothesis Hg4 : g4 <= 2^26.
+    Hypothesis Hg5 : g5 <= 2^25.
+    Hypothesis Hg6 : g6 <= 2^26.
+    Hypothesis Hg7 : g7 <= 2^25.
+    Hypothesis Hg8 : g8 <= 2^26.
+    Hypothesis Hg9 : g9 <= 2^25.
+    
+    Lemma example_mulmod_s_ppt : { b | f0 ^* g0  <= b}.
+    Abort.
+
+    Lemma example_mulmod_s_pp :  { b | f0 ^* g0 ^+  $19 ^* (f9 ^* g1 ^* $2 ^+ f8 ^* g2 ^+ f7 ^* g3 ^* $2 ^+ f6 ^* g4 ^+ f5 ^* g5 ^* $2 ^+ f4 ^* g6 ^+ f3 ^* g7 ^* $2 ^+ f2 ^* g8 ^+  f1 ^* g9 ^* $2) <= b}.
+    Abort.
+
+    Lemma example_mulmod_s_pp_shiftrl :
+        { b | shiftrl (f0 ^* g0 ^+  $19 ^* (f9 ^* g1 ^* $2 ^+ f8 ^* g2 ^+ f7 ^* g3 ^* $2 ^+ f6 ^* g4 ^+ f5 ^* g5 ^* $2 ^+ f4 ^* g6 ^+ f3 ^* g7 ^* $2 ^+ f2 ^* g8 ^+  f1 ^* g9 ^* $2)) 26 <= b}.
+    Abort.
+
+    Lemma example_mulmod_u_fg1 :  { b |
+          (let y : word 32 := (* the type declarations on the let-s make type inference not take forever *)
+             (f0 ^* g0 ^+
+              $19 ^*
+              (f9 ^* g1 ^* $2 ^+ f8 ^* g2 ^+ f7 ^* g3 ^* $2 ^+ f6 ^* g4 ^+ f5 ^* g5 ^* $2 ^+ f4 ^* g6 ^+ f3 ^* g7 ^* $2 ^+ f2 ^* g8 ^+
+               f1 ^* g9 ^* $2)) in
+           let y0 : word 32 :=
+             (shiftrl y 26 ^+
+              (f1 ^* g0 ^+ f0 ^* g1 ^+
+               $19 ^* (f9 ^* g2 ^+ f8 ^* g3 ^+ f7 ^* g4 ^+ f6 ^* g5 ^+ f5 ^* g6 ^+ f4 ^* g7 ^+ f3 ^* g8 ^+ f2 ^* g9))) in
+           let y1 : word 32 :=
+             (shiftrl y0 25 ^+
+              (f2 ^* g0 ^+ f1 ^* g1 ^* $2 ^+ f0 ^* g2 ^+
+               $19 ^* (f9 ^* g3 ^* $2 ^+ f8 ^* g4 ^+ f7 ^* g5 ^* $2 ^+ f6 ^* g6 ^+ f5 ^* g7 ^* $2 ^+ f4 ^* g8 ^+ f3 ^* g9 ^* $2))) in
+           let y2 : word 32 :=
+             (shiftrl y1 26 ^+
+              (f3 ^* g0 ^+ f2 ^* g1 ^+ f1 ^* g2 ^+ f0 ^* g3 ^+
+               $19 ^* (f9 ^* g4 ^+ f8 ^* g5 ^+ f7 ^* g6 ^+ f6 ^* g7 ^+ f5 ^* g8 ^+ f4 ^* g9))) in
+           let y3 : word 32 :=
+             (shiftrl y2 25 ^+
+              (f4 ^* g0 ^+ f3 ^* g1 ^* $2 ^+ f2 ^* g2 ^+ f1 ^* g3 ^* $2 ^+ f0 ^* g4 ^+
+               $19 ^* (f9 ^* g5 ^* $2 ^+ f8 ^* g6 ^+ f7 ^* g7 ^* $2 ^+ f6 ^* g8 ^+ f5 ^* g9 ^* $2))) in
+           let y4 : word 32 :=
+             (shiftrl y3 26 ^+
+              (f5 ^* g0 ^+ f4 ^* g1 ^+ f3 ^* g2 ^+ f2 ^* g3 ^+ f1 ^* g4 ^+ f0 ^* g5 ^+
+               $19 ^* (f9 ^* g6 ^+ f8 ^* g7 ^+ f7 ^* g8 ^+ f6 ^* g9))) in
+           let y5 : word 32 :=
+             (shiftrl y4 25 ^+
+              (f6 ^* g0 ^+ f5 ^* g1 ^* $2 ^+ f4 ^* g2 ^+ f3 ^* g3 ^* $2 ^+ f2 ^* g4 ^+ f1 ^* g5 ^* $2 ^+ f0 ^* g6 ^+
+               $19 ^* (f9 ^* g7 ^* $2 ^+ f8 ^* g8 ^+ f7 ^* g9 ^* $2))) in
+           let y6 : word 32 :=
+             (shiftrl y5 26 ^+
+              (f7 ^* g0 ^+ f6 ^* g1 ^+ f5 ^* g2 ^+ f4 ^* g3 ^+ f3 ^* g4 ^+ f2 ^* g5 ^+ f1 ^* g6 ^+ f0 ^* g7 ^+
+               $19 ^* (f9 ^* g8 ^+ f8 ^* g9))) in
+           let y7 : word 32 :=
+             (shiftrl y6 25 ^+
+              (f8 ^* g0 ^+ f7 ^* g1 ^* $2 ^+ f6 ^* g2 ^+ f5 ^* g3 ^* $2 ^+ f4 ^* g4 ^+ f3 ^* g5 ^* $2 ^+ f2 ^* g6 ^+ f1 ^* g7 ^* $2 ^+
+               f0 ^* g8 ^+ $19 ^* f9 ^* g9 ^* $2)) in
+           let y8 : word 32 :=
+             (shiftrl y7 26 ^+
+              (f9 ^* g0 ^+ f8 ^* g1 ^+ f7 ^* g2 ^+ f6 ^* g3 ^+ f5 ^* g4 ^+ f4 ^* g5 ^+ f3 ^* g6 ^+ f2 ^* g7 ^+ f1 ^* g8 ^+
+               f0 ^* g9)) in
+           let y9 : word 32 :=
+             ($19 ^* shiftrl y8 25 ^+
+              wand
+                (f0 ^* g0 ^+
+                 $19 ^*
+                 (f9 ^* g1 ^* $2 ^+ f8 ^* g2 ^+ f7 ^* g3 ^* $2 ^+ f6 ^* g4 ^+ f5 ^* g5 ^* $2 ^+ f4 ^* g6 ^+ f3 ^* g7 ^* $2 ^+
+                  f2 ^* g8 ^+ f1 ^* g9 ^* $2)) (@NToWord 32 (N.ones 26%N))) in
+           let fg1 : word 32 := (shiftrl y9 26 ^+
+            wand
+              (shiftrl y 26 ^+
+               (f1 ^* g0 ^+ f0 ^* g1 ^+
+                $19 ^* (f9 ^* g2 ^+ f8 ^* g3 ^+ f7 ^* g4 ^+ f6 ^* g5 ^+ f5 ^* g6 ^+ f4 ^* g7 ^+ f3 ^* g8 ^+ f2 ^* g9)))
+              (@NToWord 32 (N.ones 26%N))) in
+           fg1) <= b }.
+    Abort.
+
+    Require Import ZArith.
+    Variable shiftra : forall {l}, word l -> nat -> word l. (* "arithmetic" aka "signed" bitshift *)
+    Hypothesis shiftra_spec : forall {l} (w : word l) (n:nat), wordToZ (shiftra w n) = Z.shiftr (wordToZ w) (Z.of_nat n).
+
+    Lemma example_shiftra : forall x : word 4, shiftra x 2 <= 15.
+    Abort.
+
+    Lemma example_shiftra : forall x : word 4, x <= 7 -> shiftra x 2 <= 1.
+    Abort.
+
+    Lemma example_mulmod_s_pp_shiftra :
+        { b | shiftra (f0 ^* g0 ^+  $19 ^* (f9 ^* g1 ^* $2 ^+ f8 ^* g2 ^+ f7 ^* g3 ^* $2 ^+ f6 ^* g4 ^+ f5 ^* g5 ^* $2 ^+ f4 ^* g6 ^+ f3 ^* g7 ^* $2 ^+ f2 ^* g8 ^+  f1 ^* g9 ^* $2)) 26 <= b}.
+    Abort.
+  End MulmodExamples.
 End BoundedWord.
