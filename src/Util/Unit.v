20bc8ab53a8a2777e54dea261b906106e7daf373
Jason Gross <jagro@google.com>
2016-06-23 16:14:43 -0700

    Add Unit.v

---
+++ src/Util/Unit.v
@@ -0,0 +1,8 @@
+Require Import Coq.Classes.Morphisms.
+Require Import Relation_Definitions.
+
+(* an equivalence for a relation on trivial things, like [unit] *)
+Global Instance Equivalence_trivial {A} : Equivalence (fun _ _ : A => True).
+Proof.
+  repeat constructor.
+Qed.
