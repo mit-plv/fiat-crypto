cb75fd18fba514577fb465d84e31ae9c6787bcd1
Jason Gross <jagro@google.com>
2016-07-01 09:13:52 -0700

    Fix a typo in Zsplit_sums

--- src/Util/ZUtil.v
+++ src/Util/ZUtil.v
@@ -546,7 +546,7 @@ Ltac Zsplit_sums_step :=
     => etransitivity; [ eapply Z.sub_le_mono | ]
   end.
 Ltac Zsplit_sums :=
-  try (Zsplit_sums_step; [ Zsplit_sums | ]).
+  try (Zsplit_sums_step; [ Zsplit_sums.. | ]).
 Ltac Zpre_reorder_fractions_step :=
   match goal with
   | [ |- context[?x / ?y * ?z] ]
