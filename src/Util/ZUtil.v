277cc4679b571c1a7398763a7a4ab9db559bddf6
Jason Gross <jagro@google.com>
2016-08-11 16:35:41 -0700

    Add ZUtil
    After    | File Name                                       | Before   || Change
    ----------------------------------------------------------------------------------
    1m51.60s | Total                                           | 1m50.04s || +0m01.55s
    ----------------------------------------------------------------------------------
    0m03.36s | Experiments/SpecificCurve25519                  | 0m04.57s || -0m01.21s
    0m16.75s | ModularArithmetic/ModularBaseSystemProofs       | 0m16.54s || +0m00.21s
    0m14.52s | Specific/GF25519                                | 0m14.32s || +0m00.19s
    0m13.83s | Experiments/SpecEd25519                         | 0m14.05s || -0m00.22s
    0m08.98s | ModularArithmetic/Pow2BaseProofs                | 0m08.49s || +0m00.49s
    0m06.89s | ModularArithmetic/Montgomery/ZProofs            | 0m06.45s || +0m00.43s
    0m04.32s | Testbit                                         | 0m04.92s || -0m00.59s
    0m03.84s | BaseSystemProofs                                | 0m03.84s || +0m00.00s
    0m03.66s | ModularArithmetic/Tutorial                      | 0m03.57s || +0m00.09s
    0m03.31s | Util/ZUtil                                      | 0m03.25s || +0m00.06s
    0m02.60s | ModularArithmetic/BarrettReduction/ZHandbook    | 0m02.59s || +0m00.01s
    0m02.26s | ModularArithmetic/BarrettReduction/ZGeneralized | 0m02.29s || -0m00.03s
    0m02.15s | Specific/GF1305                                 | 0m02.10s || +0m00.04s
    0m02.15s | ModularArithmetic/ModularArithmeticTheorems     | 0m01.96s || +0m00.18s
    0m02.05s | ModularArithmetic/ModularBaseSystemOpt          | 0m02.13s || -0m00.08s
    0m01.79s | ModularArithmetic/BarrettReduction/ZBounded     | 0m01.67s || +0m00.12s
    0m01.59s | Encoding/PointEncodingPre                       | 0m01.49s || +0m00.10s
    0m01.34s | ModularArithmetic/BarrettReduction/Z            | 0m01.18s || +0m00.16s
    0m01.21s | BaseSystem                                      | 0m01.24s || -0m00.03s
    0m01.20s | ModularArithmetic/ExtendedBaseVector            | 0m01.13s || +0m00.07s
    0m01.11s | ModularArithmetic/PrimeFieldTheorems            | 0m01.05s || +0m00.06s
    0m01.09s | Util/NumTheoryUtil                              | 0m00.85s || +0m00.24s
    0m01.01s | Experiments/DerivationsOptionRectLetInEncoding  | 0m00.91s || +0m00.09s
    0m00.92s | Encoding/ModularWordEncodingPre                 | 0m00.63s || +0m00.29s
    0m00.88s | ModularArithmetic/ModularBaseSystemField        | 0m00.95s || -0m00.06s
    0m00.87s | ModularArithmetic/ModularBaseSystemList         | 0m00.62s || +0m00.25s
    0m00.86s | ModularArithmetic/Montgomery/ZBounded           | 0m00.82s || +0m00.04s
    0m00.85s | ModularArithmetic/ModularBaseSystemListProofs   | 0m00.86s || -0m00.01s
    0m00.69s | ModularArithmetic/ZBounded                      | 0m00.43s || +0m00.25s
    0m00.67s | Encoding/ModularWordEncodingTheorems            | 0m00.65s || +0m00.02s
    0m00.66s | Spec/ModularWordEncoding                        | 0m00.57s || +0m00.09s
    0m00.63s | ModularArithmetic/ExtPow2BaseMulProofs          | 0m00.63s || +0m00.00s
    0m00.62s | ModularArithmetic/ModularBaseSystem             | 0m00.55s || +0m00.06s
    0m00.60s | ModularArithmetic/Pre                           | 0m00.49s || +0m00.10s
    0m00.59s | ModularArithmetic/PseudoMersenneBaseParamProofs | 0m00.63s || -0m00.04s
    0m00.57s | ModularArithmetic/Pow2Base                      | 0m00.42s || +0m00.14s
    0m00.40s | ModularArithmetic/PseudoMersenneBaseParams      | 0m00.41s || -0m00.00s
    0m00.40s | Spec/ModularArithmetic                          | 0m00.37s || +0m00.03s
    0m00.38s | ModularArithmetic/Montgomery/Z                  | 0m00.43s || -0m00.04s

--- src/Util/ZUtil.v
+++ src/Util/ZUtil.v
@@ -28,7 +28,7 @@ Ltac zutil_arith := solve [ omega | lia ].
 Create HintDb zsimplify discriminated.
 Hint Rewrite Z.div_1_r Z.mul_1_r Z.mul_1_l Z.sub_diag Z.mul_0_r Z.mul_0_l Z.add_0_l Z.add_0_r Z.opp_involutive Z.sub_0_r Z_mod_same_full Z.sub_simpl_r Z.sub_simpl_l Z.add_opp_diag_r Z.add_opp_diag_l Zmod_0_l Z.add_simpl_r Z.add_simpl_l Z.opp_0 Zmod_0_r Zmod_mod : zsimplify.
 Hint Rewrite Z.div_mul Z.div_1_l Z.div_same Z.mod_same Z.div_small Z.mod_small Z.div_add Z.div_add_l Z.mod_add Z.div_0_l Z.mod_mod Z.mod_small Z_mod_zero_opp_full using zutil_arith : zsimplify.
-Hint Rewrite <- Z.opp_eq_mul_m1 : zsimplify.
+Hint Rewrite <- Z.opp_eq_mul_m1 Z.one_succ Z.two_succ : zsimplify.
 
 (** "push" means transform [-f x] to [f (-x)]; "pull" means go the other way *)
 Create HintDb push_Zopp discriminated.
