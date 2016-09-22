ce52efb544820b5ce88091cc68c94a3128c7bdd7
Jason Gross <jgross@mit.edu>
2016-09-22 14:58:37 -0400

    Revert "Add a locked version of [let] with fewer reductions"
    This reverts commit a4565d0c0c506a56a0db0ca09e9ac7af8b8eaee2.
    
    It's not needed.

--- src/Util/LockedLet.v
+++
@@ -1,9 +0,0 @@
-(** * A version of [let] that doesn't disappear under βδζ unless you also have ι and remove opacity *)
-Require Import Crypto.Util.Notations.
-
-Definition locked_let {A} (x : A) : bool * A := (true, x).
-Definition unlock_let {A} (x : A) : locked_let x = (true, x) := eq_refl.
-Global Opaque locked_let.
-Global Arguments locked_let : simpl never.
-
-Notation "'llet' x := A 'in' b" := (let '(_, x) := locked_let A in b).
