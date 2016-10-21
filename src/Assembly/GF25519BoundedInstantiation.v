0bdb30445fd22b5c8e14c9c3cc4176ba0a5e3225
Jason Gross <jgross@mit.edu>
2016-10-21 15:38:59 -0400

    Instantiate some things with bounded things
    With help from Rob, and a bit of type golf.

--- src/Assembly/GF25519BoundedInstantiation.v
+++ src/Assembly/GF25519BoundedInstantiation.v
@@ -1,4 +1,8 @@
 Require Import Coq.ZArith.ZArith.
+Require Import Crypto.Assembly.PhoasCommon.
+Require Import Crypto.Assembly.QhasmCommon.
+Require Import Crypto.Assembly.Compile.
+Require Import Crypto.Assembly.LL.
 Require Import Crypto.Assembly.GF25519.
 Require Import Crypto.Specific.GF25519.
 Require Import Crypto.Specific.GF25519BoundedCommon.
@@ -6,18 +10,38 @@ Require Import Crypto.Util.Tactics.
 Require Import Crypto.Util.Tuple.
 
 (* Totally fine to edit these definitions; DO NOT change the type signatures at all *)
-Definition ExprBinOp : Type
-  := @PhoasCommon.interp_type word64 GF25519.FE -> @PhoasCommon.interp_type word64 GF25519.FE -> @HL.HL.expr word64 (@PhoasCommon.interp_type word64) GF25519.FE.
-Definition ExprUnOp : Type
-  := @PhoasCommon.interp_type word64 GF25519.FE -> @HL.HL.expr word64 (@PhoasCommon.interp_type word64) GF25519.FE.
+Section Operations.
+  Import Assembly.GF25519.GF25519.
+  Definition wfe: Type := @interp_type (word bits) FE.
+
+  Definition ExprBinOp : Type := NAry 20 (@CompileLL.WArg bits TT) (@CompileLL.WExpr bits FE).
+  Definition ExprUnOp : Type := NAry 10 (@CompileLL.WArg bits TT) (@CompileLL.WExpr bits FE).
+
+  Local Existing Instance WordEvaluable.
+
+  Definition interp_bexpr' (op: ExprBinOp) (x y: tuple (word bits) 10): tuple (word bits) 10 :=
+    let '(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9) := x in
+    let '(y0, y1, y2, y3, y4, y5, y6, y7, y8, y9) := y in
+    let op' := NArgMap LL.Const op in
+    let z := LL.interp (op' x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 y0 y1 y2 y3 y4 y5 y6 y7 y8 y9) in
+    z.
+
+  Definition interp_uexpr' (op: ExprUnOp) (x: tuple (word bits) 10): tuple (word bits) 10 :=
+    let '(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9) := x in
+    let op' := NArgMap LL.Const op in
+    let z := LL.interp (op' x0 x1 x2 x3 x4 x5 x6 x7 x8 x9) in
+    z.
+  Definition radd : ExprBinOp := Add.wordProg.
+  Definition rsub : ExprBinOp := Sub.wordProg.
+  Definition rmul : ExprBinOp := Mul.wordProg.
+  Definition ropp : ExprUnOp := Opp.wordProg.
+End Operations.
+
+
 Definition interp_bexpr : ExprBinOp -> Specific.GF25519BoundedCommon.fe25519W -> Specific.GF25519BoundedCommon.fe25519W -> Specific.GF25519BoundedCommon.fe25519W
-  := fun f x y => HL.HL.interp (E := Evaluables.WordEvaluable) (t := GF25519.FE) (f x y).
+  := interp_bexpr'.
 Definition interp_uexpr : ExprUnOp -> Specific.GF25519BoundedCommon.fe25519W -> Specific.GF25519BoundedCommon.fe25519W
-  := fun f x => HL.HL.interp (E := Evaluables.WordEvaluable) (t := GF25519.FE) (f x).
-Axiom radd : ExprBinOp. (* := GF25519.AddExpr.ge25519_add. *)
-Axiom rsub : ExprBinOp. (* := GF25519.SubExpr.ge25519_sub. *)
-Axiom rmul : ExprBinOp. (* := GF25519.MulExpr.ge25519_mul. *)
-Axiom ropp : ExprUnOp. (* := GF25519.OppExpr.ge25519_opp. *)
+  := interp_uexpr'.
 Axiom rfreeze : ExprUnOp.
 
 Local Notation binop_correct_and_bounded rop op
@@ -33,35 +57,18 @@ Local Ltac start_correct_and_bounded_t op op_expr lem :=
 
 Lemma radd_correct_and_bounded : binop_correct_and_bounded radd carry_add.
 Proof.
-  start_correct_and_bounded_t
-    carry_add GF25519.AddExpr.ge25519_add_expr
-    (fun x y => proj2_sig (GF25519.AddExpr.ge25519_add' x y)).
-  simpl @fe25519WToZ.
+  intros; hnf in *; destruct_head' prod; simpl in * |- .
+  repeat match goal with H : is_bounded _ = true |- _ => unfold_is_bounded_in H end.
 Admitted.
 Lemma rsub_correct_and_bounded : binop_correct_and_bounded rsub carry_sub.
 Proof.
-  start_correct_and_bounded_t
-    carry_sub GF25519.SubExpr.ge25519_sub_expr
-    (fun x y => proj2_sig (GF25519.SubExpr.ge25519_sub' x y)).
-  simpl @fe25519WToZ.
 Admitted.
 Lemma rmul_correct_and_bounded : binop_correct_and_bounded rmul mul.
 Proof.
-  start_correct_and_bounded_t
-    Specific.GF25519.mul GF25519.MulExpr.ge25519_mul_expr
-    (fun x y => proj2_sig (GF25519.MulExpr.ge25519_mul' x y)).
-  simpl @fe25519WToZ.
 Admitted.
 Lemma ropp_correct_and_bounded : unop_correct_and_bounded ropp carry_opp.
 Proof.
-  start_correct_and_bounded_t
-    carry_opp GF25519.OppExpr.ge25519_opp_expr
-    (fun x => proj2_sig (GF25519.OppExpr.ge25519_opp' x)).
-  simpl @fe25519WToZ.
 Admitted.
 Lemma rfreeze_correct_and_bounded : unop_correct_and_bounded rfreeze freeze.
 Proof.
-  intros; hnf in *; destruct_head' prod; simpl in * |- ;
-  repeat match goal with H : is_bounded _ = true |- _ => unfold_is_bounded_in H end.
-  simpl @fe25519WToZ.
 Admitted.
