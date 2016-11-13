a02b6fc736299eb90e03640a9d2902a128ad07ae
Jason Gross <jgross@mit.edu>
2016-11-13 17:03:49 -0500

    Don't copy bounds files if no argument is passed

--- src/SpecificGen/copy_bounds.sh
+++ src/SpecificGen/copy_bounds.sh
@@ -7,6 +7,11 @@ cd "$DIR"
 FILENAME="$1"
 V_FILE_STEM="${FILENAME%.*}"
 
+if [ -z "$V_FILE_STEM" ]; then
+    echo "USAGE: $0 JSON_FILE"
+    exit 1
+fi
+
 for ORIG in $(git ls-files "../Specific/**GF25519*.v" | grep -v "../Specific/GF25519.v"); do
     NEW="$(echo "$ORIG" | sed s'|^../Specific|.|g' | sed s"|25519|${V_FILE_STEM}|g")"
     echo "$NEW"
