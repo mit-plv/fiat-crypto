848d7a00a5df9cef2332742cf24930dc1492a4a2
jadep <jade.philipoom@gmail.com>
2016-12-13 14:31:43 -0500

    Proved carry and proved ring-ness of base system ops

--- src/NewBaseSystem.v
+++ src/NewBaseSystem.v
@@ -15,10 +15,20 @@ Module B.
 
     Definition add (p q : rep) : rep := p ++ q.
 
+    Definition opp (p : rep) : rep :=
+      map (fun cx => (fst cx, - (snd cx))) p.
+
     Definition mul (p q:rep) : rep :=
       flat_map (fun t => let '(c, x) := t in
                          map (fun t' => let '(c', x') := t' in
                                         (c * c', x * x')) q) p.
+
+    Definition carry (from next : Z) (p : rep) : rep :=
+      let cap := (next / from)%Z in
+      flat_map (fun cx => let '(c,x) := cx in
+                          if dec (Logic.eq c from)
+                          then ((c, x mod cap) :: (next, x / cap) :: nil)%Z
+                          else ((c,x) :: nil)) p.
 
     Definition gather_or_leave b cx gso : rep * Z :=
       if dec (Logic.eq ((fst cx) mod b) 0)%Z
@@ -34,12 +44,6 @@ Module B.
                     (fst gso, snd gso::snd state)) (p, nil) base.
     Definition gather b p := snd (gather' b p).
 
-    Definition carry (from next : Z) (p : rep) : rep :=
-      let cap := (next / from)%Z in
-      flat_map (fun cx => let '(c,x) := cx in
-                          if dec (Logic.eq c from)
-                          then ((c, x mod cap) :: (next, x / cap) :: nil)%Z
-                          else nil) p.
 
   (*
     Let base := (1::10::100::1000::10000::100000::1000000::10000000::nil)%Z.
@@ -55,7 +59,7 @@ Module B.
     Compute gather base (mul f g).
   *)
 
-    Section NewBaseSystemProofs.
+    Section Proofs.
 
     Lemma eval_nil : eval nil = 0. Proof. reflexivity. Qed.
     Lemma eval_cons p q : eval (p::q) = (fst p) * (snd p) + eval q. Proof. reflexivity. Qed.
@@ -68,6 +72,12 @@ Module B.
     Lemma add_correct p q : eval (add p q) = eval p + eval q.
     Proof.
       induction p; simpl; rewrite ?eval_nil, ?eval_cons, ?left_identity, ?IHp, ?associative; reflexivity.
+    Qed.
+
+    Lemma opp_correct p : eval (opp p) = - (eval p).
+    Proof.
+      induction p; simpl; rewrite ?eval_nil, ?eval_cons; [reflexivity|].
+      simpl fst; simpl snd. rewrite IHp. ring.
     Qed.
 
     Lemma mul_single a q : eval (mul (a :: nil) q) = fst a * snd a * eval q.
@@ -87,6 +97,24 @@ Module B.
     Proof.
       induction p; [destruct q; simpl; rewrite ?eval_nil, ?mul_0_l; reflexivity |].
       rewrite mul_correct', mul_single, eval_cons, IHp; ring.
+    Qed.
+
+    Lemma carry_correct from next: from <> 0 ->
+                                   next mod from = 0 ->
+                                   next / from <> 0 ->
+                                   forall p,
+                                      eval (carry from next p) = eval p.
+    Proof.
+      induction p; [reflexivity|]; intros; simpl carry; destruct_head prod.
+      break_match; subst; rewrite !eval_app, ?eval_cons, ?eval_nil;
+        rewrite IHp; [|ring].
+      simpl fst; simpl snd; ring_simplify.
+      match goal with |- ?a + ?b + ?x = ?c + ?x =>
+                      replace (a + b) with c; [ring|] end.
+      match goal with |- _ * ?x = _ * (?x mod ?y) + _ * (?x / ?y) =>
+                      rewrite (Z.div_mod x y) at 1 by auto end.
+      rewrite Z.mul_add_distr_l, !Z.mul_assoc.
+      rewrite <-Z_div_exact_full_2 by assumption. ring.
     Qed.
 
     Lemma gather_single_all p : Logic.eq (fst (gather_single 1%Z p)) nil.
@@ -125,6 +153,40 @@ Module B.
       rewrite gather_single_all, eval_nil. simpl fst; simpl snd. ring.
     Qed.
 
-    End NewBaseSystemProofs.
+    End Proofs.
+
+    Section Ring.
+
+    Definition eq (p q : rep) : Prop := eval p = eval q.
+
+    Definition zero : rep := nil.
+
+    Definition one : rep := ((1,1) :: nil).
+
+    Definition sub (p q : rep) : rep := add p (opp q).
+
+    Lemma sub_correct p q : eval (sub p q) = eval p - eval q.
+    Proof. cbv [sub]. rewrite add_correct, opp_correct. ring. Qed.
+
+    Lemma is_ring : @Algebra.ring rep eq zero one opp add sub mul.
+    Proof.
+      cbv [eq]; eapply (isomorphism_to_subring_ring (ringR := ring_Z) (phi := eval)).
+      Grab Existential Variables.
+      + reflexivity.
+      + reflexivity.
+      + apply mul_correct.
+      + apply sub_correct.
+      + apply add_correct.
+      + apply opp_correct.
+      + tauto.
+      + repeat intro. rewrite !mul_correct. congruence.
+      + repeat intro. rewrite !sub_correct. congruence.
+      + repeat intro. rewrite !add_correct. congruence.
+      + repeat intro. rewrite !opp_correct. congruence.
+      + intros; apply Z.eq_dec.
+      + econstructor; repeat intro; congruence.
+    Qed.
+    End Ring.
+    
   End NewBaseSystem.
 End B.
