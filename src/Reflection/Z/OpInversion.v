5b43c706b98b722b44a7c36438b30eed525e3f7d
Jason Gross <jgross@mit.edu>
2017-02-01 17:06:10 -0500

    Add invert_match_op

--- src/Reflection/Z/OpInversion.v
+++ src/Reflection/Z/OpInversion.v
@@ -16,3 +16,13 @@ Ltac invert_op_step :=
   end.
 
 Ltac invert_op := repeat invert_op_step.
+
+Ltac invert_match_op_step :=
+  match goal with
+  | [ |- appcontext[match ?e with OpConst _ => _ | _ => _ end] ]
+    => invert_one_op e
+  | [ H : appcontext[match ?e with OpConst _ => _ | _ => _ end] |- _ ]
+    => invert_one_op e
+  end.
+
+Ltac invert_match_op := repeat invert_match_op_step.
