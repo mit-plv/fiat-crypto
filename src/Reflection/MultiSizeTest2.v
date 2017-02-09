565785dbd1b81a02ae446d0acdd4db11b03b8c0f
Jason Gross <jgross@mit.edu>
2017-02-08 20:26:46 -0500

    Remove useless imports

--- src/Reflection/MultiSizeTest2.v
+++ src/Reflection/MultiSizeTest2.v
@@ -1,11 +1,6 @@
 Require Import Coq.omega.Omega.
 Require Import Crypto.Reflection.Syntax.
 Require Import Crypto.Reflection.BoundByCast.
-(*Require Import Crypto.Reflection.SmartMap.
-Require Import Crypto.Reflection.Inline.*)
-Require Import Crypto.Reflection.Equality. (*
-Require Import Crypto.Reflection.Application.
-Require Import Crypto.Reflection.MapCast.*)
 
 (** * Preliminaries: bounded and unbounded number types *)
 
