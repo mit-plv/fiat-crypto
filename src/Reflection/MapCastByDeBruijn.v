7854fc8d25bd42c84a37ac5d34b4143eadd39ece
Jason Gross <jgross@mit.edu>
2017-03-17 18:28:25 -0400

    Have cast_op return option exprf instead of op
    This is, unfortunately, a pain to wrap up into a PHOAS transformation,
    because we have to compile the arguments to PHOAS, then apply the
    transformation, then compile back.  Hopefully it won't be too tricky to
    prove that the composition is the identity.

--- src/Reflection/MapCastByDeBruijn.v
+++ src/Reflection/MapCastByDeBruijn.v
@@ -1,8 +1,10 @@
+Require Import Coq.Numbers.BinNums.
 Require Import Crypto.Reflection.SmartMap.
 Require Import Crypto.Reflection.Named.Syntax.
 Require Import Crypto.Reflection.Named.MapCast.
 Require Import Crypto.Reflection.Named.InterpretToPHOAS.
 Require Import Crypto.Reflection.Named.Compile.
+Require Import Crypto.Reflection.Named.IdContext.
 Require Import Crypto.Reflection.Named.PositiveContext.
 Require Import Crypto.Reflection.Named.PositiveContext.Defaults.
 Require Import Crypto.Reflection.Linearize.
@@ -18,17 +20,30 @@ Section language.
           (interp_op_bounds : forall src dst, op src dst -> interp_flat_type interp_base_type_bounds src -> interp_flat_type interp_base_type_bounds dst)
           (pick_typeb : forall t, interp_base_type_bounds t -> base_type_code).
   Local Notation pick_type v := (SmartFlatTypeMap pick_typeb v).
-  Context (cast_op : forall t tR (opc : op t tR) args_bs,
-              op (pick_type args_bs) (pick_type (interp_op_bounds t tR opc args_bs))).
+  Context (cast_op : forall var t tR (opc : op t tR) args_bs
+                            (args : exprf base_type_code op (var:=var) (pick_type args_bs)),
+              option (exprf base_type_code op (var:=var) (pick_type (interp_op_bounds t tR opc args_bs)))).
+
+  Definition named_cast_op
+             t tR (opc : op t tR) args_bs
+             (args : Named.exprf base_type_code op BinNums.positive (pick_type args_bs))
+    : option (Named.exprf base_type_code op BinNums.positive (pick_type (interp_op_bounds t tR opc args_bs)))
+    := let Context var := PositiveContext _ var _ base_type_code_bl_transparent in
+       let ctx := idcontext (Context _) args in
+       let args_PHOAS := interpf_to_phoas (failb _) ctx args in
+       match cast_op _ _ _ opc args_bs args_PHOAS with
+       | Some e => compilef e (default_names_forf (fun _ => 1%positive) e)
+       | None => None
+       end.
 
   Definition MapCast {t} (e : Expr base_type_code op t)
              (input_bounds : interp_flat_type interp_base_type_bounds (domain t))
     : option { output_bounds : interp_flat_type interp_base_type_bounds (codomain t)
                                & Expr base_type_code op (Arrow (pick_type input_bounds) (pick_type output_bounds)) }
-    := let Context var := PositiveContext _ _ _ base_type_code_bl_transparent in
+    := let Context var := PositiveContext _ var _ base_type_code_bl_transparent in
        match option_map
                (fun e' => map_cast
-                            interp_op_bounds pick_typeb cast_op
+                            interp_op_bounds pick_typeb named_cast_op
                             (BoundsContext:=Context _)
                             empty
                             e'
--- src/Reflection/Named/MapCast.v
+++ src/Reflection/Named/MapCast.v
@@ -12,8 +12,10 @@ Section language.
           (interp_op_bounds : forall src dst, op src dst -> interp_flat_type interp_base_type_bounds src -> interp_flat_type interp_base_type_bounds dst)
           (pick_typeb : forall t, interp_base_type_bounds t -> base_type_code).
   Local Notation pick_type v := (SmartFlatTypeMap pick_typeb v).
-  Context (cast_op : forall t tR (opc : op t tR) args_bs,
-              op (pick_type args_bs) (pick_type (interp_op_bounds t tR opc args_bs)))
+  Context (cast_op
+           : forall t tR (opc : op t tR) args_bs
+                    (args : Named.exprf base_type_code op Name (pick_type args_bs)),
+              option (Named.exprf base_type_code op Name (pick_type (interp_op_bounds t tR opc args_bs))))
           {BoundsContext : Context Name interp_base_type_bounds}.
 
   Fixpoint mapf_cast
@@ -46,13 +48,14 @@ Section language.
             | None => None
             end
        | Op t tR opc args
-         => option_map
-              (fun args'
-               => let 'existT args_bounds argsv := args' in
-                  existT _
-                         (interp_op_bounds _ _ _ args_bounds)
-                         (Op (cast_op t tR opc args_bounds) argsv))
-              (@mapf_cast ctx _ args)
+         => match @mapf_cast ctx _ args with
+            | Some (existT args_bounds argsv)
+              => option_map
+                   (existT _
+                           (interp_op_bounds _ _ _ args_bounds))
+                   (cast_op t tR opc args_bounds argsv)
+            | None => None
+            end
        end.
 
   Definition map_cast
