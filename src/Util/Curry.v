f89309d47a1f4095430c984b4ab81c804a9263ae
Jason Gross <jgross@mit.edu>
2017-03-31 16:15:47 -0400

    Add [change_with_curried] to Curry.v

--- src/Util/Curry.v
+++ src/Util/Curry.v
@@ -1,2 +1,16 @@
 Definition curry2 {A B C} (f : A -> B -> C) (x : A * B) : C
 := let '(a, b) := x in f a b.
+
+Ltac change_with_curried f :=
+  cbv beta in f; (* work around https://coq.inria.fr/bugs/show_bug.cgi?id=5430 *)
+  lazymatch type of f with
+  | ?A -> ?B -> ?C
+    => let f' := fresh f in
+       rename f into f';
+       pose (fun (ab : A * B) => f' (fst ab) (snd ab)) as f;
+       change f' with (fun (a : A) (b : B) => f (a, b));
+       subst f';
+       cbv beta;
+       change_with_curried f
+  | _ => idtac
+  end.
