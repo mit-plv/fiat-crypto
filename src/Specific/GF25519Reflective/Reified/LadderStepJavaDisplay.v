d969b7e3c0ffcf997887e099d0917ac3a31ae5aa
Jason Gross <jgross@mit.edu>
2017-04-02 11:08:32 -0400

    Remove old reflective pipeline, making way the new

--- src/Specific/GF25519Reflective/Reified/LadderStepJavaDisplay.v
+++
@@ -1,4 +0,0 @@
-Require Import Crypto.Specific.GF25519Reflective.Reified.LadderStep.
-Require Export Crypto.Reflection.Z.JavaNotations.
-
-Print rladderstepW.
--- src/Specific/GF25519Reflective/Reified/MulJavaDisplay.v
+++
@@ -1,4 +0,0 @@
-Require Import Crypto.Specific.GF25519Reflective.Reified.Mul.
-Require Export Crypto.Reflection.Z.JavaNotations.
-
-Print rmulW.
