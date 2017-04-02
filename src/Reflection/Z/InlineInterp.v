92f7ffd5e4f6db8529e41d0659b1b69218dac779
Jason Gross <jgross@mit.edu>
2017-04-02 01:35:03 -0400

    Generalize Z.InterpInlineConst

--- src/Reflection/Z/InlineInterp.v
+++ src/Reflection/Z/InlineInterp.v
@@ -4,8 +4,8 @@ Require Import Crypto.Reflection.InlineInterp.
 Require Import Crypto.Reflection.Z.Syntax.
 Require Import Crypto.Reflection.Z.Inline.
 
-Definition InterpInlineConst {t} (e : Expr base_type op t) (Hwf : Wf e)
+Definition InterpInlineConst {interp_base_type interp_op} {t} (e : Expr base_type op t) (Hwf : Wf e)
   : forall x, Interp interp_op (InlineConst e) x = Interp interp_op e x
-  := @InterpInlineConst _ _ _ _ _ t e Hwf.
+  := @InterpInlineConst _ interp_base_type _ _ _ t e Hwf.
 
 Hint Rewrite @InterpInlineConst using solve [ eassumption | eauto with wf ] : reflective_interp.
