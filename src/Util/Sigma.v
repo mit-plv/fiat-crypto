7e87e2654662355dec369076a4bb7dd13900300e
Jason Gross <jgross@mit.edu>
2017-04-03 12:27:58 -0400

    Add proj2_sig_map

--- src/Util/Sigma.v
+++ src/Util/Sigma.v
@@ -232,6 +232,8 @@ Definition lift2_sig {A B C} (P:A->B->C->Prop)
 
 Definition projT2_map {A P Q} (f : forall a, P a -> Q a) (x : @sigT A P) : @sigT A Q
   := let 'existT a p := x in existT Q a (f a p).
+Definition proj2_sig_map {A} {P Q : A -> Prop} (f : forall a, P a -> Q a) (x : @sig A P) : @sig A Q
+  := let 'exist a p := x in exist Q a (f a p).
 
 (** ** Useful Tactics *)
 (** *** [inversion_sigma] *)
