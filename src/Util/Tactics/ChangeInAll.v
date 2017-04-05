8ddfa0e4d0103d29fdc12b10489c4351b5875845
Jason Gross <jgross@mit.edu>
2017-04-05 02:54:21 -0400

    Remove bad [Local]

--- src/Util/Tactics/ChangeInAll.v
+++ src/Util/Tactics/ChangeInAll.v
@@ -1,6 +1,6 @@
 (** Work around "Cannot create self-referring hypothesis" coming from
     [change x with y in *] *)
-Local Ltac change_in_all from to :=
+Ltac change_in_all from to :=
   change from with to;
   repeat match goal with
          | [ H : _ |- _ ] => progress change from with to in H
