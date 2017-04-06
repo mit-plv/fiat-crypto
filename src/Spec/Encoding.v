2adac8f52dcd9ee88f98ce9ec604eaaa9c6a115f
Andres Erbsen <andreser@mit.edu>
2017-04-06 13:20:46 -0400

    remove Encoding stuff

--- src/Spec/Encoding.v
+++
@@ -1,8 +0,0 @@
-Class CanonicalEncoding (T B:Type) := {
-  enc : T -> B ;
-  dec : B -> option T ;
-  encoding_valid : forall x, dec (enc x) = Some x ;
-  encoding_canonical : forall x_enc x, dec x_enc = Some x -> enc x = x_enc
-}.
-
-Notation "'canonical' 'encoding' 'of' T 'as' B" := (CanonicalEncoding T B) (at level 50).
