2d163540e9ce4dada64a496452eed910d7a4ec91
Jason Gross <jgross@mit.edu>
2017-04-14 23:50:05 -0400

    Generalize MapCastCorrect a bit

--- src/Compilers/Z/Bounds/MapCastByDeBruijnInterp.v
+++ src/Compilers/Z/Bounds/MapCastByDeBruijnInterp.v
@@ -25,3 +25,20 @@ Proof.
   { apply is_bounded_by_interp_op. }
   { apply pull_cast_genericize_op, is_bounded_by_interp_op. }
 Qed.
+
+Lemma MapCastCorrect_eq
+      {round_up}
+      {t} (e : Expr base_type op t)
+      {evb ev}
+      (Hwf : Wf e)
+      (input_bounds : interp_flat_type Bounds.interp_base_type (domain t))
+  : forall {b} e' (He':MapCast round_up e input_bounds = Some (existT _ b e'))
+           v v' (Hv : Bounds.is_bounded_by input_bounds v /\ cast_back_flat_const v' = v)
+           (Hevb : evb = Interp (@Bounds.interp_op) e input_bounds)
+           (Hev : ev = Interp interp_op e v),
+    evb = b
+    /\ Bounds.is_bounded_by b ev
+    /\ cast_back_flat_const (Interp interp_op e' v') = ev.
+Proof.
+  intros; subst; apply MapCastCorrect; auto.
+Qed.
