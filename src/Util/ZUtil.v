8a2f98ad75901b352a75b32bfae96148b8fb66d7
Jason Gross <jgross@mit.edu>
2017-05-11 13:50:59 -0400

    Suppress a warning about unused intropatterns

--- src/Util/ZUtil.v
+++ src/Util/ZUtil.v
@@ -604,7 +604,11 @@ Module Z.
 
   Ltac divide_exists_mul := let k := fresh "k" in
   match goal with
-  | [ H : (?a | ?b) |- _ ] => apply Z.mod_divide in H; try apply Zmod_divides in H; destruct H as [k H]
+  | [ H : (?a | ?b) |- _ ] => apply Z.mod_divide in H; try apply Zmod_divides in H;
+                              match type of H with
+                              | ex _ => destruct H as [k H]
+                              | _ => destruct H
+                              end
   | [ |- (?a | ?b) ] => apply Z.mod_divide; try apply Zmod_divides
   end; (omega || auto).
 
