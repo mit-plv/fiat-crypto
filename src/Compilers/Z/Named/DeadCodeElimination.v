b30aa37ee462081853468707ac01b7665a94ae68
Jason Gross <jgross@mit.edu>
2017-05-14 19:08:28 -0400

    Add Z/Named/DeadCodeElimination.v

---
+++ src/Compilers/Z/Named/DeadCodeElimination.v
@@ -0,0 +1,14 @@
+Require Import Coq.PArith.BinPos.
+Require Import Crypto.Compilers.Named.Context.
+Require Import Crypto.Compilers.Named.DeadCodeElimination.
+Require Import Crypto.Compilers.Z.Syntax.
+
+Section language.
+  Context {Name : Type}
+          {Context : Context Name (fun _ : base_type => positive)}.
+
+  Definition EliminateDeadCode {t} e ls
+    := @EliminateDeadCode base_type op Name Context t e ls.
+  Definition CompileAndEliminateDeadCode {t} e ls
+    := @CompileAndEliminateDeadCode base_type op Name Context t e ls.
+End language.
