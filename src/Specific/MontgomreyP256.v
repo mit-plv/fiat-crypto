6bacbc4e0b980507a2b9246f7a120dbe4052e10c
Jason Gross <jgross@mit.edu>
2017-06-16 19:39:03 -0400

    Unfold more things in src/Specific/MontgomreyP256
    It seems that something gets unfolded which should not get unfolded.
    But we no longer block on lists.

--- src/Specific/MontgomreyP256.v
+++ src/Specific/MontgomreyP256.v
@@ -3,6 +3,7 @@ Require Import Crypto.Arithmetic.Core. Import B.
 Require Import Crypto.Util.Sigma.Lift.
 Require Import Coq.ZArith.BinInt.
 Require Import Coq.PArith.BinPos.
+Require Import Crypto.Util.LetIn.
 
 Definition wt (i:nat) : Z := Z.shiftl 1 (64*Z.of_nat i).
 Definition r := Eval compute in (2^64)%positive.
@@ -21,6 +22,7 @@ Proof.
                            )); eexists.
   cbv [
       r wt sz p256
+        CPSUtil.Tuple.tl_cps CPSUtil.Tuple.hd_cps
        CPSUtil.to_list_cps CPSUtil.to_list'_cps CPSUtil.to_list_cps' CPSUtil.flat_map_cps CPSUtil.fold_right_cps
        CPSUtil.map_cps CPSUtil.Tuple.left_append_cps CPSUtil.firstn_cps CPSUtil.combine_cps CPSUtil.on_tuple_cps CPSUtil.update_nth_cps CPSUtil.from_list_default_cps CPSUtil.from_list_default'_cps
        fst snd length List.seq List.hd List.app
@@ -31,9 +33,29 @@ Proof.
        Saturated.uweight
        Saturated.Columns.mul_cps
        Saturated.Associational.mul_cps
-       Z.of_nat Pos.of_succ_nat  Nat.pred
-       Z.pow Z.pow_pos Z.mul Pos.iter Pos.mul Pos.succ
+       (*Z.of_nat Pos.of_succ_nat  Nat.pred
+       Z.pow Z.pow_pos Z.mul Pos.iter Pos.mul Pos.succ*)
+       Tuple.hd Tuple.append Tuple.tl Tuple.hd' Tuple.tl' CPSUtil.Tuple.left_tl_cps CPSUtil.Tuple.left_hd_cps CPSUtil.Tuple.hd_cps CPSUtil.Tuple.tl_cps
        Saturated.Columns.from_associational_cps
+       Saturated.Associational.multerm_cps
+       Saturated.Columns.compact_cps
+       Saturated.Columns.compact_step_cps
+       Saturated.Columns.compact_digit_cps
+       Saturated.drop_high_cps
+       Saturated.add_cps
+       Saturated.Columns.add_cps
+       Saturated.Columns.cons_to_nth_cps
+       Nat.pred
+       Saturated.join0
+       Saturated.join0_cps CPSUtil.Tuple.left_append_cps
+       CPSUtil.Tuple.mapi_with_cps
+       id
+       Positional.zeros Saturated.zero Saturated.Columns.nils Tuple.repeat Tuple.append
+       Positional.place_cps
+       CPSUtil.Tuple.mapi_with'_cps Tuple.hd Tuple.hd' Tuple.tl Tuple.tl' CPSUtil.Tuple.hd_cps CPSUtil.Tuple.tl_cps fst snd
+       Z.of_nat fst snd Z.pow Z.pow_pos Pos.of_succ_nat Z.div Z.mul Pos.mul Z.modulo Z.div_eucl Z.pos_div_eucl Z.leb Z.compare Pos.compare_cont Pos.compare Z.pow_pos Pos.iter Z.mul Pos.succ Z.ltb Z.gtb Z.geb Z.div Z.sub Z.pos_sub Z.add Z.opp Z.double
+       Decidable.dec Decidable.dec_eq_Z Z.eq_dec Z_rec Z_rec Z_rect
+       Positional.zeros Saturated.zero Saturated.Columns.nils Tuple.repeat Tuple.append
        (*
        Saturated.Associational.multerm_cps
        Saturated.Columns.from_associational_cps Positional.place_cps Saturated.Columns.cons_to_nth_cps Saturated.Columns.nils
@@ -55,10 +77,8 @@ List.tl
 Nat.max
 Positional.to_associational_cps
 Z.of_nat *)
-      ].
-  (* stuck on [("fix app" ... ) (cps_returned_opaque_list) _] *)
-  cbv [Decidable.dec Decidable.dec_eq_Z Z.eq_dec Z.modulo].
-  cbv [List.app].
+    ].
+
   (* cbv -[runtime_add runtime_mul LetIn.Let_In Definitions.Z.add_get_carry_full Definitions.Z.mul_split]. *)
 
   (* basesystem_partial_evaluation_RHS. *)
