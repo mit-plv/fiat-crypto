694c4878a779faa6d139f683928d11b98c0db812
Andres Erbsen <andreser@mit.edu>
2017-06-18 17:12:22 -0400

    handle missing /sys/class/power_supply/AC

--- etc/machine.sh
+++ etc/machine.sh
@@ -7,7 +7,11 @@ grep -q '[^0-9]' /sys/devices/system/cpu/cpu[0-9]*/topology/thread_siblings_list
 printf -
 grep -q 1 /sys/devices/system/cpu/intel_pstate/no_turbo && printf notb || printf tb
 printf -
-grep -q 1 /sys/class/power_supply/AC/online && printf ac || printf noac
+if [ -f /sys/class/power_supply/AC/online ]; then
+  grep -q 1 /sys/class/power_supply/AC/online && printf ac || printf noac
+else
+  printf nops
+fi
 printf -
 printf "$(gcc -march=native -Q --help=target|grep march | cut -d= -f2 | grep -ow '\S*')"
 printf -
