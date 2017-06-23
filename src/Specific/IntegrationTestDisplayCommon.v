d24cdf05127cf33588d121bd6a3bd72cab94a475
Jason Gross <jgross@mit.edu>
2017-06-22 22:36:27 -0400

    Fix an issue with notations
    CNotations breaks writing of Gallina and Ltac `match`.  So we need to
    import it very, very late.

--- src/Specific/IntegrationTestDisplayCommon.v
+++ src/Specific/IntegrationTestDisplayCommon.v
@@ -7,8 +7,9 @@ Require Import Crypto.Compilers.Eta.
 Require Export Coq.ZArith.ZArith.
 Require Export Crypto.Util.LetIn.
 Require Export Crypto.Util.FixedWordSizes.
-Require Export Crypto.Compilers.Z.CNotations.
-Require Export Coq.Unicode.Utf8. (* for better line breaks at function display; must come last *)
+Require Export Crypto.Compilers.Syntax.
+Require Export Crypto.Compilers.Z.HexNotationConstants.
+Require Export Crypto.Util.Notations.
 
 Global Arguments Pos.to_nat !_ / .
 Global Arguments InterpEta {_ _ _ _ _}.
@@ -89,6 +90,10 @@ Tactic Notation "display" open_constr(f) :=
   refine ret.
 Notation display f := (ltac:(let v := f in display v)) (only parsing).
 
+Require Export Crypto.Compilers.Z.CNotations.
+
 Notation "'Interp-η' f x" := (Eta.InterpEta f x) (format "'Interp-η' '//' f '//' x", at level 200, f at next level, x at next level).
 Notation ReturnType := (interp_flat_type _).
 Notation "'let' ( a , b ) := c 'in' d" := (let (a, b) := c in d) (at level 200, d at level 200, format "'let'  ( a ,  b )  :=  c  'in' '//' d").
+
+Require Export Coq.Unicode.Utf8. (* for better line breaks at function display; must come last *)
