9ec08a1cdb368467e5e79bce840686ad845b125c
Jason Gross <jgross@mit.edu>
2017-09-16 04:22:21 -0400

    Check if /sys/devices/system/cpu/intel_pstate/no_turbo exists before searching it

--- etc/machine.sh
+++ etc/machine.sh
@@ -5,7 +5,11 @@ printf "$(hostname)"
 printf -
 grep -q '[^0-9]' /sys/devices/system/cpu/cpu[0-9]*/topology/thread_siblings_list && printf ht || printf noht
 printf -
-grep -q 1 /sys/devices/system/cpu/intel_pstate/no_turbo && printf notb || printf tb
+if [ -f /sys/devices/system/cpu/intel_pstate/no_turbo ]; then
+  grep -q 1 /sys/devices/system/cpu/intel_pstate/no_turbo && printf notb || printf tb
+else
+  printf notb_support
+fi
 printf -
 if [ -f /sys/class/power_supply/AC/online ]; then
   grep -q 1 /sys/class/power_supply/AC/online && printf ac || printf noac
