1ad6f6ba1e91d021f207b2fbdcabfbd537633903
Jason Gross <jgross@mit.edu>
2017-10-13 17:19:12 -0400

    Update smithers script

--- etc/ci/smithers.sh
+++ etc/ci/smithers.sh
@@ -10,4 +10,5 @@ rm -f BUILD.ok
 ((/opt/timeout/default-timeout make -j$(nproc) TIMED=1 PROFILE=1 $TARGETS || make STDTIME='/opt/timeout/time-default-timeout-coq -f "$* (real: %e, user: %U, sys: %S, mem: %M ko)"' TIMED=1 PROFILE=1 $TARGETS) && touch BUILD.ok) 2>&1 | tee time-of-build.log
 python ./etc/coq-scripts/timing/make-one-time-file.py time-of-build.log time-of-build-pretty.log
 cat time-of-build-pretty.log
+/opt/timeout/default-timeout make -j$(nproc) TIMED=1 PROFILE=1 $TARGETS || make STDTIME='/opt/timeout/time-default-timeout-coq -f "$* (real: %e, user: %U, sys: %S, mem: %M ko)"' TIMED=1 PROFILE=1 $TARGETS
 rm BUILD.ok
