f32c60ffa460088bd3b9a15976ba3bd347e2e623
Jason Gross <jgross@mit.edu>
2017-10-20 13:35:46 -0400

    Add curry{3,4}

--- src/Util/Curry.v
+++ src/Util/Curry.v
@@ -1,7 +1,11 @@
 Require Import Crypto.Util.Tactics.ChangeInAll.
 
 Definition curry2 {A B C} (f : A -> B -> C) (x : A * B) : C
-:= let '(a, b) := x in f a b.
+  := let '(a, b) := x in f a b.
+Definition curry3 {A B C D} (f : A -> B -> C -> D) (x : A * B * C) : D
+  := let '(a, b, c) := x in f a b c.
+Definition curry4 {A B C D E} (f : A -> B -> C -> D -> E) (x : A * B * C * D) : E
+  := let '(a, b, c, d) := x in f a b c d.
 
 Ltac change_with_curried f :=
   cbv beta in f; (* work around https://coq.inria.fr/bugs/show_bug.cgi?id=5430 *)
