d810c151b464a461cf93003fbba8054a286e7dd8
Jason Gross <jgross@mit.edu>
2017-10-22 16:17:40 -0400

    Add inline_const_and_op{f,} specializations

--- src/Compilers/ZExtended/InlineConstAndOp.v
+++ src/Compilers/ZExtended/InlineConstAndOp.v
@@ -2,5 +2,9 @@ Require Import Crypto.Compilers.Syntax.
 Require Import Crypto.Compilers.InlineConstAndOp.
 Require Import Crypto.Compilers.ZExtended.Syntax.
 
+Definition inline_const_and_opf {var} {t} (e : exprf _ _ t) : @exprf base_type op var t
+  := @inline_const_and_opf base_type op interp_base_type (@interp_op) var (@Const) t e.
+Definition inline_const_and_op {var} {t} (e : expr _ _ t) : @expr base_type op var t
+  := @inline_const_and_op base_type op interp_base_type (@interp_op) var (@Const) t e.
 Definition InlineConstAndOp {t} (e : Expr t) : Expr t
   := @InlineConstAndOp base_type op interp_base_type (@interp_op) (@Const) t e.
