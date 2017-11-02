984f4adef33698ef2fa62f630eb0ef2c7b1f6bf6
Jason Gross <jgross@mit.edu>
2017-11-02 03:19:02 -0400

    python3 generate_parameters.py primes.txt; pushd src/Specific/CurveParameters/; ./remake_curves.sh -f; popd

--- src/Specific/montgomery32_2e255m765/compilerxx.sh
+++ src/Specific/montgomery32_2e255m765/compilerxx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-g++ -fno-peephole2 `#GCC BUG 81300` -march=native -mtune=native -std=gnu++11 -O3 -flto -fomit-frame-pointer -fwrapv -Wno-attributes -Wno-incompatible-pointer-types -fno-strict-aliasing -Dq_mpz='(1_mpz<<255) - 765' -Dmodulus_bytes_val='32' "$@"
+g++ -fno-peephole2 `#GCC BUG 81300` -march=native -mtune=native -std=gnu++11 -O3 -flto -fomit-frame-pointer -fwrapv -Wno-attributes -fno-strict-aliasing -Dq_mpz='(1_mpz<<255) - 765' -Dmodulus_bytes_val='32' "$@"
