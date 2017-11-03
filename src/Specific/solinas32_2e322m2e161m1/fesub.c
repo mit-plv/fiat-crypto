b53acd194a2d6e797a06a05241ea8d394cf1d5cb
Jason Gross <jgross@mit.edu>
2017-11-03 09:26:17 -0400

    Update display logs and c files

--- src/Specific/solinas32_2e322m2e161m1/fesub.c
+++ src/Specific/solinas32_2e322m2e161m1/fesub.c
@@ -27,19 +27,19 @@ static void fesub(uint32_t out[14], const uint32_t in1[14], const uint32_t in2[1
   { const uint32_t x35 = in2[2];
   { const uint32_t x33 = in2[1];
   { const uint32_t x31 = in2[0];
-  out[0] = ((0x1ffffde + x5) - x31);
-  out[1] = ((0x1fffffe + x7) - x33);
-  out[2] = ((0x1fffffe + x9) - x35);
-  out[3] = ((0x1fffffe + x11) - x37);
-  out[4] = ((0x1fffffe + x13) - x39);
-  out[5] = ((0x1fffffe + x15) - x41);
-  out[6] = ((0x1fffffe + x17) - x43);
-  out[7] = ((0x1fffffe + x19) - x45);
-  out[8] = ((0x1fffffe + x21) - x47);
-  out[9] = ((0x1fffffe + x23) - x49);
-  out[10] = ((0x1fffffe + x25) - x51);
-  out[11] = ((0x1fffffe + x27) - x53);
-  out[12] = ((0x1fffffe + x29) - x55);
-  out[13] = ((0x1fffffe + x28) - x54);
+  out[0] = ((0xfffffe + x5) - x31);
+  out[1] = ((0xfffffe + x7) - x33);
+  out[2] = ((0xfffffe + x9) - x35);
+  out[3] = ((0xfffffe + x11) - x37);
+  out[4] = ((0xfffffe + x13) - x39);
+  out[5] = ((0xfffffe + x15) - x41);
+  out[6] = ((0xfffffe + x17) - x43);
+  out[7] = ((Const 16777212 + x19) - x45);
+  out[8] = ((0xfffffe + x21) - x47);
+  out[9] = ((0xfffffe + x23) - x49);
+  out[10] = ((0xfffffe + x25) - x51);
+  out[11] = ((0xfffffe + x27) - x53);
+  out[12] = ((0xfffffe + x29) - x55);
+  out[13] = ((0xfffffe + x28) - x54);
   }}}}}}}}}}}}}}}}}}}}}}}}}}}}
 }
