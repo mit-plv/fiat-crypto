e27773d118b8c5bf2d9df8912e1e0f5e25668b44
Jason Gross <jgross@mit.edu>
2017-11-13 12:49:15 -0500

    Update display logs and c files

---
+++ src/Specific/montgomery32_2e224m2e96p1_7limbs/fenz.c
@@ -0,0 +1,17 @@
+static void fenz(ReturnType uint32_t out[1], const uint32_t in1[7]) {
+  { const uint32_t x11 = in1[6];
+  { const uint32_t x12 = in1[5];
+  { const uint32_t x10 = in1[4];
+  { const uint32_t x8 = in1[3];
+  { const uint32_t x6 = in1[2];
+  { const uint32_t x4 = in1[1];
+  { const uint32_t x2 = in1[0];
+  { uint32_t x13 = (x12 | x11);
+  { uint32_t x14 = (x10 | x13);
+  { uint32_t x15 = (x8 | x14);
+  { uint32_t x16 = (x6 | x15);
+  { uint32_t x17 = (x4 | x16);
+  { uint32_t x18 = (x2 | x17);
+  out[0] = x18;
+  }}}}}}}}}}}}}
+}
