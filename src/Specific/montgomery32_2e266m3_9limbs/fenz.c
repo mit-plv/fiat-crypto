da5e6c096d452c15ecf45572b2e431bed8e21271
Jason Gross <jgross@mit.edu>
2017-11-13 23:18:47 -0500

    Update display logs and c files

---
+++ src/Specific/montgomery32_2e266m3_9limbs/fenz.c
@@ -0,0 +1,21 @@
+static void fenz(ReturnType uint32_t out[1], const uint32_t in1[9]) {
+  { const uint32_t x15 = in1[8];
+  { const uint32_t x16 = in1[7];
+  { const uint32_t x14 = in1[6];
+  { const uint32_t x12 = in1[5];
+  { const uint32_t x10 = in1[4];
+  { const uint32_t x8 = in1[3];
+  { const uint32_t x6 = in1[2];
+  { const uint32_t x4 = in1[1];
+  { const uint32_t x2 = in1[0];
+  { uint32_t x17 = (x16 | x15);
+  { uint32_t x18 = (x14 | x17);
+  { uint32_t x19 = (x12 | x18);
+  { uint32_t x20 = (x10 | x19);
+  { uint32_t x21 = (x8 | x20);
+  { uint32_t x22 = (x6 | x21);
+  { uint32_t x23 = (x4 | x22);
+  { uint32_t x24 = (x2 | x23);
+  out[0] = x24;
+  }}}}}}}}}}}}}}}}}
+}
---
+++ src/Specific/montgomery64_2e521m1_9limbs/fenz.c
@@ -0,0 +1,21 @@
+static void fenz(ReturnType uint64_t out[1], const uint64_t in1[9]) {
+  { const uint64_t x15 = in1[8];
+  { const uint64_t x16 = in1[7];
+  { const uint64_t x14 = in1[6];
+  { const uint64_t x12 = in1[5];
+  { const uint64_t x10 = in1[4];
+  { const uint64_t x8 = in1[3];
+  { const uint64_t x6 = in1[2];
+  { const uint64_t x4 = in1[1];
+  { const uint64_t x2 = in1[0];
+  { uint64_t x17 = (x16 | x15);
+  { uint64_t x18 = (x14 | x17);
+  { uint64_t x19 = (x12 | x18);
+  { uint64_t x20 = (x10 | x19);
+  { uint64_t x21 = (x8 | x20);
+  { uint64_t x22 = (x6 | x21);
+  { uint64_t x23 = (x4 | x22);
+  { uint64_t x24 = (x2 | x23);
+  out[0] = x24;
+  }}}}}}}}}}}}}}}}}
+}
