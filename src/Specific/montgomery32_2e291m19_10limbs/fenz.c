2cc18a183ddae94bc51c1464a674bb822e2b0619
Jason Gross <jgross@mit.edu>
2017-11-14 00:21:38 -0500

    Update display logs and c files

---
+++ src/Specific/montgomery32_2e291m19_10limbs/fenz.c
@@ -0,0 +1,23 @@
+static void fenz(ReturnType uint32_t out[1], const uint32_t in1[10]) {
+  { const uint32_t x17 = in1[9];
+  { const uint32_t x18 = in1[8];
+  { const uint32_t x16 = in1[7];
+  { const uint32_t x14 = in1[6];
+  { const uint32_t x12 = in1[5];
+  { const uint32_t x10 = in1[4];
+  { const uint32_t x8 = in1[3];
+  { const uint32_t x6 = in1[2];
+  { const uint32_t x4 = in1[1];
+  { const uint32_t x2 = in1[0];
+  { uint32_t x19 = (x18 | x17);
+  { uint32_t x20 = (x16 | x19);
+  { uint32_t x21 = (x14 | x20);
+  { uint32_t x22 = (x12 | x21);
+  { uint32_t x23 = (x10 | x22);
+  { uint32_t x24 = (x8 | x23);
+  { uint32_t x25 = (x6 | x24);
+  { uint32_t x26 = (x4 | x25);
+  { uint32_t x27 = (x2 | x26);
+  out[0] = x27;
+  }}}}}}}}}}}}}}}}}}}
+}
---
+++ src/Specific/solinas64_2e521m1_10limbs/feadd.c
@@ -0,0 +1,33 @@
+static void feadd(uint64_t out[10], const uint64_t in1[10], const uint64_t in2[10]) {
+  { const uint64_t x20 = in1[9];
+  { const uint64_t x21 = in1[8];
+  { const uint64_t x19 = in1[7];
+  { const uint64_t x17 = in1[6];
+  { const uint64_t x15 = in1[5];
+  { const uint64_t x13 = in1[4];
+  { const uint64_t x11 = in1[3];
+  { const uint64_t x9 = in1[2];
+  { const uint64_t x7 = in1[1];
+  { const uint64_t x5 = in1[0];
+  { const uint64_t x38 = in2[9];
+  { const uint64_t x39 = in2[8];
+  { const uint64_t x37 = in2[7];
+  { const uint64_t x35 = in2[6];
+  { const uint64_t x33 = in2[5];
+  { const uint64_t x31 = in2[4];
+  { const uint64_t x29 = in2[3];
+  { const uint64_t x27 = in2[2];
+  { const uint64_t x25 = in2[1];
+  { const uint64_t x23 = in2[0];
+  out[0] = (x5 + x23);
+  out[1] = (x7 + x25);
+  out[2] = (x9 + x27);
+  out[3] = (x11 + x29);
+  out[4] = (x13 + x31);
+  out[5] = (x15 + x33);
+  out[6] = (x17 + x35);
+  out[7] = (x19 + x37);
+  out[8] = (x21 + x39);
+  out[9] = (x20 + x38);
+  }}}}}}}}}}}}}}}}}}}}
+}
