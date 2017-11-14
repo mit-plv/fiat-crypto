1b1211897b79e0b59ac5c669680bfd3971fee1fc
Jason Gross <jgross@mit.edu>
2017-11-14 09:41:48 -0500

    Update display logs and c files

---
+++ src/Specific/montgomery32_2e448m2e224m1_14limbs/fenz.c
@@ -0,0 +1,31 @@
+static void fenz(ReturnType uint32_t out[1], const uint32_t in1[14]) {
+  { const uint32_t x25 = in1[13];
+  { const uint32_t x26 = in1[12];
+  { const uint32_t x24 = in1[11];
+  { const uint32_t x22 = in1[10];
+  { const uint32_t x20 = in1[9];
+  { const uint32_t x18 = in1[8];
+  { const uint32_t x16 = in1[7];
+  { const uint32_t x14 = in1[6];
+  { const uint32_t x12 = in1[5];
+  { const uint32_t x10 = in1[4];
+  { const uint32_t x8 = in1[3];
+  { const uint32_t x6 = in1[2];
+  { const uint32_t x4 = in1[1];
+  { const uint32_t x2 = in1[0];
+  { uint32_t x27 = (x26 | x25);
+  { uint32_t x28 = (x24 | x27);
+  { uint32_t x29 = (x22 | x28);
+  { uint32_t x30 = (x20 | x29);
+  { uint32_t x31 = (x18 | x30);
+  { uint32_t x32 = (x16 | x31);
+  { uint32_t x33 = (x14 | x32);
+  { uint32_t x34 = (x12 | x33);
+  { uint32_t x35 = (x10 | x34);
+  { uint32_t x36 = (x8 | x35);
+  { uint32_t x37 = (x6 | x36);
+  { uint32_t x38 = (x4 | x37);
+  { uint32_t x39 = (x2 | x38);
+  out[0] = x39;
+  }}}}}}}}}}}}}}}}}}}}}}}}}}}
+}
---
+++ src/Specific/montgomery32_2e468m17_15limbs/fenz.c
@@ -0,0 +1,33 @@
+static void fenz(ReturnType uint32_t out[1], const uint32_t in1[15]) {
+  { const uint32_t x27 = in1[14];
+  { const uint32_t x28 = in1[13];
+  { const uint32_t x26 = in1[12];
+  { const uint32_t x24 = in1[11];
+  { const uint32_t x22 = in1[10];
+  { const uint32_t x20 = in1[9];
+  { const uint32_t x18 = in1[8];
+  { const uint32_t x16 = in1[7];
+  { const uint32_t x14 = in1[6];
+  { const uint32_t x12 = in1[5];
+  { const uint32_t x10 = in1[4];
+  { const uint32_t x8 = in1[3];
+  { const uint32_t x6 = in1[2];
+  { const uint32_t x4 = in1[1];
+  { const uint32_t x2 = in1[0];
+  { uint32_t x29 = (x28 | x27);
+  { uint32_t x30 = (x26 | x29);
+  { uint32_t x31 = (x24 | x30);
+  { uint32_t x32 = (x22 | x31);
+  { uint32_t x33 = (x20 | x32);
+  { uint32_t x34 = (x18 | x33);
+  { uint32_t x35 = (x16 | x34);
+  { uint32_t x36 = (x14 | x35);
+  { uint32_t x37 = (x12 | x36);
+  { uint32_t x38 = (x10 | x37);
+  { uint32_t x39 = (x8 | x38);
+  { uint32_t x40 = (x6 | x39);
+  { uint32_t x41 = (x4 | x40);
+  { uint32_t x42 = (x2 | x41);
+  out[0] = x42;
+  }}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+}
