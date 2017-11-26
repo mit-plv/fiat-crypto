6c7913c216f117d2069400d6dc81cbed407007e2
Jason Gross <jgross@mit.edu>
2017-11-26 12:27:12 -0500

    Add Tactics.RunTacticAsConstr

---
+++ src/Util/Tactics/RunTacticAsConstr.v
@@ -0,0 +1,23 @@
+Require Export Crypto.Util.FixCoqMistakes.
+
+(** Runs a tactic during expression evaluation phase, returns the constr [I] *)
+Ltac run_tactic tac :=
+  let dummy := match goal with _ => tac () end in
+  I.
+
+(** Runs a tactic during expression evaluation phase, returning [true] on success and [false] on failure. *)
+Ltac run_tactic_as_bool tac :=
+  match goal with
+  | _ => let dummy := run_tactic tac in
+         true
+  | _ => false
+  end.
+
+(** Runs a tactic during expression evaluation phase; on success,
+    returns [then_tac ()], otherwise returns [else_tac ()] *)
+Ltac constr_tryif_then_else tac then_tac else_tac :=
+  let success := run_tactic_as_bool tac in
+  lazymatch success with
+  | true => then_tac ()
+  | false => else_tac ()
+  end.
