fd02de94fdbd176dc6b7f731e357c0d0d96014a6
Jason Gross <jagro@google.com>
2018-08-29 17:17:26 -0400

    Add do_with_exactly_one_hyp

--- src/Util/Tactics/DoWithHyp.v
+++ src/Util/Tactics/DoWithHyp.v
@@ -1,7 +1,18 @@
+Require Import Crypto.Util.Tactics.Not.
 Require Export Crypto.Util.FixCoqMistakes.
 
 (** Do something with every hypothesis. *)
 Ltac do_with_hyp' tac :=
   match goal with
     | [ H : _ |- _ ] => tac H
+  end.
+
+(** Do something with any hypothesis, as long as there is only one hypothesis that it works with *)
+Ltac do_with_exactly_one_hyp tac :=
+  match goal with
+  | [ H : _ |- _ ]
+    => not (idtac; match goal with
+                  | [ H' : _ |- _ ] => tryif constr_eq H H' then fail else tac H'
+                  end);
+      tac H
   end.
