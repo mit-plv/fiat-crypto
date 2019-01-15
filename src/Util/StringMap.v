3c99dc494a807354a48e5a2f0f78f7979706cb70
Jason Gross <jgross@mit.edu>
2019-01-15 15:07:04 -0500

    Add StringMap

---
+++ src/Util/StringMap.v
@@ -0,0 +1,7 @@
+Require Import Coq.FSets.FMapInterface.
+Require Import Coq.FSets.FMapList.
+Require Import Coq.Structures.OrderedType.
+Require Import Coq.Strings.String.
+Require Import Crypto.Util.Strings.String_as_OT.
+
+Module StringMap := FMapList.Make String_as_OT.
