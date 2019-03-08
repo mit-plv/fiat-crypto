295f86cd4dccaa3a5617f8ac3b4f2dd02b9cfedd
Jason Gross <jgross@mit.edu>
2019-03-08 01:47:40 -0500

    Fix grepeat tactic
    It previously required progress on all goals, rather than any goal

--- src/Util/Tactics/GlobalTacticals.v
+++ src/Util/Tactics/GlobalTacticals.v
@@ -4,5 +4,5 @@ Ltac gprogress tac :=
 Tactic Notation "gprogress" tactic3(tac) := gprogress tac.
 Ltac gtry tac := tac + idtac.
 Tactic Notation "gtry" tactic3(tac) := gtry tac.
-Ltac grepeat tac := gtry (gprogress tac; grepeat tac).
+Ltac grepeat tac := gtry (gprogress (gtry tac); grepeat tac).
 Tactic Notation "grepeat" tactic3(tac) := grepeat tac.
