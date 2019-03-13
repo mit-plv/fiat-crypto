b7b161054e11625ffdaa5c0c40af3779d3d3511f
Jason Gross <jgross@mit.edu>
2019-03-12 23:14:16 -0400

    Add some montgomery64 .c / .log files

---
+++ src/Specific/montgomery64_2e448m2e224m1_7limbs/fesub.c
@@ -0,0 +1,46 @@
+static void fesub(uint64_t out[7], const uint64_t in1[7], const uint64_t in2[7]) {
+  { const uint64_t x14 = in1[6];
+  { const uint64_t x15 = in1[5];
+  { const uint64_t x13 = in1[4];
+  { const uint64_t x11 = in1[3];
+  { const uint64_t x9 = in1[2];
+  { const uint64_t x7 = in1[1];
+  { const uint64_t x5 = in1[0];
+  { const uint64_t x26 = in2[6];
+  { const uint64_t x27 = in2[5];
+  { const uint64_t x25 = in2[4];
+  { const uint64_t x23 = in2[3];
+  { const uint64_t x21 = in2[2];
+  { const uint64_t x19 = in2[1];
+  { const uint64_t x17 = in2[0];
+  { uint64_t x29; uint8_t/*bool*/ x30 = _subborrow_u64(0x0, x5, x17, &x29);
+  { uint64_t x32; uint8_t/*bool*/ x33 = _subborrow_u64(x30, x7, x19, &x32);
+  { uint64_t x35; uint8_t/*bool*/ x36 = _subborrow_u64(x33, x9, x21, &x35);
+  { uint64_t x38; uint8_t/*bool*/ x39 = _subborrow_u64(x36, x11, x23, &x38);
+  { uint64_t x41; uint8_t/*bool*/ x42 = _subborrow_u64(x39, x13, x25, &x41);
+  { uint64_t x44; uint8_t/*bool*/ x45 = _subborrow_u64(x42, x15, x27, &x44);
+  { uint64_t x47; uint8_t/*bool*/ x48 = _subborrow_u64(x45, x14, x26, &x47);
+  { uint64_t x49 = cmovznz64(x48, 0x0, 0xffffffffffffffffL);
+  { uint64_t x50 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x52; uint8_t/*bool*/ x53 = _addcarryx_u64(0x0, x29, x50, &x52);
+  { uint64_t x54 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x56; uint8_t/*bool*/ x57 = _addcarryx_u64(x53, x32, x54, &x56);
+  { uint64_t x58 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x60; uint8_t/*bool*/ x61 = _addcarryx_u64(x57, x35, x58, &x60);
+  { uint64_t x62 = (x49 & 0xfffffffeffffffffL);
+  { uint64_t x64; uint8_t/*bool*/ x65 = _addcarryx_u64(x61, x38, x62, &x64);
+  { uint64_t x66 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x68; uint8_t/*bool*/ x69 = _addcarryx_u64(x65, x41, x66, &x68);
+  { uint64_t x70 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x72; uint8_t/*bool*/ x73 = _addcarryx_u64(x69, x44, x70, &x72);
+  { uint64_t x74 = (x49 & 0xffffffffffffffffL);
+  { uint64_t x76; uint8_t/*bool*/ _ = _addcarryx_u64(x73, x47, x74, &x76);
+  out[0] = x52;
+  out[1] = x56;
+  out[2] = x60;
+  out[3] = x64;
+  out[4] = x68;
+  out[5] = x72;
+  out[6] = x76;
+  }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+}
