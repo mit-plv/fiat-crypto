5733ac962aa0c6ccf65227ebab30d1c32dc76308
jadep <jadep@mit.edu>
2019-10-04 15:45:47 -0400

    [WIP] mul split admits working, proof still messy

--- src/Util/ZUtil/Tactics/ZeroBounds.v
+++ src/Util/ZUtil/Tactics/ZeroBounds.v
@@ -13,8 +13,13 @@ Module Z.
     | [ |- 0 <= _ * _] => apply Z.mul_nonneg_nonneg
     | [ |- 0 <= _ / _] => apply Z.div_nonneg
     | [ |- 0 <= _ ^ _ ] => apply Z.pow_nonneg
+    | [ |- 0 <= Z.shiftl _ _] => apply Z.shiftl_nonneg
     | [ |- 0 <= Z.shiftr _ _] => apply Z.shiftr_nonneg
     | [ |- 0 <= _ mod _] => apply Z.mod_pos_bound
+    | [ |- 0 <= Z.land _ _] => apply Z.land_nonneg;
+                               first [ left; solve [zero_bounds'']
+                                     | right; solve [zero_bounds'']]
+    | [ |- 0 <= Z.lor _ _] => apply Z.lor_nonneg; split; zero_bounds''
     | [ |- 0 < _ + _] => try solve [apply Z.add_pos_nonneg; zero_bounds''];
                          try solve [apply Z.add_nonneg_pos; zero_bounds'']
     | [ |- 0 < _ - _] => apply Z.lt_0_sub
