5fc33456fc77c1590566ed9f9865da55e679cab1
Jason Gross <jgross@mit.edu>
2020-02-07 14:15:12 -0500

    Switch to GitHub actions

---
+++ etc/ci/github-actions-make.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [ ! -z "${TOUCH}" ]; then
+    make -j2 -t ${TOUCH} || exit $?
+fi
+
+rm -f finished.ok
+(make "$@" TIMED=1 2>&1 && touch finished.ok) | tee -a time-of-build.log
+python "./etc/coq-scripts/timing/make-one-time-file.py" "time-of-build.log" "time-of-build-pretty.log" || exit $?
+
+git update-index --assume-unchanged _CoqProject
+git status
+git diff
+
+cat time-of-build-pretty.log
+make "$@" TIMED=1 || exit $?
+
+if [ ! -z "$(git diff)" ]; then
+    git submodule foreach --recursive git diff
+    git submodule foreach --recursive git status
+    git diff
+    exit 1
+fi
