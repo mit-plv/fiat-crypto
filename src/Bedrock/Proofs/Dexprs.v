4edaabbaf95125bea34e1341c76db5fa48eac058
jadep <jadep@mit.edu>
2020-02-16 19:18:22 -0500

    fix indentation in Dexprs.v and remove unused lemma

--- src/Bedrock/Proofs/Dexprs.v
+++ src/Bedrock/Proofs/Dexprs.v
@@ -10,102 +10,78 @@ Import ListNotations.
 (* TODO: some of these may be unused *)
 Section Dexprs.
   Context {p : Semantics.parameters} {ok : @Semantics.parameters_ok p}.
-    Lemma expr_comm m l x1 :
-      forall x2 post,
-        WeakestPrecondition.expr
-          m l x1 (fun y : Semantics.word => WeakestPrecondition.expr m l x2 (post y)) ->
-        WeakestPrecondition.expr
-          m l x2 (fun y : Semantics.word => WeakestPrecondition.expr m l x1 (fun y2 => post y2 y)).
-    Proof.
-      induction x1;
-        cbn [WeakestPrecondition.expr WeakestPrecondition.expr_body]; intros;
-          repeat match goal with
-                 | _ => progress cbv [WeakestPrecondition.literal dlet.dlet] in *
-                 | _ => solve [eauto]
-                 | H: WeakestPrecondition.get _ _ _ |- _ =>
-                   inversion H; destruct_head'_and; clear H
-                 | H: WeakestPrecondition.load _ _ _ _ |- _ =>
-                   inversion H; destruct_head'_and; clear H
-                 | |- WeakestPrecondition.get _ _ _ =>
-                   eexists; split; [ eassumption | ]
-                 | |- WeakestPrecondition.load _ _ _ _ =>
-                   eexists; split; [ eassumption | ]
-                 | IH : _ |- WeakestPrecondition.expr _ _ _ _ => eapply IH
-                 | |- WeakestPrecondition.expr _ _ _ _ => propers_step
-                 end.
-    Qed.
 
-    Lemma dexprs_cons_iff m l :
-      forall e es v vs,
-        WeakestPrecondition.dexprs m l (e :: es) (v :: vs) <->
-        (WeakestPrecondition.expr m l e (eq v)
-         /\ WeakestPrecondition.dexprs m l es vs).
-    Proof.
-      cbv [WeakestPrecondition.dexprs].
-      induction es; split; intros;
-        repeat match goal with
-               | _ => progress cbn [WeakestPrecondition.list_map
-                                      WeakestPrecondition.list_map_body] in *
-               | _ => progress cbv beta in *
-               | H : _ :: _ = _ :: _ |- _ => inversion H; clear H; subst
-               | |- _ /\ _ => split
-               | _ => progress destruct_head'_and
-               | _ => reflexivity
-               | _ => solve [propers]
-               | _ => peel_expr
-               end.
-      eapply IHes with (vs := tl vs).
-      propers_step. peel_expr.
-      destruct vs; cbn [tl]; propers.
-    Qed.
+  Lemma dexprs_cons_iff m l :
+    forall e es v vs,
+      WeakestPrecondition.dexprs m l (e :: es) (v :: vs) <->
+      (WeakestPrecondition.expr m l e (eq v)
+       /\ WeakestPrecondition.dexprs m l es vs).
+  Proof.
+    cbv [WeakestPrecondition.dexprs].
+    induction es; split; intros;
+      repeat match goal with
+             | _ => progress cbn [WeakestPrecondition.list_map
+                                    WeakestPrecondition.list_map_body] in *
+             | _ => progress cbv beta in *
+             | H : _ :: _ = _ :: _ |- _ => inversion H; clear H; subst
+             | |- _ /\ _ => split
+             | _ => progress destruct_head'_and
+             | _ => reflexivity
+             | _ => solve [propers]
+             | _ => peel_expr
+             end.
+    eapply IHes with (vs := tl vs).
+    propers_step. peel_expr.
+    destruct vs; cbn [tl]; propers.
+  Qed.
 
-    Lemma dexprs_cons_nil m l e es :
-      WeakestPrecondition.dexprs m l (e :: es) [] -> False.
-    Proof.
-      cbv [WeakestPrecondition.dexprs].
-      induction es; intros;
-        repeat match goal with
-               | _ => progress cbn [WeakestPrecondition.list_map
-                                      WeakestPrecondition.list_map_body] in *
-               | _ => congruence 
-               | _ => solve [propers]
-               | _ => apply IHes
-               | _ => peel_expr
-               end.
-      propers_step. peel_expr. propers.
-    Qed.
+  Lemma dexprs_cons_nil m l e es :
+    WeakestPrecondition.dexprs m l (e :: es) [] -> False.
+  Proof.
+    cbv [WeakestPrecondition.dexprs].
+    induction es; intros;
+      repeat match goal with
+             | _ => progress cbn [WeakestPrecondition.list_map
+                                    WeakestPrecondition.list_map_body] in *
+             | _ => congruence
+             | _ => solve [propers]
+             | _ => apply IHes
+             | _ => peel_expr
+             end.
+    propers_step. peel_expr. propers.
+  Qed.
 
-    Lemma dexprs_app_l m l es1 :
-      forall es2 vs,
-        WeakestPrecondition.dexprs m l (es1 ++ es2) vs ->
-        (WeakestPrecondition.dexprs m l es1 (firstn (length es1) vs)) /\
-        (WeakestPrecondition.dexprs m l es2 (skipn (length es1) vs)).
-    Proof.
-      induction es1; intros.
-      { cbn [Datatypes.length skipn firstn]; rewrite app_nil_l in *.
-        split; eauto; reflexivity. }
-      { destruct vs; rewrite <-app_comm_cons in *;
-          [ match goal with H : _ |- _ => apply dexprs_cons_nil in H; tauto end | ].
-        cbn [Datatypes.length skipn firstn].
-        rewrite !dexprs_cons_iff in *.
-        destruct_head'_and.
-        repeat split; try eapply IHes1; eauto. }
-    Qed.
+  Lemma dexprs_app_l m l es1 :
+    forall es2 vs,
+      WeakestPrecondition.dexprs m l (es1 ++ es2) vs ->
+      (WeakestPrecondition.dexprs m l es1 (firstn (length es1) vs)) /\
+      (WeakestPrecondition.dexprs m l es2 (skipn (length es1) vs)).
+  Proof.
+    induction es1; intros.
+    { cbn [Datatypes.length skipn firstn]; rewrite app_nil_l in *.
+      split; eauto; reflexivity. }
+    { destruct vs; rewrite <-app_comm_cons in *;
+        [ match goal with H : _ |- _ => apply dexprs_cons_nil in H; tauto end | ].
+      cbn [Datatypes.length skipn firstn].
+      rewrite !dexprs_cons_iff in *.
+      destruct_head'_and.
+      repeat split; try eapply IHes1; eauto. }
+  Qed.
 
-    Lemma dexprs_length m l :
-      forall vs es,
-        WeakestPrecondition.dexprs m l es vs ->
-        length es = length vs.
-    Proof.
-      induction vs; destruct es; intros;
-        repeat match goal with
-               | _ => progress cbn [Datatypes.length]
-               | _ => progress destruct_head'_and
-               | H : _ |- _ => apply dexprs_cons_nil in H; tauto
-               | H : _ |- _ => apply dexprs_cons_iff in H
-               | _ => reflexivity
-               | _ => congruence
-               end.
-      rewrite IHvs; auto.
-    Qed.
+  Lemma dexprs_length m l :
+    forall vs es,
+      WeakestPrecondition.dexprs m l es vs ->
+      length es = length vs.
+  Proof.
+    induction vs; destruct es; intros;
+      repeat match goal with
+             | _ => progress cbn [Datatypes.length]
+             | _ => progress destruct_head'_and
+             | H : _ |- _ => apply dexprs_cons_nil in H; tauto
+             | H : _ |- _ => apply dexprs_cons_iff in H
+             | _ => reflexivity
+             | _ => congruence
+             end.
+    rewrite IHvs; auto.
+  Qed.
 End Dexprs.
