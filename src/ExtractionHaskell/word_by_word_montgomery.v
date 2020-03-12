6778906994b1d8c796d72c823f1e6fdf787ef1b7
Jason Gross <jgross@mit.edu>
2020-03-12 15:05:51 -0400

    Fix a typo in a comment

--- src/ExtractionHaskell/word_by_word_montgomery.v
+++ src/ExtractionHaskell/word_by_word_montgomery.v
@@ -1,4 +1,4 @@
 Require Import Crypto.StandaloneHaskellMain.
 
 (*Redirect "/tmp/word_by_word_montgomery.hs" *)Recursive Extraction WordByWordMontgomery.main.
-(* cat /tmp/solinas.hs.out | sed -f haskell.sed  > ../../solinas.hs *)
+(* cat /tmp/word_by_word_montgomery.hs.out | sed -f haskell.sed  > ../../word_by_word_montgomery.hs *)
