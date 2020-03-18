c83b0084d902b12d3bddd59588cf95b89935e692
Jason Gross <jgross@mit.edu>
2020-03-18 13:49:47 -0400

    Also tocuh-up build outputs on windows CI

--- etc/ci/github-actions-make.sh
+++ etc/ci/github-actions-make.sh
@@ -17,6 +17,12 @@ git diff
 cat time-of-build-pretty.log
 make "$@" TIMED=1 || exit $?
 
+unameOut="$(uname -s)"
+if [[ "${unameOut}" == CYGWIN* ]]; then
+    # generated build outputs have a different path, so we fix up the paths
+    git grep --name-only 'D:\\a\\fiat-crypto\\fiat-crypto.src.ExtractionOCaml.' | xargs sed s',D:\\a\\fiat-crypto\\fiat-crypto.src.ExtractionOCaml.\(.*\).exe,src/ExtractionOCaml/\1,g' -i
+fi
+
 if [ ! -z "$(git diff)" ]; then
     git submodule foreach --recursive git diff
     git submodule foreach --recursive git status
