93ce4258ddf9c6d960fb3a9f7a3bc357a2082718
Jason Gross <jgross@mit.edu>
2020-03-22 15:49:16 -0400

    Add Crypto.Util.MSets.Sum
    This, combined with MSets.Iso, will let us make an MSet based on
    MSetPositive for int.type in the stringification machinery.

---
+++ src/Util/Structures/Equalities/Sum.v
@@ -0,0 +1,132 @@
+Require Import Coq.Classes.Morphisms Coq.Setoids.Setoid.
+Require Import Coq.Structures.Equalities.
+Require Import Crypto.Util.Sum.
+
+Local Set Implicit Arguments.
+
+Module SumTyp (E1 : Typ) (E2 : Typ) <: Typ.
+  Definition t := (E1.t + E2.t)%type.
+End SumTyp.
+
+Module SumHasEq (E1 : Eq) (E2 : Eq).
+  Definition eq : relation (E1.t + E2.t) := sumwise E1.eq E2.eq.
+End SumHasEq.
+
+Module SumIsEq (E1 : EqualityType) (E2 : EqualityType).
+  Local Notation eq := (sumwise E1.eq E2.eq).
+  Global Instance eq_equiv : Equivalence eq | 5.
+  Proof.
+    constructor; exact _.
+  Defined.
+End SumIsEq.
+
+Module SumHasEqDec (E1 : Equalities.DecidableType) (E2 : Equalities.DecidableType).
+  Local Notation eq := (sumwise E1.eq E2.eq).
+  Definition eq_dec x y : {eq x y} + {~eq x y}.
+  Proof.
+    pose proof E1.eq_dec; pose proof E2.eq_dec.
+    unshelve eapply dec_sumwise; eauto.
+  Defined.
+End SumHasEqDec.
+
+Module SumHasEqb (E1 : Typ) (E2 : Typ) (E1b : HasEqb E1) (E2b : HasEqb E2).
+  Definition eqb := sum_beq _ _ E1b.eqb E2b.eqb.
+End SumHasEqb.
+
+Module SumEqbNotation (E1 : Typ) (E2 : Typ) (E1s : HasEqb E1) (E2s : HasEqb E2).
+  Module Import _SumEqbNotation.
+    Module T := SumTyp E1 E2.
+    Module E := SumHasEqb E1 E2 E1s E2s.
+  End _SumEqbNotation.
+  Include EqbNotation T E.
+End SumEqbNotation.
+
+Module SumEqbSpec (E1 : Typ) (E2 : Typ) (E1e : HasEq E1) (E2e : HasEq E2) (E1b : HasEqb E1) (E2b : HasEqb E2) (E1s : EqbSpec E1 E1e E1b) (E2s : EqbSpec E2 E2e E2b).
+  Local Notation eq := (sumwise E1e.eq E2e.eq).
+  Local Notation eqb := (sum_beq _ _ E1b.eqb E2b.eqb).
+  Lemma eqb_eq x y : eqb x y = true <-> eq x y.
+  Proof.
+    destruct x as [x|x], y as [y|y]; cbv [sumwise sum_beq];
+      try apply E1s.eqb_eq;
+      try apply E2s.eqb_eq;
+      intuition congruence.
+  Qed.
+End SumEqbSpec.
+
+Module SumHasEqBool (E1 : Eq) (E2 : Eq) (E1s : HasEqBool E1) (E2s : HasEqBool E2) := SumHasEqb E1 E2 E1s E2s <+ SumEqbSpec E1 E2 E1 E2 E1s E2s E1s E2s.
+
+Module SumUsualHasEqDec (E1 : UsualDecidableType) (E2 : UsualDecidableType).
+  Definition eq_dec (x y : E1.t + E2.t) : {eq x y} + {~eq x y}.
+  Proof.
+    destruct x as [x|x], y as [y|y];
+      [ destruct (E1.eq_dec x y); [ left | right ]
+      | right
+      | right
+      | destruct (E2.eq_dec x y); [ left | right ] ];
+      repeat first [ apply f_equal
+                   | assumption
+                   | intro
+                   | progress inversion_sum
+                   | solve [ auto with nocore ] ].
+  Defined.
+End SumUsualHasEqDec.
+
+Module SumUsualEqbSpec (E1 : UsualBoolEq) (E2 : UsualBoolEq).
+  Lemma eqb_eq x y : @sum_beq _ _ E1.eqb E2.eqb x y = true <-> eq x y.
+  Proof.
+    destruct x as [x|x], y as [y|y]; cbn;
+      rewrite ?E1.eqb_eq, ?E2.eqb_eq;
+      intuition congruence.
+  Qed.
+End SumUsualEqbSpec.
+
+Module SumHasUsualEq (E1 : UsualEq) (E2 : UsualEq).
+  Module E := SumTyp E1 E2.
+  Include HasUsualEq E.
+End SumHasUsualEq.
+
+Module SumUsualEq (E1 : UsualEq) (E2 : UsualEq) <: UsualEq := SumTyp E1 E2 <+ HasUsualEq.
+
+Module SumUsualIsEq (E1 : UsualEq) (E2 : UsualEq).
+  Module E := SumUsualEq E1 E2.
+  Include UsualIsEq E.
+End SumUsualIsEq.
+
+Module SumUsualIsEqOrig (E1 : UsualEq) (E2 : UsualEq).
+  Module E := SumUsualEq E1 E2.
+  Include UsualIsEqOrig E.
+End SumUsualIsEqOrig.
+
+Module SumMiniDecidableType (E1 : MiniDecidableType) (E2 : MiniDecidableType) <: MiniDecidableType.
+  Include SumTyp E1 E2.
+  Module E1' := Make_UDT E1.
+  Module E2' := Make_UDT E2.
+  Include SumUsualHasEqDec E1' E2'.
+End SumMiniDecidableType.
+
+Module SumUsualHasEqBool (E1 : UsualBoolEq) (E2 : UsualBoolEq) := SumHasEqb E1 E2 E1 E2 <+ SumUsualEqbSpec E1 E2.
+
+Module SumEq (E1 : Eq) (E2 : Eq) <: Eq := SumTyp E1 E2 <+ SumHasEq E1 E2.
+Module SumEqualityType (E1 : EqualityType) (E2 : EqualityType) <: EqualityType := SumEq E1 E2 <+ SumIsEq E1 E2.
+Module SumDecidableType (E1 : DecidableType) (E2 : DecidableType) <: EqualityType := SumEqualityType E1 E2 <+ SumHasEqDec E1 E2.
+Module SumBooleanEqualityType (E1 : BooleanEqualityType) (E2 : BooleanEqualityType) <: BooleanEqualityType := SumEqualityType E1 E2 <+ SumHasEqb E1 E2 E1 E2 <+ SumEqbSpec E1 E2 E1 E2 E1 E2 E1 E2.
+Module SumBooleanDecidableType (E1 : BooleanDecidableType) (E2 : BooleanDecidableType) <: BooleanDecidableType := SumBooleanEqualityType E1 E2 <+ SumHasEqDec E1 E2.
+
+Module SumEq' (E1 : Eq) (E2 : Eq) := SumEq E1 E2 <+ EqNotation.
+Module SumEqualityType' (E1 : EqualityType) (E2 : EqualityType) := SumEqualityType E1 E2 <+ EqNotation.
+Module SumDecidableType' (E1 : DecidableType) (E2 : DecidableType) := SumDecidableType E1 E2 <+ EqNotation.
+Module SumBooleanEqualityType' (E1 : BooleanEqualityType) (E2 : BooleanEqualityType) := SumBooleanEqualityType E1 E2 <+ EqNotation <+ EqbNotation.
+Module SumBooleanDecidableType' (E1 : BooleanDecidableType) (E2 : BooleanDecidableType) := SumBooleanDecidableType E1 E2 <+ EqNotation <+ EqbNotation.
+
+Module SumUsualEqualityType (E1 : UsualEqualityType) (E2 : UsualEqualityType) <: UsualEqualityType := SumUsualEq E1 E2 <+ UsualIsEq.
+
+Module SumUsualDecidableType (E1 : UsualDecidableType) (E2 : UsualDecidableType) <: UsualDecidableType
+:= SumUsualEq E1 E2 <+ UsualIsEq <+ SumUsualHasEqDec E1 E2.
+Module SumUsualDecidableTypeOrig (E1 : UsualDecidableType) (E2 : UsualDecidableType) <: UsualDecidableTypeOrig
+:= SumUsualEq E1 E2 <+ UsualIsEqOrig <+ SumUsualHasEqDec E1 E2.
+Module SumUsualDecidableTypeBoth (E1 : UsualDecidableType) (E2 : UsualDecidableType) <: UsualDecidableTypeBoth
+ := SumUsualDecidableType E1 E2 <+ UsualIsEqOrig.
+Module SumUsualBoolEq (E1 : UsualBoolEq) (E2 : UsualBoolEq) <: UsualBoolEq
+:= SumUsualEq E1 E2 <+ SumUsualHasEqBool E1 E2.
+Module SumUsualDecidableTypeFull (E1 : UsualDecidableTypeFull) (E2 : UsualDecidableTypeFull) <: UsualDecidableTypeFull
+ := SumUsualEq E1 E2 <+ UsualIsEq <+ UsualIsEqOrig <+ SumUsualHasEqDec E1 E2 <+ SumUsualHasEqBool E1 E2.
---
+++ src/Util/Structures/Orders/Sum.v
@@ -0,0 +1,233 @@
+Require Import Coq.Classes.Morphisms Coq.Setoids.Setoid.
+Require Import Coq.Structures.Equalities.
+Require Import Coq.Structures.Orders.
+Require Import Crypto.Util.Structures.Equalities.Sum.
+Require Import Crypto.Util.Structures.Orders.
+Require Import Crypto.Util.Sum.
+Require Import Crypto.Util.Tactics.DestructHead.
+
+Local Set Implicit Arguments.
+
+Module SumHasLt (E1 : EqLt) (E2 : EqLt).
+  Definition lt := sum_le E1.lt E2.lt.
+End SumHasLt.
+
+Module SumHasLe (E1 : EqLe) (E2 : EqLe).
+  Definition le := sum_le E1.le E2.le.
+End SumHasLe.
+
+Module SumIsStrOrder (E1 : StrOrder) (E2 : StrOrder).
+  Global Instance lt_strorder : StrictOrder (sum_le E1.lt E2.lt) | 1 := _.
+  Global Instance lt_compat : Proper (sumwise E1.eq E2.eq==>sumwise E1.eq E2.eq==>iff) (sum_le E1.lt E2.lt) | 1 := _.
+End SumIsStrOrder.
+
+Module SumLeIsLtEq (E1 : EqLtLe') (E2 : EqLtLe') (E1s : LeIsLtEq E1) (E2s : LeIsLtEq E2).
+  Local Infix "<=" := (sum_le E1.le E2.le).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Local Infix "==" := (sumwise E1.eq E2.eq) (at level 70).
+  Lemma le_lteq : forall x y, x <= y <-> x < y \/ x == y.
+  Proof.
+    pose proof E1s.le_lteq.
+    pose proof E2s.le_lteq.
+    intros [x|x] [y|y]; cbn; firstorder eauto.
+  Qed.
+End SumLeIsLtEq.
+
+Module SumHasCmp (E1 : Typ) (E2 : Typ) (E1c : HasCmp E1) (E2c : HasCmp E2).
+  Definition compare := Sum.compare E1c.compare E2c.compare.
+End SumHasCmp.
+
+Module SumCmpNotation (E1 : Typ) (E2 : Typ) (E1c : HasCmp E1) (E2c : HasCmp E2).
+  Module Import _SumCmpNotation.
+    Module T := SumTyp E1 E2.
+    Module C := SumHasCmp E1 E2 E1c E2c.
+  End _SumCmpNotation.
+  Include CmpNotation T C.
+End SumCmpNotation.
+
+Module SumCmpSpec (E1 : EqLt) (E2 : EqLt) (E1c : HasCompare E1) (E2c : HasCompare E2).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Local Infix "==" := (sumwise E1.eq E2.eq) (at level 70).
+  Lemma compare_spec : forall x y, CompareSpec (x == y) (x < y) (y < x) (Sum.compare E1c.compare E2c.compare x y).
+  Proof.
+    intros [x|x] [y|y]; cbn; auto using E1c.compare_spec, E2c.compare_spec.
+  Qed.
+End SumCmpSpec.
+
+Module SumLtIsTotal (E1 : EqLt) (E2 : EqLt) (E1s : LtIsTotal E1) (E2s : LtIsTotal E2).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Local Infix "==" := (sumwise E1.eq E2.eq) (at level 70).
+  Lemma lt_total : forall x y, x < y \/ x == y \/ y < x.
+  Proof.
+    pose proof E1s.lt_total; pose proof E2s.lt_total.
+    intros [x|x] [y|y]; cbn; firstorder auto.
+  Qed.
+End SumLtIsTotal.
+
+Module SumHasLeb (E1 : Typ) (E2 : Typ) (E1s : HasLeb E1) (E2s : HasLeb E2).
+  Definition leb := sum_leb E1s.leb E2s.leb.
+End SumHasLeb.
+
+Module SumHasLtb (E1 : Typ) (E2 : Typ) (E1s : HasLtb E1) (E2s : HasLtb E2).
+  Definition ltb := sum_leb E1s.ltb E2s.ltb.
+End SumHasLtb.
+
+Module SumLebNotation (E1 : Typ) (E2 : Typ) (E1s : HasLeb E1) (E2s : HasLeb E2).
+  Module Import _SumLebNotation.
+    Module T := SumTyp E1 E2.
+    Module E := SumHasLeb E1 E2 E1s E2s.
+  End _SumLebNotation.
+  Include LebNotation T E.
+End SumLebNotation.
+
+Module SumLtbNotation (E1 : Typ) (E2 : Typ) (E1s : HasLtb E1) (E2s : HasLtb E2).
+  Module Import _SumLtbNotation.
+    Module T := SumTyp E1 E2.
+    Module E := SumHasLtb E1 E2 E1s E2s.
+  End _SumLtbNotation.
+  Include LtbNotation T E.
+End SumLtbNotation.
+
+Module SumLebSpec (E1 : Typ) (E2 : Typ) (E1Le : HasLe E1) (E2Le : HasLe E2) (E1Leb : HasLeb E1) (E2Leb : HasLeb E2) (E1s : LebSpec E1 E1Le E1Leb) (E2s : LebSpec E2 E2Le E2Leb).
+  Lemma leb_le : forall x y, sum_leb E1Leb.leb E2Leb.leb x y = true <-> sum_le E1Le.le E2Le.le x y.
+  Proof.
+    pose proof E1s.leb_le; pose proof E2s.leb_le.
+    intros [x|x] [y|y]; cbn; firstorder congruence.
+  Qed.
+End SumLebSpec.
+
+Module SumLtbSpec (E1 : Typ) (E2 : Typ) (E1Lt : HasLt E1) (E2Lt : HasLt E2) (E1Ltb : HasLtb E1) (E2Ltb : HasLtb E2) (E1s : LtbSpec E1 E1Lt E1Ltb) (E2s : LtbSpec E2 E2Lt E2Ltb).
+  Lemma ltb_lt : forall x y, sum_leb E1Ltb.ltb E2Ltb.ltb x y = true <-> sum_le E1Lt.lt E2Lt.lt x y.
+  Proof.
+    pose proof E1s.ltb_lt; pose proof E2s.ltb_lt.
+    intros [x|x] [y|y]; cbn; firstorder congruence.
+  Qed.
+End SumLtbSpec.
+
+Module SumLebIsTotal (E1 : TotalLeBool) (E2 : TotalLeBool).
+  Local Infix "<=?" := (sum_leb E1.leb E2.leb) (at level 70).
+  Lemma leb_total : forall x y, (x <=? y) = true \/ (y <=? x) = true.
+  Proof.
+    pose proof E1.leb_total; pose proof E2.leb_total.
+    intros [x|x] [y|y]; cbn; firstorder fail.
+  Qed.
+End SumLebIsTotal.
+
+Module SumLebIsTransitive (E1 : LeBool) (E2 : LeBool) (E1T : LebIsTransitive E1) (E2T : LebIsTransitive E2).
+  Local Coercion is_true : bool >-> Sortclass.
+  Lemma leb_trans : Transitive (sum_leb E1.leb E2.leb).
+  Proof.
+    pose proof E1T.leb_trans; pose proof E2T.leb_trans.
+    intros [x|x] [y|y] [z|z]; cbn; trivial; try congruence;
+      etransitivity; eassumption.
+  Qed.
+End SumLebIsTransitive.
+
+Module SumUsualIsStrOrder (E1 : UsualStrOrder) (E2 : UsualStrOrder).
+  Global Instance lt_strorder : StrictOrder (sum_le E1.lt E2.lt) | 1 := _.
+  Global Instance lt_compat : Proper (eq==>eq==>iff) (sum_le E1.lt E2.lt) | 1 := _.
+End SumUsualIsStrOrder.
+
+Module SumUsualLeIsLtEq (E1 : UsualEqLtLe) (E2 : UsualEqLtLe) (E1s : LeIsLtEq E1) (E2s : LeIsLtEq E2).
+  Local Infix "<=" := (sum_le E1.le E2.le).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Lemma le_lteq : forall x y, x <= y <-> x < y \/ x = y.
+  Proof.
+    intros [x|x] [y|y]; cbn;
+      [ pose proof (E1s.le_lteq x y)
+      |
+      |
+      | pose proof (E2s.le_lteq x y) ];
+      repeat first [ progress destruct_head' iff
+                   | progress destruct_head'_or
+                   | split
+                   | progress intros
+                   | progress inversion_sum
+                   | solve [ firstorder congruence ] ].
+  Qed.
+End SumUsualLeIsLtEq.
+
+Module SumUsualCmpSpec (E1 : UsualEqLt) (E2 : UsualEqLt) (E1c : HasCompare E1) (E2c : HasCompare E2).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Lemma compare_spec : forall x y, CompareSpec (x = y) (x < y) (y < x) (Sum.compare E1c.compare E2c.compare x y).
+  Proof.
+    intros [x|x] [y|y]; cbn;
+      [ destruct (E1c.compare_spec x y)
+      |
+      |
+      | destruct (E2c.compare_spec x y) ];
+      auto using f_equal.
+  Qed.
+End SumUsualCmpSpec.
+
+Module SumUsualLtIsTotal (E1 : UsualEqLt) (E2 : UsualEqLt) (E1s : LtIsTotal E1) (E2s : LtIsTotal E2).
+  Local Infix "<" := (sum_le E1.lt E2.lt).
+  Lemma lt_total : forall x y, x < y \/ x = y \/ y < x.
+  Proof.
+    pose proof E1s.lt_total; pose proof E2s.lt_total.
+    intros [x|x] [y|y]; cbn; firstorder auto using f_equal.
+  Qed.
+End SumUsualLtIsTotal.
+
+Module SumEqLt (E1 : EqLt) (E2 : EqLt) <: EqLt := SumEq E1 E2 <+ SumHasLt E1 E2.
+Module SumEqLe (E1 : EqLe) (E2 : EqLe) <: EqLe := SumEq E1 E2 <+ SumHasLe E1 E2.
+Module SumEqLtLe (E1 : EqLtLe) (E2 : EqLtLe) <: EqLtLe := SumEq E1 E2 <+ SumHasLt E1 E2 <+ SumHasLe E1 E2.
+Module SumEqLt' (E1 : EqLt) (E2 : EqLt) <: EqLt' := SumEq E1 E2 <+ SumHasLt E1 E2 <+ EqLtNotation.
+Module SumEqLe' (E1 : EqLe) (E2 : EqLe) <: EqLe' := SumEq E1 E2 <+ SumHasLe E1 E2 <+ EqLeNotation.
+Module SumEqLtLe' (E1 : EqLtLe) (E2 : EqLtLe) <: EqLtLe' := SumEq E1 E2 <+ SumHasLt E1 E2 <+ SumHasLe E1 E2 <+ EqLtLeNotation.
+Module SumStrOrder (E1 : StrOrder) (E2 : StrOrder) <: StrOrder := SumEqualityType E1 E2 <+ SumHasLt E1 E2 <+ SumIsStrOrder E1 E2.
+Module SumStrOrder' (E1 : StrOrder) (E2 : StrOrder) <: StrOrder' := SumStrOrder E1 E2 <+ EqLtNotation.
+Module SumHasCompare (E1 : EqLt) (E2 : EqLt) (E1c : HasCompare E1) (E2c : HasCompare E2) := SumHasCmp E1 E2 E1c E2c <+ SumCmpSpec E1 E2 E1c E2c.
+Module SumDecStrOrder (E1 : DecStrOrder) (E2 : DecStrOrder) <: DecStrOrder := SumStrOrder E1 E2 <+ SumHasCompare E1 E2 E1 E2.
+Module SumDecStrOrder' (E1 : DecStrOrder) (E2 : DecStrOrder) <: DecStrOrder' := SumDecStrOrder E1 E2 <+ EqLtNotation <+ CmpNotation.
+Module SumOrderedType (E1 : OrderedType) (E2 : OrderedType) <: OrderedType := SumDecStrOrder E1 E2 <+ SumHasEqDec E1 E2.
+Module SumOrderedType' (E1 : OrderedType') (E2 : OrderedType') <: OrderedType' := SumOrderedType E1 E2 <+ EqLtNotation <+ CmpNotation.
+Module SumOrderedTypeFull (E1 : OrderedTypeFull) (E2 : OrderedTypeFull) <: OrderedTypeFull := SumOrderedType E1 E2 <+ SumHasLe E1 E2 <+ SumLeIsLtEq E1 E2 E1 E2.
+Module SumOrderedTypeFull' (E1 : OrderedTypeFull') (E2 : OrderedTypeFull') <: OrderedTypeFull' := SumOrderedTypeFull E1 E2 <+ EqLtLeNotation <+ CmpNotation.
+
+Module SumUsualEqLt (E1 : UsualEqLt) (E2 : UsualEqLt) <: UsualEqLt := SumUsualEq E1 E2 <+ SumHasLt E1 E2.
+Module SumUsualEqLe (E1 : UsualEqLe) (E2 : UsualEqLe) <: UsualEqLe := SumUsualEq E1 E2 <+ SumHasLe E1 E2.
+Module SumUsualEqLtLe (E1 : UsualEqLtLe) (E2 : UsualEqLtLe) <: UsualEqLtLe := SumUsualEq E1 E2 <+ SumHasLt E1 E2 <+ SumHasLe E1 E2.
+
+Module SumUsualStrOrder (E1 : UsualStrOrder) (E2 : UsualStrOrder) <: UsualStrOrder := SumUsualEqualityType E1 E2 <+ SumHasLt E1 E2 <+ SumUsualIsStrOrder E1 E2.
+
+Module SumUsualHasCompare (E1 : UsualEqLt) (E2 : UsualEqLt) (E1c : HasCompare E1) (E2c : HasCompare E2) := SumHasCmp E1 E2 E1c E2c <+ SumUsualCmpSpec E1 E2 E1c E2c.
+
+Module SumUsualDecStrOrder (E1 : UsualDecStrOrder) (E2 : UsualDecStrOrder) <: UsualDecStrOrder := SumUsualStrOrder E1 E2 <+ SumUsualHasCompare E1 E2 E1 E2.
+Module SumUsualOrderedType (E1 : UsualOrderedType) (E2 : UsualOrderedType) <: UsualOrderedType := SumUsualDecStrOrder E1 E2 <+ SumUsualHasEqDec E1 E2.
+Module SumUsualOrderedTypeFull (E1 : UsualOrderedTypeFull) (E2 : UsualOrderedTypeFull) <: UsualOrderedTypeFull := SumUsualOrderedType E1 E2 <+ SumHasLe E1 E2 <+ SumUsualLeIsLtEq E1 E2 E1 E2.
+
+Module SumUsualStrOrder' (E1 : UsualStrOrder) (E2 : UsualStrOrder) <: UsualStrOrder' := SumUsualStrOrder E1 E2 <+ LtNotation.
+Module SumUsualDecStrOrder' (E1 : UsualDecStrOrder) (E2 : UsualDecStrOrder) <: UsualDecStrOrder' := SumUsualDecStrOrder E1 E2 <+ LtNotation.
+Module SumUsualOrderedType' (E1 : UsualOrderedType) (E2 : UsualOrderedType) <: UsualOrderedType' := SumUsualOrderedType E1 E2 <+ LtNotation.
+Module SumUsualOrderedTypeFull' (E1 : UsualOrderedTypeFull) (E2 : UsualOrderedTypeFull) <: UsualOrderedTypeFull' := SumUsualOrderedTypeFull E1 E2 <+ LtLeNotation.
+
+Module SumTotalOrder (E1 : TotalOrder) (E2 : TotalOrder) <: TotalOrder := SumStrOrder E1 E2 <+ SumHasLe E1 E2 <+ SumLeIsLtEq E1 E2 E1 E2 <+ SumLtIsTotal E1 E2 E1 E2.
+Module SumUsualTotalOrder (E1 : UsualTotalOrder) (E2 : UsualTotalOrder) <: UsualTotalOrder
+:= SumUsualStrOrder E1 E2 <+ SumHasLe E1 E2 <+ SumUsualLeIsLtEq E1 E2 E1 E2 <+ SumUsualLtIsTotal E1 E2 E1 E2.
+
+Module SumTotalOrder' (E1 : TotalOrder) (E2 : TotalOrder) <: TotalOrder' := SumTotalOrder E1 E2 <+ EqLtLeNotation.
+Module SumUsualTotalOrder' (E1 : UsualTotalOrder) (E2 : UsualTotalOrder) <: UsualTotalOrder' := SumUsualTotalOrder E1 E2 <+ LtLeNotation.
+
+Module SumLeBool (E1 : LeBool) (E2 : LeBool) <: LeBool := SumTyp E1 E2 <+ SumHasLeb E1 E2 E1 E2.
+Module SumLtBool (E1 : LtBool) (E2 : LtBool) <: LtBool := SumTyp E1 E2 <+ SumHasLtb E1 E2 E1 E2.
+Module SumLeBool' (E1 : LeBool) (E2 : LeBool) <: LeBool' := SumLeBool E1 E2 <+ LebNotation.
+Module SumLtBool' (E1 : LtBool) (E2 : LtBool) <: LtBool' := SumLtBool E1 E2 <+ LtbNotation.
+
+Module SumTotalLeBool (E1 : TotalLeBool) (E2 : TotalLeBool) <: TotalLeBool := SumLeBool E1 E2 <+ SumLebIsTotal E1 E2.
+Module SumTotalLeBool' (E1 : TotalLeBool) (E2 : TotalLeBool) <: TotalLeBool' := SumLeBool' E1 E2 <+ SumLebIsTotal E1 E2.
+
+Module SumTotalTransitiveLeBool (E1 : TotalTransitiveLeBool) (E2 : TotalTransitiveLeBool) <: TotalTransitiveLeBool := SumTotalLeBool E1 E2 <+ SumLebIsTransitive E1 E2 E1 E2.
+Module SumTotalTransitiveLeBool' (E1 : TotalTransitiveLeBool) (E2 : TotalTransitiveLeBool) <: TotalTransitiveLeBool' := SumTotalLeBool' E1 E2 <+ SumLebIsTransitive E1 E2 E1 E2.
+
+Module SumHasBoolOrdFuns (E1 : Typ) (E2 : Typ) (E1s : HasBoolOrdFuns E1) (E2s : HasBoolOrdFuns E2) := SumTyp E1 E2 <+ SumHasEqb E1 E2 E1s E2s <+ SumHasLtb E1 E2 E1s E2s <+ SumHasLeb E1 E2 E1s E2s.
+
+Module SumHasBoolOrdFuns' (E1 : Typ) (E2 : Typ) (E1s : HasBoolOrdFuns E1) (E2s : HasBoolOrdFuns E2) := SumHasBoolOrdFuns E1 E2 E1s E2s <+ SumEqbNotation E1 E2 E1s E2s <+ SumLtbNotation E1 E2 E1s E2s <+ SumLebNotation E1 E2 E1s E2s.
+
+Module SumBoolOrdSpecs (E1 : EqLtLe) (E2 : EqLtLe) (F1 : HasBoolOrdFuns E1) (F2 : HasBoolOrdFuns E2) (E1s : BoolOrdSpecs E1 F1) (E2s : BoolOrdSpecs E2 F2)
+:= SumEqbSpec E1 E2 E1 E2 F1 F2 E1s E2s <+ SumLtbSpec E1 E2 E1 E2 F1 F2 E1s E2s <+ SumLebSpec E1 E2 E1 E2 F1 F2 E1s E2s.
+
+Module SumOrderFunctions (E1 : EqLtLe) (E2 : EqLtLe) (E1f : OrderFunctions E1) (E2f : OrderFunctions E2)
+:= SumHasCompare E1 E2 E1f E2f <+ SumHasBoolOrdFuns E1 E2 E1f E2f <+ SumBoolOrdSpecs E1 E2 E1f E2f E1f E2f.
+Module SumOrderFunctions' (E1 : EqLtLe) (E2 : EqLtLe) (E1f : OrderFunctions E1) (E2f : OrderFunctions E2)
+:= SumHasCompare E1 E2 E1f E2f <+ SumCmpNotation E1 E2 E1f E2f <+ SumHasBoolOrdFuns' E1 E2 E1f E2f <+ SumBoolOrdSpecs E1 E2 E1f E2f E1f E2f.
