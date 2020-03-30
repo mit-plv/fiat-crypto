adf21753de612855f7ecb2165c8c1b03af619259
jadep <jade.philipoom@gmail.com>
2020-03-30 10:09:19 -0400

    add header with bounds

--- src/Bedrock/Stringification.v
+++ src/Bedrock/Stringification.v
@@ -14,6 +14,7 @@ Import Stringification.Language.Compilers.
 Import Types.
 
 Import ListNotations Types.Notations.
+Import ToString.OfPHOAS.
 Local Open Scope string_scope.
 Local Open Scope list_scope.
 
@@ -97,6 +98,63 @@ Definition bedrock_func_to_lines (f : bedrock_func)
   : list string :=
   [@c_func BasicCSyntax.to_c_parameters f].
 
+Fixpoint make_base_var_data {t}
+  : base_ltype t -> list_lengths (type.base t) ->
+    option (base_var_data t) :=
+  let int_type :=
+      ToString.int.unsigned (Z.to_nat machine_wordsize) in
+  match t as t0 return
+        base_ltype t0 ->
+        list_lengths (type.base t0) ->
+        option (base_var_data t0) with
+  | base.type.prod A B =>
+    fun x ll =>
+      match make_base_var_data (fst x) (fst ll) with
+      | Some x1 =>
+        match make_base_var_data (snd x) (snd ll) with
+        | Some x2 => Some (x1, x2)
+        | None => None
+        end
+      | None => None
+      end
+  | base_Z =>
+    fun name _ =>
+      let is_pointer := false in (* never a pointer *)
+      Some (name, is_pointer, Some int_type)
+  | base_listZ =>
+    fun name len => Some (name, Some int_type, len)
+  | _ => fun _ _ => None
+  end.
+
+Definition make_var_data {t}
+  : ltype t -> list_lengths t -> option (var_data t) :=
+  match t with
+  | type.base b => make_base_var_data
+  | type.arrow _ _ => fun _ _ => None
+  end.
+
+Fixpoint make_var_data_of_args {t}
+  : type.for_each_lhs_of_arrow ltype t ->
+    type.for_each_lhs_of_arrow list_lengths t ->
+    option (type.for_each_lhs_of_arrow var_data t) :=
+  match t as t0 return
+        type.for_each_lhs_of_arrow ltype t0 ->
+        type.for_each_lhs_of_arrow list_lengths t0 ->
+        option (type.for_each_lhs_of_arrow var_data t0)
+  with
+  | type.arrow s d =>
+    fun names lengths =>
+      match make_var_data (fst names) (fst lengths) with
+      | Some x =>
+        match make_var_data_of_args (snd names) (snd lengths) with
+        | Some y => Some (x, y)
+        | None => None
+        end
+      | None => None
+      end
+  | type.base b => fun _ _ => Some tt
+  end.
+
 (* TODO: for now, name_list is just ignored -- could probably make it not ignored *)
 Definition Bedrock2_ToFunctionLines
            {relax_zrange : relax_zrange_opt}
@@ -112,10 +170,29 @@ Definition Bedrock2_ToFunctionLines
   :=
     match make_innames t, make_outnames (type.final_codomain t),
           list_lengths_from_argbounds inbounds with
-    | Some innames, Some outnames, Some lengths =>
-      let f := (name, translate_func e innames lengths outnames) in
+    | Some innames, Some outnames, Some inlengths =>
+      let out := translate_func e innames inlengths outnames in
+      let f : bedrock_func := (name, fst out) in
+      let outlengths := snd out in
       if error_free_cmd (snd (snd f))
-      then inl (bedrock_func_to_lines f, ToString.ident_info_empty)
+      then
+        match make_var_data_of_args innames inlengths,
+              make_base_var_data outnames outlengths with
+        | Some indata, Some outdata =>
+          inl (["/*"]
+                 ++ [" * Input Bounds:"]
+                 ++ List.map (fun v => " *   " ++ v)%string (input_bounds_to_string indata inbounds)
+                 ++ [" * Output Bounds:"]
+                 ++ List.map (fun v => " *   " ++ v)%string (bound_to_string outdata outbounds)
+                 ++ [" */"]
+                 ++ bedrock_func_to_lines f, ToString.ident_info_empty)
+        | _, _ =>
+          inr
+            (String.concat
+               String.NewLine
+               ("Failed to generate header for function:"
+                  :: bedrock_func_to_lines f))
+        end
       else inr
              (String.concat
                 String.NewLine
