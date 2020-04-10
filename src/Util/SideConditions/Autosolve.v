939dc265a1a13ea6ab9dda9c564448995662e10f
Jason Gross <jgross@mit.edu>
2020-04-10 00:47:25 -0400

    Remove a file importing AdmitAxiom
    It's dead code, at this point.
    
    We could remove all of the autosolve machinery, but I don't see a reason
    to at this point.

--- src/Util/SideConditions/Autosolve.v
+++ src/Util/SideConditions/Autosolve.v
@@ -1,6 +1,5 @@
 Require Import Crypto.Util.Decidable.
 Require Import Crypto.Util.SideConditions.CorePackages.
-Require Import Crypto.Util.SideConditions.AdmitPackage.
 Require Import Crypto.Util.SideConditions.ModInvPackage.
 Require Import Crypto.Util.SideConditions.ReductionPackages.
 Require Import Crypto.Util.SideConditions.RingPackage.
@@ -8,12 +7,11 @@ Require Import Crypto.Util.SideConditions.RingPackage.
 Ltac autosolve_gen autosolve_tac ring_intros_tac else_tac :=
   CorePackages.preautosolve ();
   CorePackages.Internal.autosolve ltac:(fun _ =>
-  AdmitPackage.autosolve ltac:(fun _ =>
   ModInvPackage.autosolve ltac:(fun _ =>
   ReductionPackages.autosolve autosolve_tac ltac:(fun _ =>
   RingPackage.autosolve_gen_intros ring_intros_tac ltac:(fun _ =>
   CorePackages.autosolve else_tac
-                                       ))))).
+                                       )))).
 
 Ltac autosolve_gen_intros ring_intros_tac else_tac :=
   autosolve_gen ltac:(autosolve_gen_intros ring_intros_tac) ring_intros_tac else_tac.
