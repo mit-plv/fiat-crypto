092b311e514762f41cc9478e9e02745c7eb44d1d
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-04-18 14:44:08 +0200

    Adapting to Coq PR #12023: atomic form of primitive tactics are in module Ltac.
    Previously, it was in Coq.Init.Notations. Now in Coq.Init.Ltac.
    
    The patch is backward compatible (but a bit less explicit about which
    etransitivity we are talking about).

--- src/Util/Tactics/ETransitivity.v
+++ src/Util/Tactics/ETransitivity.v
@@ -1,5 +1,9 @@
 Require Import Coq.Classes.RelationClasses.
 
+(** We call Coq's [etransitivity] for compatibility
+    because it's more powerful than [etransitivity _] in some cases,
+    e.g., when things need to be unfolded. *)
+Tactic Notation "etransitivity" := etransitivity.
 Tactic Notation "etransitivity" open_constr(y) :=
   intros;
   let R := match goal with |- ?R ?x ?z => constr:(R) end in
@@ -8,10 +12,6 @@ Tactic Notation "etransitivity" open_constr(y) :=
   let pre_proof_term_head := constr:(@transitivity _ R _) in
   let proof_term_head := (eval cbn in pre_proof_term_head) in
   refine (proof_term_head x y z _ _); [ change (R x y) | change (R y z) ].
-(** We call [Coq.Init.Notations.etransitivity] for compatibility
-    because it's more powerful than [etransitivity _] in some cases,
-    e.g., when things need to be unfolded. *)
-Tactic Notation "etransitivity" := Coq.Init.Notations.etransitivity.
 Tactic Notation "etransitivity_rev" uconstr(y) := [ > etransitivity y; cycle 1.. ].
 Tactic Notation "etransitivity_rev" := [ > etransitivity; cycle 1.. ].
 
