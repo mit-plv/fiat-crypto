1946b348cf99bd5fd4a46f4f8a96edc0bdfb72f1
jadep <jade.philipoom@gmail.com>
2020-04-29 18:33:24 -0400

    clean up API

--- src/Bedrock/API.v
+++ src/Bedrock/API.v
@@ -40,6 +40,80 @@ Arguments reified_eq {_ _ _}.
 Arguments reified_Wf3 {_ _ _}.
 Arguments reified_valid {_ _ _}.
 
+(* TODO: replace with prefix-based generators *)
+Definition inname_gen :=
+  fun n => ("in" ++ Decimal.Z.to_string (Z.of_nat n))%string.
+Definition outname_gen :=
+  fun n => ("out" ++ Decimal.Z.to_string (Z.of_nat n))%string.
+Local Notation make_bedrock_func :=
+  (@make_bedrock_func _ inname_gen outname_gen).
+(* TODO: remove these axioms once there's a nice general proof for
+     prefix-based varname generators *)
+Axiom outname_gen_inname_gen_ok :
+  forall n m : nat, outname_gen n <> inname_gen m.
+Axiom inname_gen_varname_gen_ok :
+  forall {p : Types.parameters} n m,
+    inname_gen n <> Types.varname_gen m.
+Axiom outname_gen_varname_gen_ok :
+  forall {p : Types.parameters} n m,
+    outname_gen n <> Types.varname_gen m.
+Axiom inname_gen_unique :
+  forall i j : nat, inname_gen i = inname_gen j <-> i = j.
+Axiom outname_gen_unique :
+  forall i j : nat, outname_gen i = outname_gen j <-> i = j.
+
+Ltac handle_easy_preconditions :=
+  lazymatch goal with
+  | |- ZRange.type.option.is_tighter_than _ _ = true =>
+    abstract (vm_compute; reflexivity)
+  | |- UnsaturatedSolinas.check_args _ _ _ _ _ = _ =>
+    abstract (native_compute; reflexivity)
+  | |- Types.ok => solve [typeclasses eauto]
+  | _ => first [ apply inname_gen_varname_gen_ok
+               | apply outname_gen_varname_gen_ok
+               | apply outname_gen_inname_gen_ok
+               | apply inname_gen_unique
+               | apply outname_gen_unique ]
+  | |- ?g => fail "Unrecognized goal" g
+  end.
+
+Ltac make_reified_op start :=
+  assert (reified_op start)
+  by (econstructor; [ | | apply valid_func_bool_iff ];
+      [ vm_compute; reflexivity | | ];
+      [ abstract (prove_Wf3 ())
+      | abstract (vm_compute; reflexivity) ]).
+
+Ltac parameters_from_wordsize machine_wordsize :=
+  let r := (eval cbv - [Defaults32.default_parameters
+                          Defaults64.default_parameters] in
+               (select_parameters machine_wordsize)) in
+  lazymatch r with
+  | inl ?p => p
+  | inr ?err => fail "Failed to select parameters: " err
+  end.
+
+Ltac make_all_reified_ops n s c machine_wordsize :=
+  make_reified_op
+    (PushButtonSynthesis.UnsaturatedSolinas.carry_mul
+       n s c machine_wordsize).
+
+Ltac instantiate_ops carry_mul_name :=
+  let n :=
+      lazymatch goal with
+      | |- bedrock2_unsaturated_solinas _ ?n _ _ => n end in
+  let carry_mul_res := fresh "carry_mul_res" in
+  lazymatch goal with
+  | X : reified_op
+          (PushButtonSynthesis.UnsaturatedSolinas.carry_mul _ _ _ _)
+    |- _ =>
+    destruct X as [carry_mul_res ?]
+  end;
+  let carry_mul_func :=
+      constr:(make_bedrock_func n carry_mul_res) in
+  apply Build_bedrock2_unsaturated_solinas
+    with (carry_mul:= (carry_mul_name, carry_mul_func)).
+
 Section __.
   Let n := 10%nat.
   Let s := 2^255.
@@ -49,74 +123,15 @@ Section __.
   Definition carry_mul_name := "carry_mul"%string.
 
   Instance p : Types.parameters.
-  Proof.
-    let r := (eval cbv - [Defaults32.default_parameters
-                            Defaults64.default_parameters] in
-                 (select_parameters machine_wordsize)) in
-        lazymatch r with
-        | inl ?p => exact p
-        | inr ?err => fail "Failed to select parameters: " err
-        end.
-  Defined.
+  let p := parameters_from_wordsize machine_wordsize in
+  exact p. Defined.
 
-  (* TODO: replace with prefix-based generators *)
-  Definition inname_gen :=
-    fun n => ("in" ++ Decimal.Z.to_string (Z.of_nat n))%string.
-  Definition outname_gen :=
-    fun n => ("out" ++ Decimal.Z.to_string (Z.of_nat n))%string.
-
-  (* TODO: remove these axioms once there's a nice general proof for
-     prefix-based varname generators *)
-  Axiom inname_gen_varname_gen_ok :
-    forall n m : nat, inname_gen n <> Types.varname_gen m.
-  Axiom outname_gen_varname_gen_ok :
-    forall n m : nat, outname_gen n <> Types.varname_gen m.
-  Axiom outname_gen_inname_gen_ok :
-      forall n m : nat, outname_gen n <> inname_gen m.
-  Axiom inname_gen_unique :
-    forall i j : nat, inname_gen i = inname_gen j <-> i = j.
-  Axiom outname_gen_unique :
-    forall i j : nat, outname_gen i = outname_gen j <-> i = j.
-
-  Ltac handle_easy_preconditions :=
-    lazymatch goal with
-    | |- ZRange.type.option.is_tighter_than _ _ = true =>
-      abstract (vm_compute; reflexivity)
-    | |- UnsaturatedSolinas.check_args _ _ _ _ _ = _ =>
-      abstract (native_compute; reflexivity)
-    | |- Types.ok => solve [typeclasses eauto]
-    | _ => first [ apply inname_gen_varname_gen_ok
-                 | apply outname_gen_varname_gen_ok
-                 | apply outname_gen_inname_gen_ok
-                 | apply inname_gen_unique
-                 | apply outname_gen_unique ]
-    | |- ?g => fail "Unrecognized goal" g
-    end.
-
-  Ltac make_reified_op start :=
-    assert (reified_op start)
-    by (econstructor; [ | | apply valid_func_bool_iff ];
-          [ vm_compute; reflexivity | | ];
-          [ abstract (prove_Wf3 ())
-          | abstract (vm_compute; reflexivity) ]).
+  Instance p_ok : Types.ok. typeclasses eauto. Qed.
 
   Instance test : bedrock2_unsaturated_solinas p n s c.
   Proof.
-    make_reified_op
-      (PushButtonSynthesis.UnsaturatedSolinas.carry_mul
-         n s c Semantics.width).
-    lazymatch goal with
-    | X : reified_op _ |- _ =>
-      destruct X as [res ?]
-    end.
-
-    let carry_mul_func :=
-        constr:(make_bedrock_func
-                  (inname_gen:=inname_gen)
-                  (outname_gen:=outname_gen)
-                  n res) in
-    apply Build_bedrock2_unsaturated_solinas
-      with (carry_mul:= (carry_mul_name, carry_mul_func)).
+    make_all_reified_ops n s c machine_wordsize.
+    instantiate_ops carry_mul_name.
     apply UnsaturatedSolinas.carry_mul_correct.
 
     all: try assumption.
