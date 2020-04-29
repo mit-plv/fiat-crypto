22012696731e993cd63a4bcd593caa4a0643af56
jadep <jade.philipoom@gmail.com>
2020-04-29 11:31:16 -0400

    WIP, only bytes<->Bignum left for carry_mul

--- src/Bedrock/UnsaturatedSolinas.v
+++ src/Bedrock/UnsaturatedSolinas.v
@@ -20,6 +20,7 @@ Require Import Crypto.BoundsPipeline.
 Require Import Crypto.Bedrock.Defaults.
 Require Import Crypto.Bedrock.Tactics.
 Require Import Crypto.Bedrock.Types.
+Require Import Crypto.Bedrock.Util.
 Require Import Crypto.Bedrock.Proofs.Func.
 Require Import Crypto.Bedrock.Translation.Func.
 Require Import Crypto.COperationSpecifications.
@@ -37,20 +38,20 @@ Existing Instances rep.Z rep.listZ_mem.
 (* TODO: this is copy-pasted from Stringification; put in a common location *)
 Section with_parameters.
   Context {p : Types.parameters}.
+  Context {inname_gen outname_gen : nat -> string}.
 
-  Fixpoint make_names (prefix : string) (nextn : nat) (t : base.type)
+  Fixpoint make_names
+           (name_gen : nat -> string) (nextn : nat) (t : base.type)
     : nat * base_ltype t :=
     match t as t0 return nat * base_ltype t0 with
     | base.type.prod a b =>
-      let resa := make_names prefix nextn a in
-      let resb := make_names prefix (fst resa) b in
+      let resa := make_names name_gen nextn a in
+      let resb := make_names name_gen (fst resa) b in
       (fst resb, (snd resa, snd resb))
     | base_listZ =>
-      let num := Decimal.Z.to_string (Z.of_nat nextn) in
-      (S nextn, String.append prefix num)
+      (S nextn, name_gen nextn)
     | _ =>
-      let num := Decimal.Z.to_string (Z.of_nat nextn) in
-      (S nextn, String.append prefix num)
+      (S nextn, name_gen nextn)
     end.
   Fixpoint make_innames' (nextn : nat) (t : API.type)
     : nat * type.for_each_lhs_of_arrow ltype t :=
@@ -58,7 +59,7 @@ Section with_parameters.
           nat * type.for_each_lhs_of_arrow ltype t0 with
     | type.base _ => (nextn, tt)
     | type.arrow (type.base s) d =>
-      let ress := make_names "in" nextn s in
+      let ress := make_names inname_gen nextn s in
       let resd := make_innames' (fst ress) d in
       (fst resd, (snd ress, snd resd))
     | type.arrow _ d =>
@@ -68,7 +69,7 @@ Section with_parameters.
   Definition make_innames t : type.for_each_lhs_of_arrow ltype t :=
     snd (make_innames' 0 t).
   Definition make_outnames t : base_ltype t :=
-    snd (make_names "out" 0 t).
+    snd (make_names outname_gen 0 t).
 
   Fixpoint list_lengths_repeat_base (n : nat) t : base_listonly nat t :=
     match t as t0 return base_listonly nat t0 with
@@ -101,14 +102,34 @@ Local Coercion Z.of_nat : nat >-> Z.
 Local Coercion inject_Z : Z >-> Q.
 Local Coercion Z.pos : positive >-> Z.
 
+(* Strategy for varnames:
+   - Take in generator function for innames/outnames
+   - Make separate files with varname generators and proofs about them
+     * Include a prefix-based generator
+     * Include an easy proof that if the prefixes aren't equal the generators don't collide
+
+   For the synthesis, there should be two levels
+   - This level, where you get an end-to-end proof specialized to the function but lots of other things (the varname generation and machine wordsize, for instance) are left unspecified
+     * If you want to do something special or custom, you would access this level directly
+   - Another ltac-based level on top, the "easy mode", where you're not specialized to compile-time parameters like n or machine_wordsize, but varname generation is plugged in for you and there's automation to prove most of the subgoals of calling this level, so ideally you just plug in your parameters and get a record type with the functions and their proofs
+
+
+   Steps:
+   - finish up with carry_mul (except maybe the bytes<->Bignum proofs)
+   - make the ltac just for carry_mul and test it out
+   - create examples using the ltac
+
+ *)
+
 Section __.
   Context {p : Types.parameters}
+          {inname_gen outname_gen : nat -> string}
           (n : nat) (s : Z) (c : list (Z * Z)).
 
   Definition make_bedrock_func {t} (res : API.Expr t) :=
-    fst (translate_func res (make_innames _)
+    fst (translate_func res (make_innames (inname_gen:=inname_gen) _)
                         (list_lengths_repeat_args n _)
-                        (make_outnames _)).
+                        (make_outnames (outname_gen:=outname_gen) _)).
 
   Definition carry_mul
              (res : API.Expr (type.arrow type_listZ
@@ -143,6 +164,18 @@ Section __.
       (check_args_ok :
          check_args n s c Semantics.width (ErrorT.Success tt)
          = ErrorT.Success tt).
+
+    Context (inname_gen_varname_gen_ok :
+               forall n m, inname_gen n <> varname_gen m)
+            (outname_gen_varname_gen_ok :
+               forall n m, outname_gen n <> varname_gen m)
+            (outname_gen_inname_gen_ok :
+               forall n m, outname_gen n <> inname_gen m).
+    (* TODO: make record type for varname generators *)
+    Context (inname_gen_unique :
+               forall i j : nat, inname_gen i = inname_gen j <-> i = j)
+            (outname_gen_unique :
+               forall i j : nat, outname_gen i = outname_gen j <-> i = j).
 
     Definition Bignum (addr : Semantics.word) (xs : list Z) :
       Semantics.mem -> Prop :=
@@ -394,6 +427,273 @@ Section __.
       congruence.
     Qed.
 
+    Lemma make_names_no_collision name_gen1 name_gen2 t :
+      (forall n m : nat, name_gen1 n <> name_gen2 m) ->
+      forall nextn n,
+        ~ varname_set_base (snd (make_names name_gen1 nextn t))
+          (name_gen2 n).
+    Proof.
+      intro Hdisjoint; induction t; intros;
+        cbn [varname_set_base rep.varname_set rep.Z rep.listZ_mem
+                              make_names fst snd ];
+        repeat match goal with
+               | _ => progress cbn [fst snd]
+               | |- ~ PropSet.singleton_set _ _ =>
+                 apply disjoint_singleton_r_iff;
+                   solve [auto using disjoint_singleton_singleton]
+               | |- ~ PropSet.union _ _ _ =>
+                 apply Util.not_union_iff; split; solve [auto]
+               | _ => progress break_innermost_match
+               end.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma make_innames'_varname_gen_disjoint t :
+      forall nextn n,
+        ~ varname_set_args
+          (snd (make_innames' (inname_gen:=inname_gen) nextn t))
+          (varname_gen n).
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress
+                        cbn [fst snd make_innames'
+                                 varname_set_args] in *
+               | |- ~ PropSet.empty_set _ => cbv [PropSet.empty_set]; tauto
+               | |- ~ PropSet.union _ _ _ =>
+                 apply Util.not_union_iff; split
+               | _ => progress break_innermost_match
+               | _ => solve [auto using make_names_no_collision]
+               end.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma make_innames_varname_gen_disjoint t :
+      forall n,
+        ~ varname_set_args (make_innames (inname_gen:=inname_gen) t)
+          (varname_gen n).
+    Proof. apply make_innames'_varname_gen_disjoint. Qed.
+
+    (* TODO: when make_outnames is moved, move this too *)
+    Lemma make_outnames_varname_gen_disjoint t :
+      forall n,
+        ~ varname_set_base (make_outnames (outname_gen:=outname_gen) t)
+          (varname_gen n).
+    Proof. apply make_names_no_collision; auto. Qed.
+
+    (* TODO: when make_names is moved, move this too *)
+    Lemma fst_make_names_lower_bound name_gen t :
+      forall nextn, (nextn <= fst (make_names name_gen nextn t))%nat.
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_names] in *
+               | _ => progress break_innermost_match
+               | _ => lia
+               end; [ ].
+      eapply Nat.le_trans; [ | solve [eauto] ]. eauto.
+    Qed.
+
+    (* TODO: when make_names is moved, move this too *)
+    Lemma flatten_make_names_range name_gen t :
+      forall x nextn,
+        In x (Flatten.flatten_base_ltype
+                (snd (make_names name_gen nextn t))) ->
+        exists n, x = name_gen n
+                  /\ (nextn <= n < fst (make_names name_gen nextn t))%nat.
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_names
+                                        Flatten.flatten_base_ltype] in *
+               | H : _ /\ _ |- _ => destruct H
+               | H : In _ [_] |- _ => inversion H; clear H
+               | H : In _ [] |- _ => solve [inversion H]
+               | H : In _ (_ ++ _) |- _ =>
+                 apply in_app_iff in H; destruct H
+               | H : name_gen ?x = ?y |- exists n, ?y = name_gen n /\ _ =>
+                 exists x; split; [ congruence | ]
+               | H : ?y = name_gen ?x |- exists n, ?y = name_gen n /\ _ =>
+                 exists x; split; [ assumption | ]
+               | _ => specialize (IHt1 _ _ ltac:(eassumption));
+                        destruct IHt1
+               | _ => specialize (IHt2 _ _ ltac:(eassumption));
+                        destruct IHt2
+               | _ => progress break_innermost_match
+               | _ => lia
+              end; [ | ];
+          pose proof fst_make_names_lower_bound name_gen t1 nextn;
+          pose proof fst_make_names_lower_bound
+               name_gen t2 (fst (make_names name_gen nextn t1));
+          lia.
+    Qed.
+
+    (* TODO: when make_names is moved, move this too *)
+    Lemma flatten_make_names_NoDup name_gen t :
+      (forall i j, name_gen i = name_gen j <-> i = j) ->
+      forall nextn,
+        NoDup (Flatten.flatten_base_ltype
+                 (snd (make_names name_gen nextn t))).
+    Proof.
+      intro name_gen_unique.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_names
+                                        Flatten.flatten_base_ltype]
+               | H : In _ _ |- _ => apply flatten_make_names_range in H;
+                                      destruct H
+               | H : _ /\ _ |- _ => destruct H
+               | H1 : ?x = name_gen ?n1, H2 : ?x = name_gen ?n2 |- _ =>
+                 pose proof (proj1 (name_gen_unique n1 n2)
+                                   ltac:(congruence)); clear H1 H2
+               | _ => apply NoDup_cons
+               | |- NoDup (_ ++ _) => apply NoDup_app_iff
+               | |- _ /\ _ => split
+               | |- ~ In _ _ => try apply in_nil; intro
+               | _ => progress break_innermost_match
+               | _ => solve [auto using in_nil, NoDup_nil]
+               | _ => lia
+               end.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma fst_make_innames'_lower_bound t :
+      forall nextn,
+        (nextn <= fst (make_innames' (inname_gen:=inname_gen) nextn t))%nat.
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_innames'] in *
+               | _ => progress break_innermost_match
+               | _ => progress break_innermost_match_hyps
+               | _ => lia
+               | _ => solve [eauto]
+               end; [ ].
+      eapply Nat.le_trans; [ apply fst_make_names_lower_bound | ].
+      eauto.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma flatten_make_innames'_range t :
+      forall x nextn,
+        In x (Flatten.flatten_argnames
+                (snd (make_innames' (inname_gen:=inname_gen) nextn t))) ->
+        exists n,
+          x = inname_gen n
+          /\ (nextn <= n < fst (make_innames' (inname_gen:=inname_gen)
+                                              nextn t))%nat.
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_innames'
+                                        Flatten.flatten_argnames] in *
+               | H : _ /\ _ |- _ => destruct H
+               | H : In _ [] |- _ => solve [inversion H]
+               | H : In _ (_ ++ _) |- _ =>
+                 apply in_app_iff in H; destruct H
+               | H : In _ _ |- _ => apply flatten_make_names_range in H;
+                                      destruct H
+               | H : ?y = inname_gen ?x
+                 |- exists n, ?y = inname_gen n /\ _ =>
+                 exists x; split; [ assumption | ]
+               | _ => specialize (IHt2 _ _ ltac:(eassumption));
+                        destruct IHt2
+               | _ => progress break_innermost_match
+               | _ => lia
+               end; [ | ].
+      { pose proof fst_make_innames'_lower_bound t2 (fst (make_names inname_gen nextn t)).
+        lia. }
+      { pose proof fst_make_names_lower_bound inname_gen t nextn.
+        lia. }
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma flatten_make_innames'_NoDup t :
+      forall nextn,
+        NoDup
+          (Flatten.flatten_argnames
+             (snd (make_innames' (inname_gen:=inname_gen) nextn t))).
+    Proof.
+      induction t; intros;
+        repeat match goal with
+               | _ => progress intros
+               | _ => progress cbn [fst snd make_innames'
+                                        Flatten.flatten_argnames]
+               | _ => apply NoDup_nil
+               | H : _ /\ _ |- _ => destruct H
+               | H : In _ _ |- _ => apply flatten_make_names_range in H;
+                                      destruct H
+               | H : In _ _ |- _ => apply flatten_make_innames'_range in H;
+                                      destruct H
+               | H1 : ?x = inname_gen ?n1, H2 : ?x = inname_gen ?n2 |- _ =>
+                 pose proof (proj1 (inname_gen_unique n1 n2)
+                                   ltac:(congruence)); clear H1 H2
+               | |- NoDup (_ ++ _) => apply NoDup_app_iff
+               | |- _ /\ _ => split
+               | |- ~ In _ _ => intro
+               | _ => progress break_innermost_match
+               | _ => solve [ auto using flatten_make_names_NoDup]
+               | _ => lia
+               end.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma flatten_make_innames_NoDup t :
+      NoDup
+        (Flatten.flatten_argnames
+           (make_innames (inname_gen:=inname_gen) t)).
+    Proof. apply flatten_make_innames'_NoDup. Qed.
+
+    Lemma flatten_make_innames_exists t x :
+      In x (Flatten.flatten_argnames
+              (make_innames (inname_gen:=inname_gen) t)) ->
+      exists n : nat, x = inname_gen n.
+    Proof.
+      cbv [make_innames]; intros.
+      destruct (flatten_make_innames'_range t _ _ ltac:(eassumption)).
+      cleanup. eexists; eauto.
+    Qed.
+
+    Lemma flatten_make_outnames_exists t x :
+      In x (Flatten.flatten_base_ltype
+              (make_outnames (outname_gen:=outname_gen) t)) ->
+      exists n : nat, x = outname_gen n.
+    Proof.
+      cbv [make_outnames]; intros.
+      destruct (flatten_make_names_range
+                  outname_gen t _ _ ltac:(eassumption)).
+      cleanup. eexists; eauto.
+    Qed.
+
+    (* TODO: when make_innames is moved, move this too *)
+    Lemma make_innames_make_outnames_disjoint t1 t2 :
+      PropSet.disjoint
+        (varname_set_args (make_innames (inname_gen:=inname_gen) t1))
+        (varname_set_base (make_outnames (outname_gen:=outname_gen) t2)).
+    Proof.
+      rewrite Flatten.varname_set_args_flatten.
+      rewrite Flatten.varname_set_flatten.
+      apply NoDup_disjoint, NoDup_app_iff.
+      ssplit;
+        repeat match goal with
+               | _ => progress intros
+               | H : In _ _ |- _ => apply flatten_make_innames_exists in H;
+                                      destruct H
+               | H : In _ _ |- _ => apply flatten_make_outnames_exists in H;
+                                      destruct H
+               | H1 : ?x = inname_gen ?n1, H2 : ?x = outname_gen ?n2 |- _ =>
+                 specialize (outname_gen_inname_gen_ok n2 n1); congruence
+               | |- ~ In _ _ => intro
+               | _ => solve [eauto using flatten_make_innames_NoDup,
+                             flatten_make_names_NoDup]
+               end.
+    Qed.
+
     (* For out, you can get an array of bytes from a bignum using
        Bignum_to_bytes. *)
     Instance spec_of_carry_mul : spec_of "carry_mul" :=
@@ -472,12 +772,7 @@ Section __.
 
       { cbn. rewrite !bounded_by_loose_bounds_length by auto.
         reflexivity. }
-      { (* varname_gen doesn't collide with make_innames
-           (need to think about this one -- maybe find a way of constructing
-           parameters that guarantees the same varname_gen? Or some varname_gen
-           that is parameterized over the prefix, and a proof that the prefix is
-           not in or out? *)
-        admit. }
+      { apply make_innames_varname_gen_disjoint. }
       { (* arg pointers are correct *)
         cbn; sepsimpl.
         exists 1%nat; sepsimpl;
@@ -503,15 +798,11 @@ Section __.
         rewrite !word.of_Z_unsigned.
         ecancel_assumption. }
       { (* no duplicates in argnames *)
-        cbn; repeat constructor; cbn; try tauto.
-        destruct 1; congruence. }
-      { (* outnames and varname_gen don't collide *)
-        admit. }
+        apply flatten_make_innames_NoDup. }
+      { apply make_outnames_varname_gen_disjoint. }
       { (* no duplicates in outnames *)
-        cbn; repeat constructor; cbn. tauto. }
-      { (* innames and outnames are disjoint --
-           probably can make a separate lemma for this *)
-        admit. }
+        apply flatten_make_names_NoDup; auto. }
+      { apply make_innames_make_outnames_disjoint. }
       { cbn. sepsimpl.
         match goal with
         | H : context [array ptsto _ _ ?bs] |- _ =>
@@ -539,8 +830,7 @@ Section __.
           ssplit; reflexivity. }
         { rewrite !word.of_Z_unsigned.
           cbv [Bignum] in *.
-          ecancel_assumption. }
+          ecancel_assumption. } }
     Qed.
-
   End Proofs.
 End __.
