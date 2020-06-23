252d7d04619d9441b9b17d0eb5d514b31d732226
jadep <jade.philipoom@gmail.com>
2020-06-23 07:30:43 -0400

    separate bignums into their own file

--- src/Bedrock/Field/UnsaturatedSolinas.v
+++ src/Bedrock/Field/UnsaturatedSolinas.v
@@ -32,6 +32,7 @@ Require Import Crypto.Bedrock.Proofs.Func.
 Require Import Crypto.Bedrock.Translation.Func.
 Require Import Crypto.Bedrock.Field.Tactics.
 Require Import Crypto.Bedrock.Field.Operation.
+Require Import Crypto.Bedrock.Field.Bignum.
 Require Import Crypto.COperationSpecifications.
 Require Import Crypto.PushButtonSynthesis.UnsaturatedSolinas.
 Require Import Crypto.Util.ListUtil.
@@ -87,48 +88,6 @@ Ltac apply_correctness_in H :=
   | context [UnsaturatedSolinas.one] =>
     apply UnsaturatedSolinas.one_correct in H
   end.
-
-Section Bignum.
-  Context {p : Types.parameters}.
-
-  Definition Bignum
-             (n : nat) (px : Semantics.word) (x : list Semantics.word)
-    : Semantics.mem -> Prop :=
-    sep (emp (length x = n)) (array scalar (word.of_Z word_size_in_bytes) px x).
-
-  Definition EncodedBignum
-             (n_bytes : nat) (px : Semantics.word) (x : list Byte.byte)
-    : Semantics.mem -> Prop :=
-    sep (emp (length x = n_bytes)) (array ptsto (word.of_Z 1) px x).
-
-  Section Proofs.
-    Context {ok : Types.ok}.
-    Existing Instance semantics_ok.
-
-    Lemma Bignum_of_bytes n addr bs :
-      length bs = (n * Z.to_nat word_size_in_bytes)%nat ->
-      Lift1Prop.iff1
-        (array ptsto (word.of_Z 1) addr bs)
-        (Bignum n addr (map word.of_Z
-                            (eval_bytes (width:=Semantics.width) bs))).
-    Admitted. (* TODO *)
-
-    Lemma Bignum_to_bytes n addr x :
-      list_Z_bounded_by (max_bounds n) (map word.unsigned x) ->
-      Lift1Prop.iff1
-        (Bignum n addr x)
-        (array ptsto (word.of_Z 1) addr (encode_bytes x)).
-    Admitted. (* TODO *)
-  End Proofs.
-End Bignum.
-Notation BignumSuchThat :=
-  (fun n addr ws P =>
-     let xs := map word.unsigned ws in
-     sep (emp (P xs)) (Bignum n addr ws)).
-Notation EncodedBignumSuchThat :=
-  (fun n addr ws P =>
-     let xs := map Byte.byte.unsigned ws in
-     sep (emp (P xs)) (EncodedBignum n addr ws)).
 
 Section __.
   Context {p : Types.parameters}
