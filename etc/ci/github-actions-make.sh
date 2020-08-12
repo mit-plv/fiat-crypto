67b7762e900c273a7235a1dd701da19c4c5424ce
Jason Gross <jgross@mit.edu>
2020-08-12 13:18:06 -0400

    Test for --output-sync on CI
    Mac OS apparently doesn't support it

--- etc/ci/github-actions-make.sh
+++ etc/ci/github-actions-make.sh
@@ -6,8 +6,15 @@ if [ ! -z "${TOUCH}" ]; then
     make -j2 -t ${TOUCH} || exit $?
 fi
 
+OUTPUT_SYNC=""
+if make -h 2>/dev/null | grep -q output-sync; then
+    OUTPUT_SYNC="--output-sync"
+else
+    echo "WARNING: make does not support --output-sync"
+fi
+
 rm -f finished.ok
-(make "$@" --output-sync TIMED=1 TIMING=1 2>&1 && touch finished.ok) | tee -a time-of-build.log
+(make "$@" ${OUTPUT_SYNC} TIMED=1 TIMING=1 2>&1 && touch finished.ok) | tee -a time-of-build.log
 python "./etc/coq-scripts/timing/make-one-time-file.py" "time-of-build.log" "time-of-build-pretty.log" || exit $?
 
 git update-index --assume-unchanged _CoqProject
