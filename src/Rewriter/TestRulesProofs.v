9428f47f189ff7bd0eab5df32015155303b41391
Jason Gross <jgross@mit.edu>
2020-08-17 17:42:30 -0400

    Remove an Admitted that we didn't need to have

--- src/Rewriter/TestRulesProofs.v
+++ src/Rewriter/TestRulesProofs.v
@@ -1,6 +1,16 @@
 Require Crypto.Util.PrimitiveHList.
 Require Import Crypto.Rewriter.TestRules.
 
+Local Ltac start_proof :=
+  cbv [snd]; hnf; cbv [PrimitiveHList.hlist];
+  repeat apply PrimitiveProd.Primitive.pair; try exact tt.
 Lemma test_rewrite_rules_proofs
   : PrimitiveHList.hlist (@snd bool Prop) test_rewrite_rulesT.
-Proof using Type. Admitted.
+Proof using Type.
+  start_proof.
+  all: try reflexivity.
+  (** If testing out rules, change Qed to Admitted *)
+Qed.
+(*
+Admitted.
+*)
