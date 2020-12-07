e4ab86c455359db71bbfbd1c93d6c8486ef8f63c
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-12-07 15:49:55 -0500

    make hexdump notation work

--- src/Bedrock/Field/Synthesis/Examples/CompilerTest.v
+++ src/Bedrock/Field/Synthesis/Examples/CompilerTest.v
@@ -120,13 +120,16 @@ Proof.
 Module PrintAssembly.
   Import riscv.Utility.InstructionNotations.
   Goal True. let r := eval unfold asm in asm in idtac (* r *). Abort.
+End PrintAssembly.
 
-Definition as_bytes: list Byte.byte := instrencode asm.
+(* Note: this must be Coq.Init.Byte.byte, not coqutil.Byte.byte,
+   which is a Notation for `(Coq.Init.Byte.byte: Type)` and doesn't
+   work with bedrock2.Hexdump. *)
+Definition as_bytes: list Coq.Init.Byte.byte := instrencode asm.
 
 Module PrintBytes.
   Import bedrock2.Hexdump.
   Local Open Scope hexdump_scope.
   Set Printing Width 100.
-  (* TODO why does this print [ and ] and ; ? *)
   Goal True. let x := eval vm_compute in as_bytes in idtac (* x *). Abort.
 End PrintBytes.
