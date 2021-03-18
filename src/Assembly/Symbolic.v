d85cf2eb904f2cbe8c0f8cddee7e404b5dcec78e
Andres Erbsen <andreser@mit.edu>
2021-03-18 09:55:08 -0400

    tuple in reg_state, symbolic_state

--- src/Assembly/Symbolic.v
+++ src/Assembly/Symbolic.v
@@ -1,18 +1,43 @@
 Require Import Coq.Lists.List.
 Require Import Coq.ZArith.ZArith.
+Require Crypto.Util.Tuple.
+Require Import Util.OptionList.
 Module Import List.
-  Definition option_all [A] : list (option A) -> option (list A). Admitted.
-  Definition indexof [A] : (A -> bool) -> list A -> option nat. Admitted.
+  Definition option_all [A] : list (option A) -> option (list A) := OptionList.Option.List.lift.
+  Lemma option_all_Some [A] ol l (H : @option_all A ol = Some l)
+    : forall i a, nth_error l i = Some a -> nth_error ol i = (Some (Some a)).
+  Admitted.
+  Lemma option_all_None [A] ol (H : @option_all A ol = None)
+    : exists j, nth_error ol j = None.
+  Admitted.
+
+  Section IndexOf.
+    Context [A] (f : A -> bool).
+    Fixpoint indexof (l : list A) : option nat :=
+      match l with
+      | nil => None
+      | cons a l =>
+          if f a then Some 0
+          else option_map S (indexof l )
+      end.
+    Lemma indexof_none l (H : indexof l = None) :
+      forall i a, nth_error l i = Some a -> f a = false.
+    Admitted.
+    Lemma indexof_Some l i (H : indexof l = Some i) :
+      exists a, nth_error l i = Some a -> f a = true.
+    Admitted.
+    Lemma indexof_first l i (H : indexof l = Some i) :
+      forall j a, j < i -> nth_error l j = Some a -> f a = false.
+    Admitted.
+  End IndexOf.
 End List.
 
 Require Import Crypto.Assembly.Syntax.
-Print FLAG.
 Definition idx := Z.
 Local Set Boolean Equality Schemes.
-Variant op := const (_ : Z) | add (* | ... *).
+Variant op := const (_ : Z) | add (width : Z) (* | ... *).
 
 Definition node (arg : Set) : Set := op * list arg.
-Definition node_eqb [arg : Set] (arg_eqb : arg -> arg -> bool) : node arg -> node arg -> bool. Admitted.
 
 Local Unset Elimination Schemes.
 Inductive expr : Set :=
@@ -23,13 +48,22 @@ Local Set Elimination Schemes.
 Definition invert_ExprRef (e : expr) :=
   match e with ExprRef i => Some i | _ => None end.
 
+Definition node_eqb [arg : Set] (arg_eqb : arg -> arg -> bool) : node arg -> node arg -> bool := 
+  Prod.prod_beq _ _ op_beq (ListUtil.list_beq _ arg_eqb).
+
 Definition dag := list (node idx).
 
-Definition memstate := list (Z * list (option idx)).
-Definition regstate := REG -> option idx.
-Definition flagstate := FLAG -> option idx.
+Definition reg_state := Tuple.tuple (option idx) 16.
+Definition flag_state := Tuple.tuple (option idx) 6.
+Definition mem_state := list (Z * list (option idx)).
 
-Definition state : Set := dag * regstate * flagstate * memstate.
+Record symbolic_state := { dag_state :> dag ; machine_reg_state :> reg_state ; machine_flag_state :> flag_state ; machine_mem_state :> mem_state }.
+Definition update_reg_with (st : symbolic_state) (f : reg_state -> reg_state) : symbolic_state
+  := {| dag_state := st.(dag_state); machine_reg_state := f st.(machine_reg_state) ; machine_flag_state := st.(machine_flag_state) ; machine_mem_state := st.(machine_mem_state) |}.
+Definition update_flag_with (st : symbolic_state) (f : flag_state -> flag_state) : symbolic_state
+  := {| dag_state := st.(dag_state); machine_reg_state := st.(machine_reg_state) ; machine_flag_state := f st.(machine_flag_state) ; machine_mem_state := st.(machine_mem_state) |}.
+Definition update_mem_with (st : symbolic_state) (f : mem_state -> mem_state) : symbolic_state
+  := {| dag_state := st.(dag_state); machine_reg_state := st.(machine_reg_state) ; machine_flag_state := st.(machine_flag_state) ; machine_mem_state := f st.(machine_mem_state) |}.
 
 Section WithDag.
   Context (dag : dag).
