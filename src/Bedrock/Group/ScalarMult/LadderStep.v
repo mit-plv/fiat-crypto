d1cf2d454c5933e465727c306e3e5673361411dd
Dustin Jamner <dijamner@me.com>
2021-10-06 15:06:48 -0400

    Move generic code out of ladderstep. Fix bugs

--- src/Bedrock/Group/ScalarMult/LadderStep.v
+++ src/Bedrock/Group/ScalarMult/LadderStep.v
@@ -1,7 +1,7 @@
 Require Import Rupicola.Lib.Api.
-Require Import Rupicola.Lib.Alloc.
+Require Import Rupicola.Lib.Alloc. 
+Require Import Rupicola.Lib.SeparationLogicImpl.
 Require Import Crypto.Arithmetic.PrimeFieldTheorems.
-Require Import Crypto.Bedrock.Group.Point.
 Require Import Crypto.Bedrock.Specs.Field.
 Require Import Crypto.Bedrock.Field.Interface.Compilation2.
 Local Open Scope Z_scope.
@@ -74,30 +74,19 @@ Section __.
               * FElem (Some tight_bounds) pX3 X5
               * FElem (Some tight_bounds) pZ3 Z5 * R)%sep mem'}.
 
-  (*TODO
   Lemma compile_ladderstep {tr mem locals functions}
         (x1 x2 z2 x3 z3 : F M_pos) :
     let v := ladderstep_gallina x1 x2 z2 x3 z3 in
     forall {P} {pred: P v -> predicate} {k: nlet_eq_k P v} {k_impl}
            Rout
-           X1 X1_ptr X1_var X2 X2_ptr X2_var Z2 Z2_ptr Z2_var
-           X3 X3_ptr X3_var Z3 Z3_ptr Z3_var,
+           X1_ptr X1_var X2_ptr X2_var Z2_ptr Z2_var
+           X3_ptr X3_var Z3_ptr Z3_var,
 
       spec_of_ladderstep functions ->
 
-      feval X1 = x1 ->
-      feval X2 = x2 ->
-      feval Z2 = z2 ->
-      feval X3 = x3 ->
-      feval Z3 = z3 ->
-      bounded_by tight_bounds X1 ->
-      bounded_by tight_bounds X2 ->
-      bounded_by tight_bounds Z2 ->
-      bounded_by tight_bounds X3 ->
-      bounded_by tight_bounds Z3 ->
-      (FElem X1_ptr X1
-       * FElem X2_ptr X2 * FElem Z2_ptr Z2
-       * FElem X3_ptr X3 * FElem Z3_ptr Z3 * Rout)%sep mem ->
+      (FElem (Some tight_bounds) X1_ptr x1
+       * FElem (Some tight_bounds) X2_ptr x2 * FElem (Some tight_bounds) Z2_ptr z2
+       * FElem (Some tight_bounds) X3_ptr x3 * FElem (Some tight_bounds) Z3_ptr z3 * Rout)%sep mem ->
 
         map.get locals X1_var = Some X1_ptr ->
         map.get locals X2_var = Some X2_ptr ->
@@ -106,18 +95,14 @@ Section __.
         map.get locals Z3_var = Some Z3_ptr ->
 
         (let v := v in
-         forall (X4 Z4 X5 Z5 (* output values *)
-                 : felem) m
-                (Heq : ladderstep_gallina
-                         (feval X1) (feval X2) (feval Z2)
-                         (feval X3) (feval Z3)
-                       = (feval X4, feval Z4, feval X5, feval Z5)),
-          bounded_by tight_bounds X4 ->
-          bounded_by tight_bounds Z4 ->
-          bounded_by tight_bounds X5 ->
-          bounded_by tight_bounds Z5 ->
-          (FElem X1_ptr X1 * FElem X2_ptr X4 * FElem Z2_ptr Z4
-           * FElem X3_ptr X5 * FElem Z3_ptr Z5 * Rout)%sep m ->
+         forall X4 Z4 X5 Z5 (* output values *) m
+                (Heq : ladderstep_gallina x1 x2 z2 x3 z3
+                       = (X4, Z4, X5, Z5)),
+          (FElem (Some tight_bounds) X1_ptr x1
+           * FElem (Some tight_bounds) X2_ptr X4
+           * FElem (Some tight_bounds) Z2_ptr Z4
+           * FElem (Some tight_bounds) X3_ptr X5
+           * FElem (Some tight_bounds) Z3_ptr Z5 * Rout)%sep m ->
            (<{ Trace := tr;
                Memory := m;
                Locals := locals;
@@ -145,28 +130,7 @@ Section __.
         | |- sep _ _ _ => ecancel_assumption
         | _ => solve [eauto]
         end.
-    Qed.
-*)
-  
-  Ltac ladderstep_compile_custom :=
-    (*TODO: move some of this into core rupicola?*)
-    lazymatch goal with
-    | [v := alloc _ |-_] =>
-      unfold alloc in v;
-      subst v
-      | _ =>
-        simple apply compile_nlet_as_nlet_eq
-    end;
-    field_compile_step; [ repeat compile_step .. | intros ];
-    eauto with compiler;
-    (* rewrite results in terms of feval to match lemmas *)
-    repeat lazymatch goal with
-           | H : feval _ = ?x |- context [?x] =>
-             is_var x; rewrite <-H
-           end.
-
-  Ltac compile_custom ::= ladderstep_compile_custom.
-
+  Qed.
   
   Lemma relax_bounds_FElem x_ptr x
     : Lift1Prop.impl1 (FElem (Some tight_bounds) x_ptr x)
@@ -201,158 +165,23 @@ Section __.
     intros; ecancel_assumption.
     Qed.
 
-
-(*TODO: move to separate file; copy source and re-edit?*)
-  Require Import coqutil.Tactics.syntactic_unify coqutil.Tactics.rdelta.
- 
-
-Require Import Coq.Classes.Morphisms.
-Require Import coqutil.sanity coqutil.Decidable coqutil.Tactics.destr.
-Section SepProperties.
-  Context {key value} {map : map.map key value} {ok : map.ok map}.
-  Context {key_eqb: key -> key -> bool} {key_eq_dec: EqDecider key_eqb}.
-  Local Open Scope sep_scope.
-  
-  Definition hd {T} := Eval cbv delta in @List.hd T.
-  Definition tl {T} := Eval cbv delta in @List.tl T.
-  Definition firstn {T} := Eval cbv delta in @List.firstn T.
-  Definition skipn {T} := Eval cbv delta in @List.skipn T.
-  Definition app {T} := Eval cbv delta in @List.app T.
-
-  Local Infix "++" := app. Local Infix "++" := app : list_scope.
-  Let nth n xs := hd (emp(map:=map) True) (skipn n xs).
-  Let remove_nth n (xs : list (map -> Prop)) :=
-    (firstn n xs ++ tl (skipn n xs)).
-
-  
-  Lemma seps_nth_to_head n xs : Lift1Prop.iff1 (sep (nth n xs) (seps (remove_nth n xs))) (seps xs).
-  Proof.
-    cbv [nth remove_nth].
-    pose proof (List.firstn_skipn n xs : (firstn n xs ++ skipn n xs) = xs).
-    set (xsr := skipn n xs) in *; clearbody xsr.
-    set (xsl := firstn n xs) in *; clearbody xsl.
-    subst xs.
-    setoid_rewrite <-seps'_iff1_seps.
-    destruct xsr.
-    { cbn [seps']; rewrite sep_emp_True_l, 2List.app_nil_r; exact (reflexivity _). }
-    cbn [hd tl].
-    induction xsl; cbn; [exact (reflexivity _)|].
-    rewrite <-IHxsl; clear IHxsl.
-    rewrite (sep_comm _ (seps' _)), <-(sep_assoc _ (seps' _)), <-(sep_comm _ (_ * seps' _)).
-    exact (reflexivity _).
-  Qed.
-  
-    (* iff1 instead of eq as a hyp would be a more general lemma, but eq is more convenient to use *)
-  Lemma cancel_seps_at_indices i j xs ys
-        (Hij : Lift1Prop.impl1 (nth i xs) (nth j ys))
-        (Hrest : Lift1Prop.impl1 (seps (remove_nth i xs)) (seps (remove_nth j ys)))
-    : Lift1Prop.impl1 (seps xs) (seps ys).
-  Proof.
-    rewrite <-(seps_nth_to_head i xs), <-(seps_nth_to_head j ys).
-    rewrite Hij, Hrest.
-    exact (reflexivity _).
-  Qed.
-
-  
-End SepProperties.
-  
-  (* leaves two open goals:
-   1) equality between left sep clause #i and right sep clause #j
-   2) updated main goal *)
-Ltac cancel_seps_at_indices i j :=
-  lazymatch goal with
-  | |- Lift1Prop.impl1 (seps ?LHS) (seps ?RHS) =>
-    simple refine (cancel_seps_at_indices i j LHS RHS _ _);
-    cbn [firstn skipn app hd tl]
-  end.
-
-Create HintDb ecancel_impl discriminated.
-Hint Extern 1 => exact (fun m x => x) : ecancel_impl.
-
-Ltac prop_at_index_is_evar j ys :=
-  let p := eval cbn [firstn skipn app hd tl nth] in (nth j ys) in
-      match p with
-      | (fun _ => ?pb) => is_evar pb
-      | _ => fail "malformed input"
-      end.
-
-(*TODO: performance*)
-Ltac find_implication xs y :=
-  multimatch xs with
-  | cons ?x _ =>
-    let H := fresh in
-    constr:(O)
-  | cons _ ?xs => let i := find_implication xs y in constr:(S i)
-  end.
-
-
-(*TODO: performance. I've replaced the deltavar stuff with heavier operations  *)
-Ltac ecancel_step :=
-      let RHS := lazymatch goal with |- Lift1Prop.impl1 _ (seps ?RHS) => RHS end in
-      let jy := index_and_element_of RHS in (* <-- multi-success! *)
-      let j := lazymatch jy with (?i, _) => i end in
-      let y := lazymatch jy with (_, ?y) => y end in
-      (* For implication, we don't want to touch RHS evars 
-         until everything else is solved.
-         Makes sure j doesn't point to an evar
-       *)
-      assert_fails (idtac; prop_at_index_is_evar 0%nat RHS);      
-      let LHS := lazymatch goal with |- Lift1Prop.impl1 (seps ?LHS) _ => LHS end in
-      let i := find_implication LHS y in (* <-- multi-success! *)
-      cancel_seps_at_indices i j; [solve [auto 1 with ecancel_impl]|].
-(*TODO: should I use deltavar here too?*)
-Ltac ecancel :=
-  cancel; repeat ecancel_step; (solve [ exact (fun m x => x) ]).
-
-
- Ltac ecancel_assumption_impl :=
-    multimatch goal with
-    | |- ?PG ?m1 =>
-      multimatch goal with
-      | H: ?PH ?m2
-        |- _ =>
-        syntactic_unify_deltavar m1 m2;
-        refine (Morphisms.subrelation_refl Lift1Prop.impl1 PH PG _ m1 H); clear H;
-        cbn[seps];(*TODO: why is this needed?*)
-        solve[ecancel]
-      end
-    end.
-
- 
-    (*TODO: speed up by combining pred_seps first and using 1 proper/ecancel_assumption?*)
-    Ltac clear_pred_seps :=   
-    unfold pred_sep;
-    repeat change (fun x => ?h x) with h;
-    repeat match goal with
-           | [ H : _ ?m |- _ ?m] =>
-             eapply Proper_sep_impl1;
-               [ eapply P_to_bytes | clear H m; intros H m | ecancel_assumption]
-           end.
-    Ltac compile_solve_side_conditions ::=
-  match goal with
-  | |- (_ ⋆ _) _ => cbn[fst snd] in *; ecancel_assumption
-  | |- map.get _ _ = _ => solve [ subst_lets_in_goal; solve_map_get_goal ]
-  | |- map.getmany_of_list _ _ = _ => apply map.getmany_of_list_cons
-  | |- map.remove_many _ _ = _ => solve_map_remove_many
-  | |- _ = _ => solve_map_eq
-  | |- _ <> _ => congruence
-  | |- _ /\ _ => split
-  | |- exists _,_ => eexists (*TODO: is this worth adding?*)
-  | |- pred_sep _ _ _ _ _ _ => clear_pred_seps
-  | _ => first
-  [ compile_cleanup
-  | compile_autocleanup
-  | reflexivity
-  | compile_use_default_value
-  | solve
-  [ typeclasses eauto | eauto with compiler_cleanup ] ]
-  end.
-
     
+    Hint Immediate relax_bounds_FElem : ecancel_impl.
+
+
     Ltac ecancel_assumption ::=  ecancel_assumption_impl.
 
- 
-Hint Immediate relax_bounds_FElem : ecancel_impl.
+  Ltac ladderstep_compile_custom :=
+    try simple apply compile_nlet_as_nlet_eq;
+    field_compile_step; [ repeat compile_step .. | intros ];
+    eauto with compiler;
+    (* rewrite results in terms of feval to match lemmas *)
+    repeat lazymatch goal with
+           | H : feval _ = ?x |- context [?x] =>
+             is_var x; rewrite <-H
+           end.
+
+  Ltac compile_custom ::= ladderstep_compile_custom.
 
 Derive ladderstep_body SuchThat
        (let args := ["X1"; "X2"; "Z2"; "X3"; "Z3"] in
@@ -364,7 +193,15 @@ Derive ladderstep_body SuchThat
                         proc [mul;add;sub;square;scmula24] in
           exact (__rupicola_program_marker ladderstep_gallina -> goal)))
        As ladderstep_correct.
-Proof. compile. Qed.
+Proof.
+  compile_setup.
+  repeat compile_step.
+  (*TODO: why isn't this integrated w/ compile_step?
+    I need something in compile_step that peels off existentials
+   *)
+  repeat compile_cleanup_post.
+  compile_step.
+Qed.
   
   
 End __.
