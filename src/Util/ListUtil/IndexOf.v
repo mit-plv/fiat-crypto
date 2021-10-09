a67c2b577ad47291abce07173e3eb2de61bfbb4d
Jason Gross <jgross@mit.edu>
2021-10-08 17:22:06 -0700

    Add indexof_None, indexof_app

--- src/Util/ListUtil/IndexOf.v
+++ src/Util/ListUtil/IndexOf.v
@@ -1,4 +1,6 @@
 Require Import Coq.Lists.List.
+Require Import Crypto.Util.Option.
+Require Import Crypto.Util.Tactics.BreakMatch.
 
 Module List.
   Section IndexOf.
@@ -17,6 +19,27 @@ Module List.
       destruct (f a) eqn:?; cbn.
       { inversion 1; subst. eexists. split. exact eq_refl. eassumption. }
       { cbv [option_map]; destruct (indexof l) eqn:?; inversion 1; subst; eauto. }
+    Qed.
+
+    Lemma indexof_None l (H : indexof l = None) :
+      forall i a, nth_error l i = Some a -> f a = false.
+    Proof using Type.
+      induction l, i.
+      all: repeat first [ reflexivity
+                        | progress cbn in *
+                        | progress cbv [option_map] in *
+                        | progress intros
+                        | progress inversion_option
+                        | progress subst
+                        | progress break_innermost_match_hyps
+                        | solve [ eauto ] ].
+    Qed.
+
+    Lemma indexof_app l1 l2
+      : indexof (l1 ++ l2) = Option.sequence (indexof l1) (option_map (fun x => List.length l1 + x) (indexof l2)).
+    Proof using Type.
+      revert l2; induction l1 as [|x xs IH]; cbn; intros; [ | rewrite IH ].
+      all: cbv [option_map Option.sequence]; break_innermost_match; reflexivity.
     Qed.
   End IndexOf.
 
