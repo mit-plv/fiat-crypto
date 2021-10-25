2eb08f6649be24406f266f4492076c07f1127d7d
Dustin Jamner <dijamner@me.com>
2021-10-25 19:37:16 -0400

    Reindent for consistent lines

--- src/Bedrock/Group/ScalarMult/MontgomeryLadder.v
+++ src/Bedrock/Group/ScalarMult/MontgomeryLadder.v
@@ -20,15 +20,14 @@ Import DownToCompiler.
 (* TODO: migrate these to rupicola.
    Currently break when put in Notations.v
  *)
-(* TODO: which tuple to use? *)
 Notation "'let/n' ( w , x , y , z ) := val 'in' body" :=
   (nlet [IdentParsing.TC.ident_to_string w;
         IdentParsing.TC.ident_to_string x;
         IdentParsing.TC.ident_to_string y;
         IdentParsing.TC.ident_to_string z]
         val  (fun '(w, x, y, z) => body))
-   (at level 200, w ident, x  ident, y ident, z ident, body at level 200,
-    only parsing).
+    (at level 200, w ident, x  ident, y ident, z ident, body at level 200,
+     only parsing).
 
 
 Notation "'let/n' ( v , w , x , y , z ) := val 'in' body" :=
@@ -55,7 +54,7 @@ Section __.
           {scalar_field_parameters : ScalarFieldParameters}.
   Context {scalar_field_parameters_ok : ScalarFieldParameters_ok}.
   Context {field_representaton : FieldRepresentation}
-  {scalar_representation : ScalarRepresentation}.
+          {scalar_representation : ScalarRepresentation}.
   Context {field_representation_ok : FieldRepresentation_ok}.
   Hint Resolve @relax_bounds : compiler.
 
@@ -103,18 +102,18 @@ Section __.
             R,
       { requires tr mem :=
           (FElem out_bound pOUT OUT * Scalar pK K * FElem (Some tight_bounds) pU U
-               *  R)%sep mem;
+           *  R)%sep mem;
         ensures tr' mem' :=
           tr' = tr
-          /\ (let OUT :=  montladder_gallina
-                            scalarbits
-                            (fun i =>
-                               Z.testbit (F.to_Z (sceval K))
-                                         (Z.of_nat i))
-                            U in
+          /\ (let OUT := montladder_gallina
+                           scalarbits
+                           (fun i =>
+                              Z.testbit (F.to_Z (sceval K))
+                                        (Z.of_nat i))
+                           U in
               (FElem (Some tight_bounds) pOUT OUT * Scalar pK K
                * FElem (Some tight_bounds) pU U
-                * R)%sep mem') }.
+               * R)%sep mem') }.
 
     (* Adding word.unsigned_of_Z_1 and word.unsigned_of_Z_0 as hints to
        compiler doesn't work, presumably because of the typeclass
@@ -134,112 +133,109 @@ Section __.
     simple eapply compile_sctestbit; shelve : compiler.
 
 
-  (*TODO: get compiling
-  Require Import Rupicola.Examples.CMove.
-   *)
-  
- Definition felem_cswap := "felem_cswap".
-  (*TODO: move to the right place;
+    (*TODO: determine the best place for felem_cswap. Should it get its own file?*)
+    Definition felem_cswap := "felem_cswap".
+    (*TODO: move to the right place;
     TODO: instantiate
-   *)  
-  Instance spec_of_felem_cswap : spec_of felem_cswap :=
-    fnspec! felem_cswap mask ptr1 ptr2 / b1 b2 b c1 c2 R,
-    { requires tr mem :=
-        mask = word.of_Z (Z.b2z b) /\
-        (FElem b1 ptr1 c1 * FElem b2 ptr2 c2 * R)%sep mem;
-      ensures tr' mem' :=
-        tr' = tr /\
-        let (c1,c2) := cswap b c1 c2 in
-        let (b1,b2) := cswap b b1 b2 in
-        (FElem b1 ptr1 c1 * FElem b2 ptr2 c2 * R)%sep mem' }.
-  
-   Lemma compile_felem_cswap {tr m l functions} swap (lhs rhs : F M_pos) :
-    let v := cswap swap lhs rhs in
-    forall {P} {pred: P v -> predicate} {k: nlet_eq_k P v} {k_impl}
-      R mask_var lhs_ptr lhs_var b_lhs rhs_ptr rhs_var b_rhs,
+     *)  
+    Instance spec_of_felem_cswap : spec_of felem_cswap :=
+      fnspec! felem_cswap mask ptr1 ptr2 / b1 b2 b c1 c2 R,
+      { requires tr mem :=
+          mask = word.of_Z (Z.b2z b) /\
+          (FElem b1 ptr1 c1 * FElem b2 ptr2 c2 * R)%sep mem;
+        ensures tr' mem' :=
+          tr' = tr /\
+          let (c1,c2) := cswap b c1 c2 in
+          let (b1,b2) := cswap b b1 b2 in
+          (FElem b1 ptr1 c1 * FElem b2 ptr2 c2 * R)%sep mem' }.
+    
+    Lemma compile_felem_cswap {tr m l functions} swap (lhs rhs : F M_pos) :
+      let v := cswap swap lhs rhs in
+      forall {P} {pred: P v -> predicate} {k: nlet_eq_k P v} {k_impl}
+             R mask_var lhs_ptr lhs_var b_lhs rhs_ptr rhs_var b_rhs,
 
-      spec_of_felem_cswap functions ->
+        spec_of_felem_cswap functions ->
 
-      map.get l mask_var = Some (word.of_Z (Z.b2z swap)) ->
-      
-      map.get l lhs_var = Some lhs_ptr ->
-      map.get l rhs_var = Some rhs_ptr ->
+        map.get l mask_var = Some (word.of_Z (Z.b2z swap)) ->
+        
+        map.get l lhs_var = Some lhs_ptr ->
+        map.get l rhs_var = Some rhs_ptr ->
 
-      (FElem b_lhs lhs_ptr lhs * FElem b_rhs rhs_ptr rhs * R)%sep m ->
+        (FElem b_lhs lhs_ptr lhs * FElem b_rhs rhs_ptr rhs * R)%sep m ->
 
-      (let v := v in
-       let (b1,b2) := cswap swap b_lhs b_rhs in
-       forall m',
-         (FElem b1 lhs_ptr (fst v) * FElem b2 rhs_ptr (snd v) * R)%sep m' ->
-         (<{ Trace := tr;
-             Memory := m';
-             Locals := l;
-             Functions := functions }>
+        (let v := v in
+         let (b1,b2) := cswap swap b_lhs b_rhs in
+         forall m',
+           (FElem b1 lhs_ptr (fst v) * FElem b2 rhs_ptr (snd v) * R)%sep m' ->
+           (<{ Trace := tr;
+               Memory := m';
+               Locals := l;
+               Functions := functions }>
+            k_impl
+            <{ pred (k v eq_refl) }>)) ->
+        <{ Trace := tr;
+           Memory := m;
+           Locals := l;
+           Functions := functions }>
+        cmd.seq
+          (cmd.call [] felem_cswap [expr.var mask_var; expr.var lhs_var; expr.var rhs_var])
           k_impl
-          <{ pred (k v eq_refl) }>)) ->
-      <{ Trace := tr;
-         Memory := m;
-         Locals := l;
-         Functions := functions }>
-      cmd.seq
-        (cmd.call [] felem_cswap [expr.var mask_var; expr.var lhs_var; expr.var rhs_var])
-        k_impl
-      <{ pred (nlet_eq [lhs_var; rhs_var] v k) }>.
-   Proof.     
-     Local Ltac prove_field_compilation locals :=
-       repeat straightline' locals;
-       handle_call;
-       lazymatch goal with
-       | |- sep _ _ _ => ecancel_assumption
-       | _ => idtac
-       end; eauto;
-       sepsimpl; repeat straightline' locals; subst; eauto.
-     prove_field_compilation l.
-     destruct swap; eapply H4; unfold v; unfold cswap; simpl; eauto.
-   Qed.
-   Hint Resolve compile_felem_cswap : compiler.
+        <{ pred (nlet_eq [lhs_var; rhs_var] v k) }>.
+    Proof.     
+      Local Ltac prove_field_compilation locals :=
+        repeat straightline' locals;
+        handle_call;
+        lazymatch goal with
+        | |- sep _ _ _ => ecancel_assumption
+        | _ => idtac
+        end; eauto;
+        sepsimpl; repeat straightline' locals; subst; eauto.
+      prove_field_compilation l.
+      destruct swap; eapply H4; unfold v; unfold cswap; simpl; eauto.
+    Qed.
+    Hint Resolve compile_felem_cswap : compiler.
 
-  Existing Instance felem_alloc.
+    Existing Instance felem_alloc.
 
 
-  Lemma cswap_same {A} b (a : A): cswap b a a = (a,a).
-  Proof.
-    destruct b; reflexivity.
-  Qed.
-  
-  Local Ltac ecancel_assumption ::= ecancel_assumption_impl.
+    Lemma cswap_same {A} b (a : A): cswap b a a = (a,a).
+    Proof.
+      destruct b; reflexivity.
+    Qed.
+    
+    Local Ltac ecancel_assumption ::= ecancel_assumption_impl.
 
-  Lemma scalarbits_bound : scalarbits < 2 ^ width.
-  Proof.
-    rewrite <- scalarbits_small.
-    unfold word.wrap.
-    apply Z_mod_lt.
-    pose proof word.width_pos.
-    pose proof (Z.pow_pos_nonneg 2 width ltac:(lia)).
-    lia.
-  Qed.
+    Lemma scalarbits_bound : scalarbits < 2 ^ width.
+    Proof.
+      rewrite <- scalarbits_small.
+      unfold word.wrap.
+      apply Z_mod_lt.
+      pose proof word.width_pos.
+      pose proof (Z.pow_pos_nonneg 2 width ltac:(lia)).
+      lia.
+    Qed.
 
-  (* TODO: why doesn't `Existing Instance` work?
+    (* TODO: why doesn't `Existing Instance` work?
     Existing Instance spec_of_sctestbit.*)
-  Hint Extern 1 (spec_of sctestbit) =>
-  (simple refine (@spec_of_sctestbit _ _ _ _ _ _ _ _)) : typeclass_instances.
-  Hint Extern 1 (spec_of "ladderstep") =>
-  (simple refine (@spec_of_ladderstep _ _ _ _ _ _ _ _)) : typeclass_instances.
+    Hint Extern 1 (spec_of sctestbit) =>
+    (simple refine (@spec_of_sctestbit _ _ _ _ _ _ _ _)) : typeclass_instances.
+    Hint Extern 1 (spec_of "ladderstep") =>
+    (simple refine (@spec_of_ladderstep _ _ _ _ _ _ _ _)) : typeclass_instances.
 
-  (* TODO: this seems a bit delicate*)
-  Ltac compile_cswap :=
-    eapply compile_felem_cswap;
-    [solve[repeat compile_step] ..
-    | repeat compile_step;
-      rewrite cswap_same;
-      compile_step;      
-      match goal with
-      | [|- (WeakestPrecondition.cmd _ _ _ _ _ (_ (let (_,_) := ?v in _)))] =>
-        destruct v
-      end].
-  
-  Hint Extern 8 (WeakestPrecondition.cmd _ _ _ _ _ (_ (nlet_eq _ (cswap _ _ _) _))) =>
-  compile_cswap; shelve : compiler.
+    (* TODO: this seems a bit delicate*)
+    Ltac compile_cswap :=
+      eapply compile_felem_cswap;
+      [solve[repeat compile_step] ..
+      | repeat compile_step;
+        rewrite cswap_same;
+        compile_step;      
+        match goal with
+        | [|- (WeakestPrecondition.cmd _ _ _ _ _ (_ (let (_,_) := ?v in _)))] =>
+          destruct v
+        end].
+    
+    Hint Extern 8 (WeakestPrecondition.cmd _ _ _ _ _ (_ (nlet_eq _ (cswap _ _ _) _))) =>
+    compile_cswap; shelve : compiler.
     
     Ltac compile_ladderstep :=
       eapply compile_ladderstep;
@@ -265,18 +261,18 @@ Section __.
 
     Hint Extern 8 (word.unsigned (word.of_Z _) = _) =>
     simple eapply word_unsigned_of_Z_eq; [ ZnWords |] : compiler.
-  
-  Derive montladder_body SuchThat
-           (let args := ["OUT"; "K"; "U" (*;"X1"; "Z1"; "X2"; "Z2" *)] in
+    
+    Derive montladder_body SuchThat
+           (let args := ["OUT"; "K"; "U"] in
             let montladder : Syntax.func :=
                 ("montladder", (args, [], montladder_body)) in
-          ltac:(
-            let goal := Rupicola.Lib.Tactics.program_logic_goal_for_function
-                          montladder [felem_cswap; felem_copy; felem_small_literal;
-                                        sctestbit; "ladderstep"; inv; mul] in
-            exact (__rupicola_program_marker montladder_gallina -> goal)))
-         As montladder_correct.
-  Proof.
+            ltac:(
+              let goal := Rupicola.Lib.Tactics.program_logic_goal_for_function
+                            montladder [felem_cswap; felem_copy; felem_small_literal;
+                                       sctestbit; "ladderstep"; inv; mul] in
+              exact (__rupicola_program_marker montladder_gallina -> goal)))
+           As montladder_correct.
+    Proof.
       pose proof scalarbits_pos.
       pose proof unsigned_of_Z_1.
       pose proof unsigned_of_Z_0.
@@ -310,13 +306,13 @@ Section __.
       compile_step.        
       solve[repeat compile_step].
       {
-          cbn [P2.car P2.cdr seps].
-          unfold v8 in *.
-          rewrite Heq in Heqv9.
-          inversion Heqv9; subst.
-          ecancel_assumption.
+        cbn [P2.car P2.cdr seps].
+        unfold v8 in *.
+        rewrite Heq in Heqv9.
+        inversion Heqv9; subst.
+        ecancel_assumption.
       }
-  Qed.
+    Qed.
   End MontLadder.
 End __.
 
@@ -326,6 +322,6 @@ Import bedrock2.Syntax.Coercions.
 Local Unset Printing Coercions.
 (* Set the printing width so that arguments are printed on 1 line.
    Otherwise the build breaks.
-*)
+ *)
 Local Set Printing Width 160.
 Redirect "Crypto.Bedrock.Group.ScalarMult.MontgomeryLadder.montladder_body" Print montladder_body.
