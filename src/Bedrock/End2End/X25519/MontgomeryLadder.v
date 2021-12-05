e53e02b6c1195325fd22f19be21c1afd977ae19b
Jade Philipoom <jade@mit.edu>
2021-12-05 13:19:53 -0500

    finish proof about compiler output and factor out remaining admits
    WIP, working on compiler proof
    
    compiler proof statement
    
    wip separating proof
    
    WIP
    
    wip, changed ext_spec in field25519 to be parameterized
    
    wip
    
    finish proof about compiler output and factor out remaining admits

--- src/Bedrock/End2End/X25519/MontgomeryLadder.v
+++ src/Bedrock/End2End/X25519/MontgomeryLadder.v
@@ -2,11 +2,14 @@ Require Import Coq.Strings.String.
 Require Import Coq.Lists.List.
 Require Import Coq.ZArith.ZArith.
 Require Import Crypto.Spec.Curve25519.
+Require Import bedrock2.Map.Separation.
 Require Import bedrock2.Syntax.
 Require Import compiler.Pipeline.
 Require Import compiler.MMIO.
+Require Import coqutil.Word.Bitwidth32.
 Require Import Crypto.Arithmetic.PrimeFieldTheorems.
 Require Import Crypto.Bedrock.End2End.X25519.Field25519.
+Require Import Crypto.Bedrock.Field.Interface.Compilation2.
 Require Import Crypto.Bedrock.Field.Synthesis.New.UnsaturatedSolinas.
 Require Import Crypto.Bedrock.Group.AdditionChains.
 Require Import Crypto.Bedrock.Group.ScalarMult.LadderStep.
@@ -15,17 +18,24 @@ Require Import Crypto.Bedrock.Group.ScalarMult.MontgomeryLadder.
 Local Open Scope string_scope.
 Import ListNotations.
 
-Definition ladderstep : func :=
-  Eval vm_compute in
-    (ladderstep_body
-      (field_parameters:=field_parameters)
-      (field_representaton:=field_representation n s c)).
 
-Definition montladder : func :=
-  Eval vm_compute in
-    (montladder_body (Z.to_nat (Z.log2_up Curve25519.l))
-      (field_parameters:=field_parameters)
-      (field_representaton:=field_representation n s c)).
+Derive ladderstep SuchThat
+       (ladderstep = ladderstep_body
+                       (BW:=BW32)
+                       (field_parameters:=field_parameters)
+                       (field_representaton:=field_representation n s c))
+       As ladderstep_defn.
+Proof. vm_compute. subst; exact eq_refl. Qed.
+
+Derive montladder SuchThat
+       (montladder
+        = montladder_body
+            (BW:=BW32)
+            (Z.to_nat (Z.log2_up Curve25519.l))
+            (field_parameters:=field_parameters)
+            (field_representaton:=field_representation n s c))
+       As montladder_defn.
+Proof. vm_compute. subst; exact eq_refl. Qed.
 
 Definition funcs : list func :=
   [
@@ -48,8 +58,11 @@ Require Import bedrock2.ToCString.
 Compute c_module funcs.
 *)
 
-Definition compiler_test_out := Eval vm_compute in
-  compile (compile_ext_call (funname_env:=SortedListString.map)) (map.of_list funcs).
-Compute
-  match compiler_test_out with
-  Success ((x, z), y) => Success (length x, z) | Failure e => Failure e end.
+Derive montladder_compiler_result SuchThat
+       (compile
+         (compile_ext_call (funname_env:=SortedListString.map))
+         (map.of_list funcs) = Success montladder_compiler_result)
+       As montladder_compiler_result_ok.
+Proof.
+  vm_compute. apply f_equal. subst; exact eq_refl.
+Qed.
---
+++ src/Bedrock/End2End/X25519/MontgomeryLadderProperties.v
@@ -0,0 +1,321 @@
+Require Import Coq.ZArith.ZArith.
+Require Import bedrock2.Map.Separation.
+Require Import bedrock2.Map.SeparationLogic.
+Require Import bedrock2.Syntax.
+Require Import compiler.Pipeline.
+Require Import compiler.MMIO.
+Require Import coqutil.Map.SortedListWord.
+Require Import coqutil.Map.Z_keyed_SortedListMap.
+Require Import coqutil.Word.Bitwidth32.
+Require Import Crypto.Arithmetic.PrimeFieldTheorems.
+Require Import Crypto.Bedrock.End2End.X25519.Field25519.
+Require Import Crypto.Bedrock.End2End.X25519.MontgomeryLadder.
+Require Import Crypto.Bedrock.Field.Synthesis.New.UnsaturatedSolinas.
+Require Import Crypto.Bedrock.Specs.Field.
+Require Import Crypto.Bedrock.Field.Interface.Compilation2.
+Require Import Crypto.Bedrock.Group.ScalarMult.MontgomeryLadder.
+
+Local Arguments map.rep: clear implicits.
+
+Definition montladder_insns := fst (fst montladder_compiler_result).
+Definition montladder_finfo := snd (fst montladder_compiler_result).
+Definition montladder_stack_size := snd montladder_compiler_result.
+
+Definition fname := fst montladder.
+Definition argnames := fst (fst (snd montladder)).
+Definition retnames := snd (fst (snd montladder)).
+Definition fbody := snd (snd montladder).
+Definition f_rel_pos : Z.
+  let x := constr:(map.get montladder_finfo fname) in
+  let y := eval vm_compute in x in
+  match y with
+  | Some (_, ?pos) => exact pos
+  end.
+Defined.
+
+Local Instance mem : map.map (word.rep (width:=32)) Init.Byte.byte := SortedListWord.map _ _.
+Local Existing Instance BW32.
+
+(* Postcondition extracted from spec_of_montladder *)
+Definition montladder_post (pOUT pK pU : word.rep (word:=BasicC32Semantics.word))
+          (Kbytes : list Byte.byte) (K : Z)
+          (U : F (Field.M_pos (FieldParameters:=field_parameters)))
+          (OUT : F (Field.M_pos (FieldParameters:=field_parameters)))
+          (R : map.rep word.rep Init.Byte.byte mem -> Prop)
+          (tr : Semantics.trace) :
+  Semantics.trace -> map.rep _ _ mem -> list (word.rep (word:=BasicC32Semantics.word)) -> Prop :=
+   (fun (tr' : Semantics.trace)
+        (mem' : map.rep word.rep Init.Byte.byte mem)
+        (rets : list word.rep) =>
+      rets = [] /\
+      tr' = tr /\
+      (FElem
+         (BW:=BW32)
+         (field_representaton:=field_representation n s c)
+         (Some Field.tight_bounds) pOUT
+         (montladder_gallina Field.M_pos Field.a24 (Z.to_nat (Z.log2_up Curve25519.l)) K U)
+         ⋆ Array.array ptsto (word.of_Z 1) pK Kbytes
+         ⋆ FElem (BW:=BW32)
+                 (field_representaton:=field_representation n s c)
+                 (Some Field.tight_bounds) pU U ⋆ R)%sep
+        mem').
+
+Local Instance Registers : map.map Z (@word.rep 32 BasicC32Semantics.word)
+  := Zkeyed_map _.
+
+Require Import riscv.Spec.Decode.
+
+Local Existing Instance RV32I_bitwidth.
+
+(* TODO: does something like this already exist? *)
+(* when the function name being called is not first in the list of functions,
+   peel off non-matching names *)
+Local Ltac prepare_call_step :=
+  lazymatch goal with
+  | |- ?call (?f :: ?funcs) ?fname ?t ?m ?args ?post =>
+    assert_fails (unify (fst f) fname);
+    let H := fresh in
+    assert (call funcs fname t m args post) as H;
+    [ | remember funcs; rewrite (surjective_pairing f);
+        cbn [WeakestPrecondition.call WeakestPrecondition.call_body ];
+        lazymatch goal with |- context[if (String.eqb ?x ?y) then _ else _] =>
+          let x' := (eval vm_compute in x) in change x with x';
+          let y' := (eval vm_compute in y) in change y with y';
+          destr (String.eqb x' y'); [ congruence | ]
+        end; exact H ]
+  end.
+Local Ltac prepare_call := repeat prepare_call_step.
+
+
+(* TODO: move to Spec.Field? *)
+Section Generic.
+  Context {width : Z} {BW : Bitwidth width} {word : word width}
+          {mem : map.map word.rep (Init.Byte.byte : Type)}
+          {locals : map.map string (word.rep (word:=word))}
+          {ext_spec : Semantics.ExtSpec}
+          {field_parameters : FieldParameters}
+          {field_representation : FieldRepresentation}.
+        
+  Lemma peel_func_binop
+      {name} (op : BinOp name) funcs0 funcs :
+    fst funcs0 <> name ->
+    spec_of_BinOp op funcs ->
+    spec_of_BinOp op (funcs0 :: funcs).
+  Proof.
+    cbv [spec_of_BinOp binop_spec]; intros.
+    cbn [WeakestPrecondition.call WeakestPrecondition.call_body ].
+    destruct funcs0; cbn [fst snd] in *.
+    lazymatch goal with |- context[if (String.eqb ?x ?y) then _ else _] =>
+      destr (String.eqb x y); [ congruence | ]
+    end.
+    eauto.
+  Qed.
+  
+  Lemma peel_func_unop
+      {name} (op : UnOp name) funcs0 funcs :
+    fst funcs0 <> name ->
+    spec_of_UnOp op funcs ->
+    spec_of_UnOp op (funcs0 :: funcs).
+  Proof.
+    cbv [spec_of_UnOp unop_spec]; intros.
+    cbn [WeakestPrecondition.call WeakestPrecondition.call_body ].
+    destruct funcs0; cbn [fst snd] in *.
+    lazymatch goal with |- context[if (String.eqb ?x ?y) then _ else _] =>
+      destr (String.eqb x y); [ congruence | ]
+    end.
+    eauto.
+  Qed.
+  
+End Generic.
+
+Lemma valid_funs_funcs : ExprImp.valid_funs (map.of_list funcs).
+Proof.
+  unfold ExprImp.valid_funs. unfold funcs.
+  (* TODO: need lemma/tactic to break down map.get (map.of_list [...]) into a
+     case for each function, then valid_fun is just proving there are no
+     duplicates in arg/ret names *)
+Admitted.
+
+(* TODO: replace with real cswap correctness proof once it exists *)
+Lemma fe25519_cswap_correct
+    {ext_spec : Semantics.ExtSpec}
+    {ext_spec_ok : Semantics.ext_spec.ok ext_spec} :
+  forall functions,
+    CSwap.spec_of_cswap
+      (field_parameters:=field_parameters)
+      (field_representaton:=field_representation n s c)
+      (felem_cswap :: functions). 
+Admitted.
+
+(* TODO: replace with real copy correctness proof once it exists *)
+Lemma fe25519_copy_correct
+    {ext_spec : Semantics.ExtSpec}
+    {ext_spec_ok : Semantics.ext_spec.ok ext_spec} :
+  forall functions,
+    spec_of_felem_copy
+      (field_parameters:=field_parameters)
+      (field_representation:=field_representation n s c)
+      (fe25519_copy :: functions).
+Admitted.
+
+(* TODO: replace with real small-literal correctness proof once it exists *)
+Lemma fe25519_small_literal_correct
+    {ext_spec : Semantics.ExtSpec}
+    {ext_spec_ok : Semantics.ext_spec.ok ext_spec} :
+  forall functions,
+    spec_of_felem_small_literal
+      (field_parameters:=field_parameters)
+      (field_representation:=field_representation n s c)
+      (fe25519_small_literal :: functions).
+Admitted.
+
+(* TODO: replace with real inv correctness proof once it exists *)
+Lemma fe25519_inv_correct
+    {ext_spec : Semantics.ExtSpec}
+    {ext_spec_ok : Semantics.ext_spec.ok ext_spec} :
+  forall functions,
+    spec_of_UnOp un_inv
+      (field_parameters:=field_parameters)
+      (field_representation:=field_representation n s c)
+      (AdditionChains.fe25519_inv
+         (word:=BasicC32Semantics.word)
+         (field_parameters:=field_parameters) :: functions).
+Admitted.
+
+(* TODO: fill this in. Naive.word may need to be adjusted for behavior in the
+   (out of spec) case when shift arguments are out of range. *)
+Local Instance riscv_ok_word : RiscvWordProperties.word.riscv_ok BasicC32Semantics.word.
+Admitted.
+
+Lemma montladder_compiles_correctly :
+  forall (t : Semantics.trace)
+         (mH : map.rep word.rep Init.Byte.byte mem)
+         (pOUT pK pU : word.rep) (out_bound : option Field.bounds)
+         (OUT U: F Field.M_pos) (Kbytes : list Byte.byte) (K : Z)
+         (initial : MetricRiscvMachine)
+         (stack_lo stack_hi ret_addr p_funcs : word.rep)
+         (R Rdata Rexec : map.rep word.rep Init.Byte.byte mem -> Prop),
+         LittleEndianList.le_combine Kbytes = K ->
+         length Kbytes = 32%nat ->
+         (FElem (BW:=BW32)
+                (field_representaton:=field_representation n s c)
+                out_bound pOUT OUT
+          ⋆ Array.array ptsto (word.of_Z 1) pK Kbytes
+          ⋆  FElem (BW:=BW32)
+                   (field_representaton:=field_representation n s c)
+                   (Some Field.tight_bounds) pU U
+          ⋆ R)%sep mH ->
+         montladder_stack_size <=
+         word.unsigned (word.sub stack_hi stack_lo) /
+         SeparationLogic.bytes_per_word ->
+         word.unsigned (word.sub stack_hi stack_lo)
+         mod SeparationLogic.bytes_per_word = 0 ->
+         getPc (getMachine initial) = word.add p_funcs (word.of_Z f_rel_pos) ->
+         map.get (getRegs (getMachine initial)) RegisterNames.ra =
+         Some ret_addr ->
+         word.unsigned ret_addr mod 4 = 0 ->
+         map.getmany_of_list (getRegs (getMachine initial))
+           (firstn (Datatypes.length argnames) (reg_class.all reg_class.arg)) =
+         Some [pOUT; pK; pU] ->
+         getLog (getMachine initial) = t ->
+         LowerPipeline.machine_ok p_funcs stack_lo stack_hi montladder_insns
+           mH Rdata Rexec initial ->
+         FlatToRiscvCommon.runsTo initial
+           (fun final : MetricRiscvMachine =>
+            exists
+              (mH' : map.rep word.rep Init.Byte.byte mem)
+            (retvals : list word.rep),
+              map.getmany_of_list (getRegs (getMachine final))
+                (firstn (Datatypes.length retnames)
+                   (reg_class.all reg_class.arg)) =
+              Some retvals /\
+              montladder_post pOUT pK pU Kbytes K U OUT R t
+                 (getLog (getMachine final)) mH' retvals /\
+              map.agree_on LowerPipeline.callee_saved
+                (getRegs (getMachine initial)) (getRegs (getMachine final)) /\
+              getPc (getMachine final) = ret_addr /\
+              LowerPipeline.machine_ok p_funcs stack_lo stack_hi
+                montladder_insns mH' Rdata Rexec final).
+Proof.
+  intros.
+  eapply (compiler_correct_wp
+            (ext_spec:=FE310CSemantics.ext_spec)
+            (string_keyed_map := SortedListString.map)
+            (string_keyed_map_ok := SortedListString.ok))
+    with (fname:=fname).
+  all:cbn [fname montladder fst snd].
+
+  (* fill in easy subgoals that instantiate evars *)
+  all:lazymatch goal with
+      | |- compile _ _ = Success _ =>
+        rewrite montladder_compiler_result_ok;
+        transitivity (Success (montladder_insns,
+                               montladder_finfo,
+                               montladder_stack_size));
+        [ | reflexivity ]; reflexivity
+      | _ => idtac
+      end.
+  all:lazymatch goal with
+      | |- getPc (getMachine _) = _ => eassumption
+      | |- getLog (getMachine _) = _ => eassumption
+      | |- map.get (getRegs (getMachine _)) _ = Some _ => eassumption
+      | |- map.getmany_of_list
+             (getRegs (getMachine _)) _ = Some _ => eassumption
+      | |- context [LowerPipeline.machine_ok] => eassumption
+      | |- map.get montladder_finfo _ = Some _ => reflexivity
+      | _ => idtac
+      end.
+  
+  (* solve remaining goals one by one *)
+  { eapply (compile_ext_call_correct (funname_env_ok:=SortedListString.ok)). }
+  { intros. cbv [compile_ext_call compile_interact].
+    repeat (destruct_one_match; try reflexivity). }
+  { apply valid_funs_funcs. }
+  { lazy. repeat constructor; cbn [In]; intuition idtac; congruence. }
+  { unfold funcs. 
+    (* montladder is not at the front of the function list; remove everything
+       that doesn't match the name *)
+    prepare_call.
+    (* use montladder correctness proof *)
+    rewrite montladder_defn.
+    eapply @montladder_correct; try (typeclasses eauto).
+    { apply Signature.field_representation_ok.
+      apply UnsaturatedSolinas.relax_valid. }
+    { reflexivity. }
+    { cbv [Core.__rupicola_program_marker]. tauto. }
+    { apply fe25519_cswap_correct. }
+    { apply fe25519_copy_correct. }
+    { cbv [spec_of_felem_small_literal].
+      intros; prepare_call.
+      eapply @fe25519_small_literal_correct;
+        try (typeclasses eauto); ecancel_assumption. }
+    { cbv [LadderStep.spec_of_ladderstep]; intros. 
+      prepare_call. rewrite ladderstep_defn.
+      eapply @LadderStep.ladderstep_correct; try (typeclasses eauto).
+      { apply Signature.field_representation_ok.
+        apply UnsaturatedSolinas.relax_valid. }
+      { cbv [Core.__rupicola_program_marker]; tauto. }
+      { repeat (apply peel_func_binop; [ lazy; congruence | ]).
+        apply fe25519_mul_correct. }
+      { repeat (apply peel_func_binop; [ lazy; congruence | ]).
+        apply fe25519_add_correct. }
+      { repeat (apply peel_func_binop; [ lazy; congruence | ]).
+        apply fe25519_sub_correct. }
+      { repeat (apply peel_func_binop; [ lazy; congruence | ]).
+        apply fe25519_square_correct. }
+      { repeat (apply peel_func_binop; [ lazy; congruence | ]).
+        apply fe25519_scmula24_correct. }
+      { ecancel_assumption. } }
+    { repeat (apply peel_func_unop; [ lazy; congruence | ]).
+      apply fe25519_inv_correct. }
+    { repeat (apply peel_func_unop; [ lazy; congruence | ]).
+      apply fe25519_mul_correct. }
+    { ssplit; try eassumption; [ ].
+      lazymatch goal with H : length Kbytes = _ |- _ => rewrite H end. 
+      lazy; congruence. } }
+  { assumption. }
+  { assumption. }
+  { assumption. }
+Qed.
+
+(* Print Assumptions montladder_compiles_correctly. *)
