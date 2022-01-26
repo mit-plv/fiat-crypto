083a3efe2eceb8e79caac93a50fba32c95c73bea
Jason Gross <jgross@mit.edu>
2022-01-26 08:27:20 -0800

    Remove duplicate file

--- src/Util/ChangeInAll.v
+++
@@ -1,7 +0,0 @@
-(** Work around "Cannot create self-referring hypothesis" coming from
-    [change x with y in *] *)
-Ltac change_in_all from to :=
-  change from with to;
-  repeat match goal with
-         | [ H : _ |- _ ] => progress change from with to in H
-         end.
