cff358a16ed99176393792e0ecfbcb691a7cec15
Jason Gross <jgross@mit.edu>
2022-05-16 14:23:01 +0100

    Add support for `is_true` relations in `setoid_subst_rel` (#1233)

--- src/Util/Tactics/SetoidSubst.v
+++ src/Util/Tactics/SetoidSubst.v
@@ -7,6 +7,14 @@ Ltac setoid_subst'' R x :=
     => free_in x y; rewrite ?H in *; clear x H
   | [ H : R ?y x |- _ ]
     => free_in x y; rewrite <- ?H in *; clear x H
+  | [ H : is_true (R x ?y) |- _ ]
+    => free_in x y; rewrite ?H in *; clear x H
+  | [ H : is_true (R ?y x) |- _ ]
+    => free_in x y; rewrite <- ?H in *; clear x H
+  | [ H : R x ?y = true |- _ ]
+    => free_in x y; change (is_true (R x y)) in H; rewrite ?H in *; clear x H
+  | [ H : R ?y x = true |- _ ]
+    => free_in x y; change (is_true (R y x)) in H; rewrite <- ?H in *; clear x H
   end.
 
 Ltac setoid_subst' x :=
@@ -14,6 +22,10 @@ Ltac setoid_subst' x :=
   match goal with
   | [ H : ?R x _ |- _ ] => setoid_subst'' R x
   | [ H : ?R _ x |- _ ] => setoid_subst'' R x
+  | [ H : is_true (?R x _) |- _ ] => setoid_subst'' R x
+  | [ H : is_true (?R _ x) |- _ ] => setoid_subst'' R x
+  | [ H : ?R x _ = true |- _ ] => setoid_subst'' R x
+  | [ H : ?R _ x = true |- _ ] => setoid_subst'' R x
   end.
 
 Ltac setoid_subst_rel' R :=
@@ -21,6 +33,10 @@ Ltac setoid_subst_rel' R :=
   match goal with
   | [ H : R ?x _ |- _ ] => setoid_subst'' R x
   | [ H : R _ ?x |- _ ] => setoid_subst'' R x
+  | [ H : is_true (R ?x _) |- _ ] => setoid_subst'' R x
+  | [ H : is_true (R _ ?x) |- _ ] => setoid_subst'' R x
+  | [ H : R ?x _ = true |- _ ] => setoid_subst'' R x
+  | [ H : R _ ?x = true |- _ ] => setoid_subst'' R x
   end.
 
 Ltac setoid_subst_rel R := repeat setoid_subst_rel' R.
@@ -29,6 +45,10 @@ Ltac setoid_subst_all :=
   repeat match goal with
          | [ H : ?R ?x ?y |- _ ] => is_var x; setoid_subst'' R x
          | [ H : ?R ?x ?y |- _ ] => is_var y; setoid_subst'' R y
+         | [ H : is_true (?R ?x ?y) |- _ ] => is_var x; setoid_subst'' R x
+         | [ H : is_true (?R ?x ?y) |- _ ] => is_var y; setoid_subst'' R y
+         | [ H : ?R ?x ?y = true |- _ ] => is_var x; setoid_subst'' R x
+         | [ H : ?R ?x ?y = true |- _ ] => is_var y; setoid_subst'' R y
          end.
 
 Tactic Notation "setoid_subst" ident(x) := setoid_subst' x.
