12693b094b52d81d7c1eac45ee13502e1f69b70c
Jason Gross <jgross@mit.edu>
2022-05-24 16:15:46 +0100

    Add `{NoDupA,SortA}_flat_map`

--- src/Bedrock/Everything.v
+++ src/Bedrock/Everything.v
@@ -297,6 +297,7 @@ Crypto.Util.ListUtil.Permutation
 Crypto.Util.ListUtil.PermutationCompat
 Crypto.Util.ListUtil.RemoveN
 Crypto.Util.ListUtil.SetoidList
+Crypto.Util.ListUtil.SetoidListFlatMap
 Crypto.Util.ListUtil.Split
 Crypto.Util.ListUtil.StdlibCompat
 Crypto.Util.Listable
