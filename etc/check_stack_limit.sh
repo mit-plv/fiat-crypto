326f3025ebc3c1906e4f1f61345fc8484b6f28b1
Jason Gross <jgross@mit.edu>
2022-10-04 22:56:55 +0530

    Use printf rather than echo as per https://github.com/mit-plv/fiat-crypto/pull/1407#issuecomment-1267188636

--- etc/check_stack_limit.sh
+++ etc/check_stack_limit.sh
@@ -4,10 +4,9 @@ set -eu
 
 stacksize="$(ulimit -s || true)"
 recstacksize=32768
-nl=$'\n'
 suggest="Warning: We recommend a stack size of at least $recstacksize kbytes (set with ulimit -S -s $recstacksize) to avoid stack overflows in OCaml compilation."
-test ! -z "$stacksize" || { >&2 echo "Warning: Could not determine stack size.${nl}${suggest}"; exit 1; }
+test ! -z "$stacksize" || { >&2 printf "Warning: Could not determine stack size.\n%s\n" "${suggest}"; exit 1; }
 if [ "$stacksize" == "unlimited" ]; then exit 0; fi
 # test for integer stacksize
-test "$stacksize" -eq "$stacksize" 2>/dev/null || { >&2 echo "Warning: Stack size ($stacksize) could not be parsed as a number.${nl}${suggest}"; exit 1; }
-test "$stacksize" -ge "$recstacksize" || { >&2 echo "Warning: Stack size ($stacksize) may be too small.${nl}${suggest}"; exit 1; }
+test "$stacksize" -eq "$stacksize" 2>/dev/null || { >&2 printf "Warning: Stack size (%s) could not be parsed as a number.\n%s\n" "$stacksize"  "${suggest}"; exit 1; }
+test "$stacksize" -ge "$recstacksize" || { >&2 printf "Warning: Stack size (%s) may be too small.\n%s\n" "$stacksize" "${suggest}"; exit 1; }
