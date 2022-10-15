e185777c3e6cd710132ca03fe6890aeb9c58210a
Jason Gross <jgross@mit.edu>
2022-10-15 09:31:34 +0530

    Update ensure_stack_limit.sh to be POSIX-compliant
    Fixes #1437

--- etc/ensure_stack_limit.sh
+++ etc/ensure_stack_limit.sh
@@ -7,7 +7,8 @@ if command -v ulimit &>/dev/null; then
     (
         stacksize="$(ulimit -S -s)"
         test ! -z "$stacksize" || { >&2 printf "Warning: Could not determine stack size.\n"; exit 1; }
-        if [ "$stacksize" == "unlimited" ]; then exit 0; fi
+        # unlimited stack is fine.  N.B. == is not fully POSIX-compliant, so we need != or ==
+        test "$stacksize" != "unlimited" || exit 0
         # test for integer stacksize
         test "$stacksize" -eq "$stacksize" 2>/dev/null || { >&2 printf "Warning: Stack size (%s) could not be parsed as a number.\n" "$stacksize"; exit 1; }
         test "$stacksize" -ge "$recstacksize" || { >&2 printf "Warning: Stack size (%s) may be too small.\n" "$stacksize"; exit 1; }
