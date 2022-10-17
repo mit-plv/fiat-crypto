868aad560344321676217b3cd1e741488ac5f9c2
Jason Gross <jgross@mit.edu>
2022-10-17 11:31:49 +0530

    Be more robust to ulimit failures (#1445)
    See https://github.com/coq/coq/pull/16671#issuecomment-1280142881

--- etc/ensure_stack_limit.sh
+++ etc/ensure_stack_limit.sh
@@ -4,8 +4,9 @@ set -eu
 
 recstacksize=32768
 if command -v ulimit >/dev/null 2>/dev/null; then
+    hardstacksize="$(ulimit -H -s || true)"
     (
-        stacksize="$(ulimit -S -s)"
+        stacksize="$(ulimit -S -s || true)"
         test ! -z "$stacksize" || { >&2 printf "Warning: Could not determine stack size.\n"; exit 1; }
         # unlimited stack is fine.  N.B. == is not fully POSIX-compliant, so we need != or ==
         test "$stacksize" != "unlimited" || exit 0
@@ -13,8 +14,8 @@ if command -v ulimit >/dev/null 2>/dev/null; then
         test "$stacksize" -eq "$stacksize" 2>/dev/null || { >&2 printf "Warning: Stack size (%s) could not be parsed as a number.\n" "$stacksize"; exit 1; }
         test "$stacksize" -ge "$recstacksize" || { >&2 printf "Warning: Stack size (%s) may be too small.\n" "$stacksize"; exit 1; }
     ) || {
-        >&2 printf "Warning: To avoid stack overflows in OCaml compilation, setting stack size to the recommended minimum of %s kbytes\n" "$recstacksize";
-        ulimit -S -s "$recstacksize";
+        >&2 printf "Warning: To avoid stack overflows in OCaml compilation, setting stack size to the recommended minimum of %s kbytes\n" "$recstacksize"
+        ulimit -S -s "$recstacksize" || { >&2 printf "Warning: Failed to set stack size to %s, possibly because the hard stack size limit is %s.\n" "$recstacksize" "$hardstacksize"; exit 1; }
     }
 else
     >&2 printf "Warning: Could not determine stack size because ulimit was not found.\nWarning: We recommend a stack size of at least %s kbytes to avoid stack overflows in OCaml compilation.\n" "$recstacksize"
