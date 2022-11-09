c3a75495febf315696e35bacb67cac5bf1f66767
Jason Gross <jgross@mit.edu>
2022-11-09 13:03:24 -0500

    Add some ZUtil/Land/Fold.v lemmas

---
+++ src/Util/ZUtil/Land/Fold.v
@@ -0,0 +1,25 @@
+Require Import Coq.ZArith.ZArith.
+Require Import Coq.micromega.Lia.
+Require Import Coq.Lists.List.
+Require Import Crypto.Util.ZUtil.Land.
+Require Import Crypto.Util.ZUtil.Hints.Core.
+Local Open Scope bool_scope. Local Open Scope Z_scope.
+
+Module Z.
+  Lemma fold_right_land_m1_cps v ls
+    : fold_right Z.land v ls
+      = Z.land (fold_right Z.land (-1) ls) v.
+  Proof.
+    induction ls as [|?? IH]; cbn [fold_right].
+    { now rewrite Z.land_m1'_l. }
+    { rewrite <- !Z.land_assoc, IH; reflexivity. }
+  Qed.
+
+  Lemma fold_right_land_ones_id sz ls
+    : Z.land (fold_right Z.land (Z.ones sz) ls) (Z.ones sz)
+      = Z.land (fold_right Z.land (-1) ls) (Z.ones sz).
+  Proof.
+    rewrite fold_right_land_m1_cps, <- Z.land_assoc, Z.land_diag.
+    reflexivity.
+  Qed.
+End Z.
