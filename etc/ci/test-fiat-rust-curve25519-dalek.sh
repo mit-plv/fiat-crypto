526e1be1c777b066b2e20d38921937544729a0df
Jason Gross <jgross@mit.edu>
2022-12-14 23:24:49 -0500

    Update test-fiat-rust-curve25519-dalek.sh

--- etc/ci/test-fiat-rust-curve25519-dalek.sh
+++ etc/ci/test-fiat-rust-curve25519-dalek.sh
@@ -14,7 +14,7 @@ cat >> Cargo.toml <<EOF
 fiat-crypto = { path = "../fiat-rust" }
 EOF
 
-cargo test --features="std fiat_backend" --no-default-features --target x86_64-unknown-linux-gnu || exit $?
-cargo test --features="std fiat_backend" --no-default-features --target i686-unknown-linux-gnu || exit $?
+RUSTFLAGS='--cfg curve25519_dalek_backend="fiat"' cargo test --target x86_64-unknown-linux-gnu || exit $?
+RUSTFLAGS='--cfg curve25519_dalek_backend="fiat"' cargo test --target i686-unknown-linux-gnu || exit $?
 
 popd >/dev/null
