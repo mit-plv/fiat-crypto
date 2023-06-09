68f7408e98e5cd61b551e0c707d2273aef33b4f6
Brian McSwiggen <mcswiggen@google.com>
2023-06-09 16:56:13 -0400

    simple square_and_add function

--- src/Bedrock/End2End/X25519/MontgomeryLadder.v
+++ src/Bedrock/End2End/X25519/MontgomeryLadder.v
@@ -16,34 +16,41 @@ Require Import Crypto.Bedrock.Group.ScalarMult.LadderStep.
 Require Import Crypto.Bedrock.Group.ScalarMult.CSwap.
 Require Import Crypto.Bedrock.Group.ScalarMult.MontgomeryLadder.
 Require Import Crypto.Bedrock.End2End.X25519.Field25519.
+Require Import Crypto.Bedrock.Specs.Field.
 Local Open Scope string_scope.
+Local Open Scope Z_scope.
 Import ListNotations.
 
 Local Instance frep25519 : Field.FieldRepresentation(field_parameters:=field_parameters) := field_representation n Field25519.s c.
-Derive ladderstep SuchThat (ladderstep = ladderstep_body) As ladderstep_defn.
-Proof. vm_compute. subst; exact eq_refl. Qed.
-
-Derive montladder SuchThat (montladder = montladder_body (Z.to_nat (Z.log2 Curve25519.order)))
-       As montladder_defn.
-Proof. vm_compute. subst; exact eq_refl. Qed.
 
 Require Import bedrock2.NotationsCustomEntry.
 
-Definition x25519 := func! (out, sk, pk) {
-  stackalloc 40 as U;
-  fe25519_from_bytes(U, pk);
-  stackalloc 40 as OUT;
-  montladder(OUT, sk, U);
-  fe25519_to_bytes(out, OUT)
+(* Definition square_and_add := func! (o, x, y) {
+  stackalloc 40 as tmp;
+  fe25519_square(tmp, x);
+  fe25519_add(o, tmp, y)
+}. *)
+
+Definition square_and_add := func! (o, x, y) {
+  fe25519_square(o, x);
+  fe25519_add(o, o, y)
 }.
 
-Definition x25519_base := func! (out, sk) {
-  stackalloc 40 as U;
-  fe25519_from_word(U, $9);
-  stackalloc 40 as OUT;
-  montladder(OUT, sk, U);
-  fe25519_to_bytes(out, OUT)
+Definition mul_and_add := func! (o, x, y) {
+  fe25519_mul(o, x, x);
+  fe25519_add(o, o, y)
 }.
+
+Require Import bedrock2.FE310CSemantics bedrock2.Semantics .
+Require Import bedrock2.WeakestPrecondition bedrock2.ProgramLogic.
+Require Import coqutil.Word.Interface coqutil.Word.Bitwidth.
+Require Import coqutil.Map.Interface bedrock2.Map.SeparationLogic.
+Require Import bedrock2.Array bedrock2.Scalars.
+Require Import bedrock2.ZnWords.
+Require Import coqutil.Tactics.Tactics.
+
+Section WithParameters.
+  Check _: FieldRepresentation.
 
 Import LittleEndianList.
 Local Coercion F.to_Z : F >-> Z.
@@ -53,74 +60,135 @@ Require Import coqutil.Map.OfListWord Coq.Init.Byte coqutil.Byte.
 Import ProgramLogic.Coercions.
 Local Notation "m =* P" := ((P%sep) m) (at level 70, only parsing) (* experiment*).
 Local Notation "xs $@ a" := (Array.array ptsto (word.of_Z 1) a xs) (at level 10, format "xs $@ a").
-Definition x25519_gallina := montladder_gallina (Field.M_pos(FieldParameters:=field_parameters)) Field.a24 (Z.to_nat (Z.log2 (Z.pos order))).
-Global Instance spec_of_x25519 : spec_of "x25519" :=
-  fnspec! "x25519" out sk pk / (o s p : list Byte.byte) (R : _ -> Prop),
-  { requires t m := m =* s$@sk * p$@pk * o$@out * R /\
-      length s = 32%nat /\ length p = 32%nat /\ length o = 32%nat /\ byte.unsigned (nth 31 p x00) <= 0x7f;
-    ensures t' m := t=t' /\ m=* s$@sk ⋆ p$@pk ⋆ R ⋆
-      (le_split 32 (x25519_gallina (le_combine s) (Field.feval_bytes p)))$@out }.
 
-Local Instance spec_of_fe25519_from_word : spec_of "fe25519_from_word" := Field.spec_of_from_word.
-Local Instance spec_of_fe25519_from_bytes : spec_of "fe25519_from_bytes" := Field.spec_of_from_bytes.
-Local Instance spec_of_fe25519_to_bytes : spec_of "fe25519_to_bytes" := Field.spec_of_to_bytes.
-Local Instance spec_of_montladder : spec_of "montladder" := spec_of_montladder(Z.to_nat (Z.log2 Curve25519.order)).
+Local Notation FElem := (FElem(FieldRepresentation:=frep25519)).
+Local Notation bounded_by := (bounded_by(FieldRepresentation:=frep25519)).
+Local Notation word := (Naive.word 32).
+Local Notation felem := (felem(FieldRepresentation:=frep25519)). 
+
+Global Instance spec_of_square_and_add : spec_of "square_and_add" :=
+  fnspec! "square_and_add" (out xk yk: word) / (o x y : felem) (R : _ -> Prop),
+  { requires t m :=
+      bounded_by loose_bounds x /\
+      bounded_by tight_bounds y /\
+      m =* (FElem xk x) * (FElem yk y) * (FElem out o) * R;
+    ensures t' m' :=
+      t=t' /\
+      exists o', feval o' = F.add (F.pow (feval x) 2) (feval y)
+        /\ bounded_by loose_bounds o'
+        /\ m' =* (FElem xk x) * (FElem yk y) * (FElem out o') * R }.
+
+Global Instance spec_of_mul_and_add : spec_of "mul_and_add" :=
+  fnspec! "mul_and_add" (out xk yk: word) / (o x y : felem) (R : _ -> Prop),
+  { requires t m :=
+      bounded_by loose_bounds x /\
+      bounded_by tight_bounds y /\
+      m =* (FElem xk x) * (FElem yk y) * (FElem out o) * R;
+    ensures t' m' :=
+      t=t' /\
+      exists o', feval o' = F.add (F.mul (feval x) (feval x)) (feval y)
+        /\ bounded_by loose_bounds o'
+        /\ m' =* (FElem xk x) * (FElem yk y) * (FElem out o') * R }.
+
+Local Instance spec_of_fe25519_square : spec_of "fe25519_square" := Field.spec_of_UnOp un_square.
+Local Instance spec_of_fe25519_mul : spec_of "fe25519_mul" := Field.spec_of_BinOp bin_mul.
+Local Instance spec_of_fe25519_add : spec_of "fe25519_add" := Field.spec_of_BinOp bin_add.
+
+Import WeakestPrecondition.
+
+From coqutil.Tactics Require Import Tactics letexists eabstract rdelta reference_to_string ident_of_string.
+Require Import bedrock2.Syntax.
+Require Import bedrock2.WeakestPrecondition.
+Require Import bedrock2.WeakestPreconditionProperties.
+Require Import bedrock2.Loops.
+Require Import bedrock2.Map.SeparationLogic bedrock2.Scalars.
 
 Local Arguments word.rep : simpl never.
 Local Arguments word.wrap : simpl never.
 Local Arguments word.unsigned : simpl never.
 Local Arguments word.of_Z : simpl never.
-Lemma x25519_ok : program_logic_goal_for_function! x25519.
+Lemma square_and_add_ok : program_logic_goal_for_function! square_and_add.
 Proof.
   repeat straightline.
-  seprewrite_in (@Bignum.Bignum_of_bytes _ _ _ _ _ _ 10 a) H2. { transitivity 40%nat; trivial. }
+  repeat match goal with
+  | |- _ /\ _ => split
+  | |- call _ _ _ _ _ _ => straightline_call
+  | _ => straightline
+  end.
+  (* square *)
+  - (* square precondition bounds on x *) apply H1.
+  - (* mem sep for x *) eexists. ecancel_assumption.
+  - (* mem sep for out *) ecancel_assumption.
+  (* add *)
+  - (* add precondition bounds on x^2 *) apply H7.
+  - (* add precondition bounds on y *) apply H2.
+  - (* mem sep for o (holding x^2) *) eexists. ecancel_assumption.
+  - (* mem sep for y *) eexists. ecancel_assumption.
+  - (* mem sep for o (holding x^2+y) *) ecancel_assumption.
+  (* post-conditions *)
+  - exists x1. split. 2:split.
+    + (* math adds up *) simpl in H9. simpl in H6. rewrite H6 in H9. simpl. apply H9. (* Definitely better tactics for this *)
+    + (* postcondition bounds on out *) simpl in H10. simpl. apply H10. (* should just work to apply H10... *)
+    + (* postcondition mem sep *) ecancel_assumption.
+Qed.
 
-  straightline_call; ssplit.
-  { eexists. ecancel_assumption. }
-  { cbv [Field.FElem].
-    use_sep_assumption. cancel. cancel_seps_at_indices 0%nat 0%nat; cbn; trivial. eapply RelationClasses.reflexivity. }
-  { unfold Field.bytes_in_bounds, frep25519, field_representation, Signature.field_representation, Representation.frep.
-    match goal with |- ?P ?x ?z => let y := eval cbv in x in change (P y z) end; cbn.
-    repeat (destruct p as [|? p]; try (cbn [length] in *;discriminate); []).
-    cbn; cbn [nth] in *.
-    cbv [COperationSpecifications.list_Z_bounded_by FoldBool.fold_andb_map map seq]; cbn.
-    pose proof byte.unsigned_range as HH. setoid_rewrite <-Le.Z.le_sub_1_iff in HH. cbn in HH.
-    setoid_rewrite Zle_is_le_bool in HH. setoid_rewrite <-Bool.andb_true_iff in HH.
-    rewrite 31HH; cbn.
-    eapply Bool.andb_true_iff; split; trivial.
-    eapply Bool.andb_true_iff; split; eapply Zle_is_le_bool; trivial.
-    eapply byte.unsigned_range. }
+Local Ltac unwrap_calls :=
+  repeat match goal with
+  | |- _ /\ _ => split
+  | |- call _ _ _ _ _ _ => straightline_call
+  | _ => straightline
+  end.
+
+Local Ltac solve_bounds :=
+  repeat match goal with
+  | H: bounded_by loose_bounds _ |- bounded_by loose_bounds _ => apply H
+  | H: bounded_by tight_bounds _ |- bounded_by tight_bounds _ => apply H
+  | H: bounded_by _ _ |- bounded_by _ _ => cbv [un_xbounds bin_xbounds bin_ybounds un_square bin_mul bin_add un_outbounds bin_outbounds] in *
+  end.
+
+Local Ltac solve_mem :=
+  repeat match goal with
+  | |- exists _, _%sep _ => eexists
+  | |- _%sep _ => ecancel_assumption
+  end.
+
+Lemma square_and_add_ok2 : program_logic_goal_for_function! square_and_add.
+Proof.
   repeat straightline.
+  unwrap_calls.
+  all: try solve_mem. all: try solve_bounds.
+  (* post-conditions *)
+  - exists x1. split. 2:split.
+    + (* math adds up *) simpl in H9. simpl in H6. rewrite H6 in H9. simpl. apply H9. (* Definitely better tactics for this *)
+    + (* postcondition bounds on out *) simpl in H10. simpl. apply H10. (* should just work to apply H10... *)
+    + (* postcondition mem sep *) ecancel_assumption.
+Qed.
 
-  seprewrite_in (@Bignum.Bignum_of_bytes _ _ _ _ _ _ 10 a2) H15. { transitivity 40%nat; trivial. }
-
-  straightline_call; ssplit.
-  3: { unfold FElem, Field.FElem in *; extract_ex1_and_emp_in_goal; ssplit.
-       { use_sep_assumption. cancel; repeat ecancel_step.
-       cancel_seps_at_indices 0%nat 0%nat; trivial. cbn; reflexivity. }
-    all : eauto.
-    { instantiate (1:=None). exact I. } }
-  { reflexivity. }
-  { rewrite H3. vm_compute. inversion 1. }
+Lemma mul_and_add_ok : program_logic_goal_for_function! mul_and_add.
+Proof.
   repeat straightline.
-
-  cbv [FElem] in H22. extract_ex1_and_emp_in H22.
-  straightline_call; ssplit.
-  { ecancel_assumption. }
-  { transitivity 32%nat; auto. }
-  { eexists. ecancel_assumption. }
-  { intuition idtac. }
-  repeat straightline_cleanup.
-  repeat straightline.
-
-  cbv [Field.FElem] in *.
-  seprewrite_in @Bignum.Bignum_to_bytes H25.
-  seprewrite_in @Bignum.Bignum_to_bytes H25.
-  extract_ex1_and_emp_in H25.
-
-  repeat straightline; intuition eauto.
-  rewrite H29 in *. cbv [x25519_gallina].
-  use_sep_assumption; cancel. eapply RelationClasses.reflexivity.
+  repeat match goal with
+  | |- _ /\ _ => split
+  | |- call _ _ _ _ _ _ => straightline_call
+  | _ => straightline
+  end.
+  (* square *)
+  - (* mul precondition bounds on x *) apply H1.
+  - (* same bounds gain *) apply H1.
+  - (* mem sep for x *) eexists. ecancel_assumption.
+  - (* same mem sep again *) eexists. ecancel_assumption.
+  - (* mem sep for out *) ecancel_assumption.
+  (* add *)
+  - (* add precondition bounds on x^2 *) apply H7.
+  - (* add precondition bounds on y *) apply H2.
+  - (* mem sep for o (holding x^2) *) eexists. ecancel_assumption.
+  - (* mem sep for y *) eexists. ecancel_assumption.
+  - (* mem sep for o (holding x^2+y) *) ecancel_assumption.
+  (* post-conditions *)
+  - exists x1. split. 2:split.
+    + (* math adds up *) simpl in H9. simpl in H6. rewrite H6 in H9. simpl. apply H9. (* Definitely better tactics for this *)
+    + (* postcondition bounds on out *) simpl in H10. simpl. apply H10. (* should just work to apply H10... *)
+    + (* postcondition mem sep *) ecancel_assumption.
 Qed.
 
 Global Instance spec_of_x25519_base : spec_of "x25519_base" :=
