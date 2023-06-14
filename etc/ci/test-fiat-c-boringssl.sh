5ef196152bada47ccb17167baa4530a4e77a49ad
Owen Conoly <owenc@mit.edu>
2023-06-14 16:46:43 -0400

    remove boringssl blacklist; use a whitelist instead

--- etc/ci/test-fiat-c-boringssl.sh
+++ etc/ci/test-fiat-c-boringssl.sh
@@ -26,10 +26,9 @@ pushd boringssl >/dev/null
 echo "::group::Patching BoringSSL"
 ({
     set -ex
-    ( cd third_party/fiat && for i in *.h; do
-          if [ "$i" != "curve25519_64_msvc.h" ] && [ "$i" != "p256_64_msvc.h" ]; then
-              cp "${SUBCOMPONENT_PATH}/${i/.h/.c}" "$i" || exit $?;
-          fi;
+    ( cd third_party/fiat &&
+      for i in "curve25519_32.h" "curve25519_64.h" "p256_32.h" "p256_64.h"; do
+        cp "${SUBCOMPONENT_PATH}/${i/.h/.c}" "$i" || exit $?
       done ) || exit $?
     ( cd third_party/fiat && git --no-pager diff )
 }) || exit $?
