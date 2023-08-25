c3aad15b5ebc6febbeb0053536d23d8165167e12
Jade Philipoom <jadep@google.com>
2023-08-25 16:34:01 +0200

    clean up debugging printouts and reset them to focus on mul, comment out things that rely on mul_split

--- src/Bedrock/Field/Synthesis/Examples/p224_64_new.v
+++ src/Bedrock/Field/Synthesis/Examples/p224_64_new.v
@@ -49,22 +49,26 @@ Section Field.
   Instance p224_ops : @word_by_word_Montgomery_ops from_mont_string to_mont_string _ _ _ _ _ _ _ _ _ _ _ _ _ (WordByWordMontgomery.n m machine_wordsize) m.
   Proof using Type. Time constructor; make_computed_op. Defined.
 
-
+  (* TODO: remove these debugging printouts! *)
   Goal False.
-    Local Notation ttype := Language.Compilers.type.type.
-    Local Notation tbase := Language.Compilers.type.base.
-    Local Notation ttype_base := Language.Compilers.base.type.type_base.
-    Local Notation btype := Language.Compilers.base.type.type.
-    Local Notation blist := Language.Compilers.base.type.list.
+    (* Import things to make names shorter *)
     Require Import IdentifiersBasicGENERATED.
-    Local Notation eAbs := Language.Compilers.expr.Abs.
-    Local Notation eApp := Language.Compilers.expr.App.
-    Local Notation eVar := Language.Compilers.expr.Var.
-    Local Notation eLetIn := Language.Compilers.expr.LetIn.
-    Local Notation eIdent := Language.Compilers.expr.Ident.
+    Require Import Rewriter.Language.Language.
+
     Set Printing Depth 100000.
-    pose add_op.
-    cbn [add_op p224_ops] in c.
+
+    (* The below lines will show the bedrock2 translation of modular
+       multiplication. Look for "ERROR" to see where in the translation ran into
+       an expression it couldn't handle. *)
+    pose (F:=b2_func mul_op).
+    lazy [b2_func mul_op p224_ops] in F.
+    clear F.
+
+    (* The below lines will show the fiat-crypto pipeline output expression for
+       modular multiplication. It's a big expression, so printing it takes a few
+       minutes. *)
+    pose (X:=res mul_op).
+    Time lazy [res mul_op p224_ops] in X.
 
   Abort.
 
@@ -99,148 +103,10 @@ Section Field.
         | |- (_ = _)%Z => vm_compute; reflexivity
         end.
 
-
-      Require Import Crypto.Language.API.
-      Import API.Compilers.
-      Print Func.valid_func_bool.
-      Locate API.expr.
-      Definition cmd_bool
-                 {t} (e : @API.expr (fun _ => unit) t) : bool :=
-        match e return bool with
-        | expr.LetIn
-            (type.base (base.type.prod
-                          (base.type.type_base a)
-                          (base.type.type_base b)))
-            (type.base c) x f =>
-          true
-        | expr.LetIn
-            (type.base (base.type.type_base a))
-            (type.base b) x f =>
-          true
-        | expr.App (type.base s) _ f x =>
-          true
-        | expr.Ident _ i => true
-        | _ => false
-        end.
-      Fixpoint func_bool {t} (e : @API.expr (fun _ => unit) t) : bool :=
-        match e with
-        | expr.Abs _ _ f =>func_bool (f tt)
-        | _ => cmd_bool e
-        end.
-      Lemma valid_expr_bool_if_base_LetIn {A B} (x : API.expr A) (f : unit -> API.expr B) :
-        Cmd.valid_expr_bool_if_base (expr.LetIn x f) = false.
-      Proof.
-        cbv [Cmd.valid_expr_bool_if_base].
-        destruct B; reflexivity.
-      Qed.
-      
-  Derive p224_add
+  Derive p224_from_bytes
          SuchThat (forall functions,
                       Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
                       Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
-                      spec_of_BinOp bin_add
-                        (field_representation:=field_representation m)
-                        (p224_add :: functions))
-         As p224_add_correct.
-  Proof.
-   Time derive_bedrock2_func add_op.
-    begin_derive_bedrock2_func.
-    4:{
-      eapply Func.valid_func_bool_iff.
-      cbn [add_op p224_ops res].
-      cbv [Func.valid_func_bool].
-      repeat  lazymatch goal with
-              | |- context [Func.valid_cmd_bool_if_base (eAbs ?x)] =>
-                change (Func.valid_cmd_bool_if_base (eAbs x)) with false
-              end.
-      cbv [Func.valid_cmd_bool_if_base].
-      cbv [Cmd.valid_cmd_bool].
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite valid_expr_bool_if_base_LetIn.
-      rewrite !valid_expr_bool_if_base_LetIn.
-      Set Printing Depth 100000.
-      repeat match goal with
-             | |- context [Cmd.valid_expr_bool_if_base ?x] =>
-               change (Cmd.valid_expr_bool_if_base x) with false
-             end.
-      cbn iota.
-      cbv [Cmd.valid_cons_App1_bool
-             Cmd.valid_cons_App2_bool
-             Cmd.is_cons_ident
-             Cmd.is_nil_ident].
-      repeat match goal with
-             | |- context [(Expr.valid_expr_bool true ?x || Cmd.valid_special_bool ?x)%bool] =>
-                          first [ change (Expr.valid_expr_bool true x) with true; cbn [orb]
-                                | change (Cmd.valid_special_bool x) with true; rewrite Bool.orb_true_r ]
-             | |- context [Expr.valid_expr_bool true ?x] =>
-               change (Expr.valid_expr_bool true x) with true
-             end.
-      cbn [orb andb].
-      match goal with
-      | |- context [Cmd.valid_special_bool ?x] =>
-        assert (Cmd.valid_special_bool x = true)
-      end.
-      { cbv [Cmd.valid_special_bool].
-        cbv [invert_expr.invert_App_cast].
-        rewrite Util.invert_App_Z_cast2_eq_Some.
-        cbn [fst snd].
-        lazymatch goal with
-        | |- context [Cmd.valid_special3_bool ?x ?r] =>
-          change (Cmd.valid_special3_bool x r) with false
-        end.
-        cbn [orb].
-        cbv [Cmd.valid_special4_bool].
-        rewrite Cmd.invert_AppIdent4_eq_Some.
-        cbv [Cmd.valid_ident_special4].
-        cbn [fst snd].
-        cbv [Cmd.is_add_with_get_carry_ident].
-        cbv [Cmd.is_sub_with_get_borrow_ident].
-        cbv [Expr.is_literalz].
-        rewrite Z.eqb_refl.
-        repeat lazymatch goal with
-               | |- context [Expr.valid_expr_bool true ?x] =>
-                 change (Expr.valid_expr_bool true x) with true
-               end.
-        cbn [andb].
-        cbv [Cmd.is_word_and_carry_range Expr.range_good Cmd.is_carry_range].
-        cbn [fst snd]. rewrite !ZRange.zrange_lb by reflexivity.
-        cbn [andb].
-        cbv [Cmd.valid_carry_bool].
-        Locate valid_carry_bool.
-        (* problem is that valid_carry_bool only matches cast, while it should also accept a 0/1 literal *)
-      }
-        
-      rewrite !Bool.orb_false_r.
-      lazymatch goal with
-      | |- context [Expr.valid_expr_bool true ?x] =>
-        pose (e:=Expr.valid_expr_bool true x);
-          assert (Expr.valid_expr true x)
-      end.
-      {
-        lazy in e.
-        Unset Printing Notations.
-        repeat lazymatch goal with
-               | |- Expr.valid_expr
-                     _ (expr.App (expr.App (expr.Ident Compilers.ident_Z_cast) _)  _) =>
-                 apply Expr.valid_cast1; [ | reflexivity .. ]
-               | |- Expr.valid_expr _ (expr.Var _) =>
-                 apply Expr.valid_var_z
-               | _ => apply Expr.valid_binop; [ cbn; congruence | | ]
-               end.
-        apply Expr.valid_zselect; try reflexivity; [ ].
-        apply Expr.valid_snd_cast.
-        (* valid_snd_cast requires that the range is good, but it's [0,1] *)
-
-   Time derive_bedrock2_func add_op.
-  Qed.
-  Print p224_add.
-
-  Derive p224_from_bytes
-         SuchThat (forall functions,
                       spec_of_from_bytes
                         (field_representation:=field_representation_raw m)
                         (p224_from_bytes :: functions))
@@ -249,30 +115,44 @@ Section Field.
 
   Derive p224_to_bytes
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_to_bytes
                         (field_representation:=field_representation_raw m)
                         (p224_to_bytes :: functions))
          As p224_to_bytes_correct.
   Proof. Time derive_bedrock2_func to_bytes_op. Qed.
 
+  (* TODO: this proof doesn't work because `mul_split` is not yet supported. *)
+  (*
   Derive p224_mul
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_BinOp bin_mul
                         (field_representation:=field_representation m)
                         (p224_mul :: functions))
          As p224_mul_correct.
   Proof. Time derive_bedrock2_func mul_op. Qed.
+   *)
 
+  (* TODO: this proof doesn't work because `mul_split` is not yet supported. *)
+  (*
   Derive p224_square
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_UnOp un_square
                         (field_representation:=field_representation m)
                         (p224_square :: functions))
          As p224_square_correct.
   Proof. Time derive_bedrock2_func square_op. Qed.
+   *)
 
   Derive p224_add
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_BinOp bin_add
                         (field_representation:=field_representation m)
                         (p224_add :: functions))
@@ -281,15 +161,21 @@ Section Field.
 
   Derive p224_sub
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_BinOp bin_sub
                         (field_representation:=field_representation m)
                         (p224_sub :: functions))
          As p224_sub_correct.
   Proof. Time derive_bedrock2_func sub_op. Qed.
 
+  (* TODO: this proof doesn't work because `mul_split` is not yet supported. *)
+  (*
   (*TODO: adapt derive_bedrock2_func to also derive the remaining functions.*)
   Derive p224_from_mont
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_UnOp un_from_mont
                         (field_representation:=field_representation m)
                         (p224_from_mont :: functions))
@@ -302,9 +188,14 @@ Section Field.
             + auto.
             + auto.
   Qed.
+  *)
 
+  (* TODO: this proof doesn't work because `mul_split` is not yet supported. *)
+  (*
   Derive p224_to_mont
          SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_UnOp un_to_mont
                         (field_representation:=field_representation m)
                         (p224_to_mont :: functions))
@@ -315,9 +206,12 @@ Section Field.
         - eapply Func.valid_func_bool_iff. abstract vm_cast_no_check (eq_refl true).
           Unshelve. all: auto.
      Qed.
+   *)
 
   Derive p224_select_znz
            SuchThat (forall functions,
+                      Cmd.spec_of_add_carryx (add_carryx:=Defaults.add_carryx) functions ->
+                      Cmd.spec_of_sub_borrowx (sub_borrowx:=Defaults.sub_borrowx) functions ->
                       spec_of_selectznz
                         (field_representation:=field_representation m)
                         (p224_select_znz :: functions))
