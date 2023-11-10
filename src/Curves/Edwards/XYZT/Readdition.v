5d2a3cf5b625c0b27f2bfe44052c1721fe8f4a32
Brian McSwiggen <mcswiggen@google.com>
2023-11-10 15:35:13 -0500

    Define types and functions for cached addition

---
+++ src/Curves/Edwards/XYZT/Readdition.v
@@ -0,0 +1,102 @@
+Require Import Coq.Classes.Morphisms.
+
+Require Import Crypto.Spec.CompleteEdwardsCurve Crypto.Curves.Edwards.AffineProofs.
+Require Import Crypto.Curves.Edwards.XYZT.Basic.
+
+Require Import Crypto.Algebra.Field.
+Require Import Crypto.Algebra.Hierarchy.
+Require Import Crypto.Util.Notations Crypto.Util.GlobalSettings.
+Require Export Crypto.Util.FixCoqMistakes.
+Require Import Crypto.Util.Decidable.
+Require Import Crypto.Util.Prod.
+Require Import Crypto.Util.Tactics.BreakMatch.
+Require Import Crypto.Util.Tactics.DestructHead.
+Require Import Crypto.Util.Tactics.UniquePose.
+
+Section ExtendedCoordinates.
+  Context {F Feq Fzero Fone Fopp Fadd Fsub Fmul Finv Fdiv}
+          {field:@Algebra.Hierarchy.field F Feq Fzero Fone Fopp Fadd Fsub Fmul Finv Fdiv}
+          {char_ge_3 : @Ring.char_ge F Feq Fzero Fone Fopp Fadd Fsub Fmul (BinNat.N.succ_pos BinNat.N.two)}
+          {Feq_dec:DecidableRel Feq}.
+
+  Local Infix "=" := Feq : type_scope. Local Notation "a <> b" := (not (a = b)) : type_scope.
+  Local Notation "0" := Fzero.  Local Notation "1" := Fone. Local Notation "2" := (Fadd 1 1).
+  Local Infix "+" := Fadd. Local Infix "*" := Fmul.
+  Local Infix "-" := Fsub. Local Infix "/" := Fdiv.
+  Local Notation "x ^ 2" := (x*x).
+
+  Context {a d: F}
+          {nonzero_a : a <> 0}
+          {square_a : exists sqrt_a, sqrt_a^2 = a}
+          {nonsquare_d : forall x, x^2 <> d}.
+  Local Notation Epoint := (@E.point F Feq Fone Fadd Fmul a d).
+  Local Notation Eadd := (E.add(nonzero_a:=nonzero_a)(square_a:=square_a)(nonsquare_d:=nonsquare_d)).
+
+  Local Notation onCurve x y := (a*x^2 + y^2 = 1 + d*x^2*y^2) (only parsing).
+
+  (* Match this context to the Basic.ExtendedCoordinates context *)
+  Local Notation point := (point(F:=F)(Feq:=Feq)(Fzero:=Fzero)(Fadd:=Fadd)(Fmul:=Fmul)(a:=a)(d:=d)).
+  Local Notation coordinates := (coordinates(F:=F)(Feq:=Feq)(Fzero:=Fzero)(Fadd:=Fadd)(Fmul:=Fmul)(a:=a)(d:=d)).
+  Context {a_eq_minus1:a = Fopp 1} {twice_d} {k_eq_2d:twice_d = d+d}.
+  Local Notation m1add := (m1add(F:=F)(Feq:=Feq)(Fzero:=Fzero)(Fone:=Fone)(Fopp:=Fopp)(Fadd:=Fadd)(Fsub:=Fsub)(Fmul:=Fmul)(Finv:=Finv)(Fdiv:=Fdiv)(field:=field)(char_ge_3:=char_ge_3)(Feq_dec:=Feq_dec)(a:=a)(d:=d)(nonzero_a:=nonzero_a)(square_a:=square_a)(nonsquare_d:=nonsquare_d)(a_eq_minus1:=a_eq_minus1)(twice_d:=twice_d)(k_eq_2d:=k_eq_2d)).
+
+  (* Stolen from Basic; should be a way to reuse instead? *)
+  Ltac t_step :=
+    match goal with
+    | |- Proper _ _ => intro
+    | _ => progress intros
+    | _ => progress destruct_head' prod
+    | _ => progress destruct_head' @E.point
+    | _ => progress destruct_head' @Basic.point
+    | _ => progress destruct_head' and
+    | _ => progress cbv [eq CompleteEdwardsCurve.E.eq E.eq E.zero E.add E.opp fst snd coordinates E.coordinates proj1_sig] in *
+    | |- _ /\ _ => split | |- _ <-> _ => split
+    end.
+  Ltac t := repeat t_step; try Field.fsatz.
+
+  (* Define a new cached point type *)
+  Definition cached :=
+    { C | let '(half_YmX,half_YpX,Z,Td) := C in
+          exists (P : point),
+          let '(X1,Y1,Z1,Ta,Tb) := coordinates P in
+          half_YmX = (Y1-X1)/2 /\
+          half_YpX = (Y1+X1)/2 /\
+          Z1 = Z /\
+          Td = (Ta*Tb)*d }.
+  Definition cached_coordinates (C:cached) : F*F*F*F := proj1_sig C.
+
+  Program Definition m1_prep (P : point) : cached :=
+      match coordinates P return F*F*F*F with
+        (X, Y, Z, Ta, Tb) =>
+        let half_YmX := (Y-X)/2 in
+        let half_YpX := (Y+X)/2 in
+        let Td := Ta*Tb*d in
+        (half_YmX, half_YpX, Z, Td)
+      end.
+    Next Obligation. admit. Admitted.
+
+  Program Definition m1_readd
+      (P1 : point) (C : cached) : point :=
+    match coordinates P1, C return F*F*F*F*F with
+      (X1, Y1, Z1, Ta1, Tb1), (half_YmX, half_YpX, Z2, Td) =>
+      let A := (Y1-X1)*half_YmX in
+      let B := (Y1+X1)*half_YpX in
+      let C := (Ta1*Tb1)*Td in
+      let D := Z1*Z2 in
+      let E := B-A in
+      let F := D-C in
+      let G := D+C in
+      let H := B+A in
+      let X3 := E*F in
+      let Y3 := G*H in
+      let Z3 := F*G in
+      (X3, Y3, Z3, E, H)
+    end.
+  Next Obligation. admit. Admitted.
+
+  Lemma readd_correct P Q :
+    Basic.eq (m1_readd P (m1_prep Q)) (m1add P Q).
+  admit.
+  Admitted.
+
+End ExtendedCoordinates.
