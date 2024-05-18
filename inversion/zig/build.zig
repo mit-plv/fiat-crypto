03123b07391b8fccb1b35bf5f9407fc46746c1b1
Frank Denis <github@pureftpd.org>
2024-05-18 10:26:01 +0200

    Make build.zig files compatible with newly released zig 0.12
    Fixes #1912

--- inversion/zig/build.zig
+++ inversion/zig/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "inversion.zig" },
+        .root_source_file = b.path("inversion.zig"),
     });
     const run_main_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run tests");
