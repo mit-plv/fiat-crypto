a1cfdca6b5ca1d709d7f81c8957ff7f4df66a270
Frank Denis <124872+jedisct1@users.noreply.github.com>
2024-05-20 09:22:11 +0200

    Make build.zig files compatible with newly released zig 0.12 (#1913)
    Fixes #1912

--- fiat-zig/build.zig
+++ fiat-zig/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
     const run_main_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run library tests");
