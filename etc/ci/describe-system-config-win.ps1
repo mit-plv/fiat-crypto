1bca15ac939d085c74be7a2f40944c464955f607
Jason Gross <jgross@mit.edu>
2024-09-19 16:38:10 -0700

    Don't rely on CYGWIN_ROOT in windows config (For #1939)

--- etc/ci/describe-system-config-win.ps1
+++ etc/ci/describe-system-config-win.ps1
@@ -15,8 +15,8 @@ Print-Group -name "wmic cpu list /format:list" -command { wmic cpu list /format:
 
 # Running git configuration commands via bash
 $CYGWIN_ROOT = $env:CYGWIN_ROOT
-Print-Group -name "git config -l" -command { & "$CYGWIN_ROOT\bin\bash.exe" -l -c 'git config -l' }
-Print-Group -name "git config --global -l" -command { & "$CYGWIN_ROOT\bin\bash.exe" -l -c 'git config --global -l' }
+Print-Group -name "git config -l" -command { opam exec -- git config -l }
+Print-Group -name "git config --global -l" -command { opam exec -- git config --global -l }
 
 # Using opam and OCaml tools
 Print-Group -name "opam list" -command { opam list }
@@ -32,6 +32,6 @@ Print-Group -name "ldd --version" -command { opam exec -- ldd --version }
 
 # Using make with environmental variables
 Print-Group -name "make printenv" -command { opam exec -- make printenv }
-Print-Group -name "PATH=$PATH" -command { & "$CYGWIN_ROOT\bin\bash.exe" -l -c 'echo "${PATH}"' }
+Print-Group -name "PATH=$PATH" -command { opam exec -- bash -l -c 'echo "${PATH}"' }
 
 Print-Group -name "Writing info to '$env:GITHUB_STEP_SUMMARY'" -command { & PowerShell.exe -ExecutionPolicy Bypass -File "$SCRIPT_DIR\github-actions-record-coq-info.ps1" }
