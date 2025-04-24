bacefb683b9f8e52b3508df56759273110c08d41
Jason Gross <jgross@mit.edu>
2025-04-23 17:46:33 -0700

    Fix for tc inference having a bit of trouble

--- src/Arithmetic/WordByWordMontgomery.v
+++ src/Arithmetic/WordByWordMontgomery.v
@@ -202,13 +202,12 @@ Module WordByWordMontgomery.
     Local Lemma r_big : r > 1.
     Proof using lgr_big. clear -lgr_big; subst r. auto with zarith. Qed.
     Local Notation wprops := (@uwprops lgr lgr_big).
-    Let wprops' := wprops.
-    Let weight_0_wprops := weight_0 wprops.
-    Let weight_positive_wprops := weight_positive wprops.
-    Let weight_multiples_wprops := weight_multiples wprops.
-    Let weight_divides_wprops := weight_divides wprops.
+    Local Definition weight_0_wprops := weight_0 wprops.
+    Local Definition weight_positive_wprops := weight_positive wprops.
+    Local Definition weight_multiples_wprops := weight_multiples wprops.
+    Local Definition weight_divides_wprops := weight_divides wprops.
 
-    Local Hint Immediate wprops' : core.
+    Local Hint Extern 0 weight_properties => simple apply wprops : core.
     Local Hint Immediate weight_0_wprops : core.
     Local Hint Immediate weight_positive_wprops : core.
     Local Hint Immediate weight_multiples_wprops : core.
@@ -1054,17 +1053,16 @@ Module WordByWordMontgomery.
 
     Local Notation wprops := (@uwprops bitwidth bitwidth_big).
     Local Notation small := (@small bitwidth n).
-    Let wprops' := wprops.
-    Let weight_0_wprops := weight_0 wprops.
-    Let weight_positive_wprops := weight_positive wprops.
-    Let weight_multiples_wprops := weight_multiples wprops.
-    Let weight_divides_wprops := weight_divides wprops.
+    Local Definition weight_0_wprops' := weight_0 wprops.
+    Local Definition weight_positive_wprops' := weight_positive wprops.
+    Local Definition weight_multiples_wprops' := weight_multiples wprops.
+    Local Definition weight_divides_wprops' := weight_divides wprops.
 
-    Local Hint Immediate wprops' : core.
-    Local Hint Immediate weight_0_wprops : core.
-    Local Hint Immediate weight_positive_wprops : core.
-    Local Hint Immediate weight_multiples_wprops : core.
-    Local Hint Immediate weight_divides_wprops : core.
+    Local Hint Extern 0 weight_properties => simple apply wprops : core.
+    Local Hint Immediate weight_0_wprops' : core.
+    Local Hint Immediate weight_positive_wprops' : core.
+    Local Hint Immediate weight_multiples_wprops' : core.
+    Local Hint Immediate weight_divides_wprops' : core.
 
     Local Lemma r'_correct_alt : ((r mod m) * (r' mod m)) mod m = 1.
     Proof using r'_correct. pull_Zmod; apply r'_correct. Qed.
