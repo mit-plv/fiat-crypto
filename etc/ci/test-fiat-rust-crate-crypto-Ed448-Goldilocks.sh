9ab11efc549acf90902d3fb24b72bbf8e171c279
Jason Gross <jgross@mit.edu>
2025-07-21 11:53:48 -0700

    [CI] [Rust] Handle existence of patch.crates-io section

--- etc/ci/test-fiat-rust-crate-crypto-Ed448-Goldilocks.sh
+++ etc/ci/test-fiat-rust-crate-crypto-Ed448-Goldilocks.sh
@@ -9,11 +9,17 @@ git clone https://github.com/crate-crypto/Ed448-Goldilocks.git --branch=master E
 
 pushd Ed448-Goldilocks >/dev/null || exit $?
 
-cat >> Cargo.toml <<EOF
+if grep -q "^\[patch\.crates-io\]" Cargo.toml; then
+    # Add to existing [patch.crates-io] section
+    sed -i '/^\[patch\.crates-io\]/a fiat-crypto = { path = "../fiat-rust" }' Cargo.toml
+else
+    # Create new [patch.crates-io] section
+    cat >> Cargo.toml <<EOF
 
 [patch.crates-io]
 fiat-crypto = { path = "../fiat-rust" }
 EOF
+fi
 
 cargo test --features="fiat_u64_backend" --no-default-features || exit $?
 
--- etc/ci/test-fiat-rust-curve25519-dalek.sh
+++ etc/ci/test-fiat-rust-curve25519-dalek.sh
@@ -9,10 +9,17 @@ git clone https://github.com/dalek-cryptography/curve25519-dalek curve25519-dale
 
 pushd curve25519-dalek >/dev/null || exit $?
 
-cat >> Cargo.toml <<EOF
+if grep -q "^\[patch\.crates-io\]" Cargo.toml; then
+    # Add to existing [patch.crates-io] section
+    sed -i '/^\[patch\.crates-io\]/a fiat-crypto = { path = "../fiat-rust" }' Cargo.toml
+else
+    # Create new [patch.crates-io] section
+    cat >> Cargo.toml <<EOF
+
 [patch.crates-io]
 fiat-crypto = { path = "../fiat-rust" }
 EOF
+fi
 
 RUSTFLAGS='--cfg curve25519_dalek_backend="fiat" --cfg curve25519_dalek_bits="64"' cargo test || exit $?
 RUSTFLAGS='--cfg curve25519_dalek_backend="fiat" --cfg curve25519_dalek_bits="32"' cargo test || exit $?
