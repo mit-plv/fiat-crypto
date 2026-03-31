969337da772f17bc2e07a7b015cff2383647bbcb
Miriam Polzer <mpolzer@google.com>
2026-03-31 13:07:47 +0000

    Split file and minor cleanups

--- src/Bedrock/P256/RecodeProofs.v
+++ src/Bedrock/P256/RecodeProofs.v
@@ -1,7 +1,6 @@
 Require Import ZArith.ZArith Lia Lists.List.
 From coqutil Require Import
   Byte
-  Word.Naive
   Word.LittleEndianList
   Word.Properties
   Tactics.Tactics
@@ -18,7 +17,7 @@ From bedrock2 Require Import
   ZnWords
   BasicC64Semantics.
 
-Require Import Bedrock.P256.Specs.
+Require Import Bedrock.P256.Specs Bedrock.P256.RecodeSpecs.
 From bedrock2Examples Require Import full_sub.
 
 Import BinInt String ListNotations.
@@ -28,147 +27,10 @@ Import ProgramLogic.Coercions.
 #[local] Open Scope Z_scope.
 #[local] Open Scope list_scope.
 
-Definition ctime_ltu :=
-  func! (a, b) ~> r {
-    unpack! _d, r = br_full_sub(a, b, $0);
-    unpack! r = br_value_barrier(r)
-  }.
+#[local] Notation bytearray := (Array.array ptsto (word.of_Z 1)).
 
 (* Limb size (nonzero). *)
 #[local] Notation w := 5.
-
-Definition extract_limb_at_bit :=
-  func! (p_input, total_bits, i) ~> r {
-    idx = i >> $3; (* index = i/8 *)
-    a = load1(p_input + idx);
-    b = $0;
-    if idx + $1 < (total_bits + $7) >> $3 {
-      b = load1(p_input + idx + $1)
-    };
-    s = a + (b << $8);
-    t = s >> (i & $7); (* offset = i%8 *)
-    r = t & (($1 << $w) - $1)
-  }.
-
-Definition decompose_to_limbs :=
-  func! (p_output, p_input, total_bits) {
-    i = $0;
-    while i < total_bits {
-      unpack! r = extract_limb_at_bit(p_input, total_bits, i);
-      store1(p_output, r); p_output = p_output + $1;
-      i = i + $w;
-      $(cmd.unset "r")
-    }
-  }.
-
-Definition signed_recode_carry :=
-  func! (p_limbs, ci, n) ~> ci {
-      while n {
-        x = load1(p_limbs) + ci;
-        unpack! ci = ctime_ltu($(2^(w - 1)), x);
-        unpack! x = br_cmov(ci, x - $(2^w), x);
-        store1(p_limbs, x); p_limbs = p_limbs + $1;
-        n = n - $1;
-        $(cmd.unset "x")
-      }
-    }.
-
-Definition signed_recode :=
-  func! (p_limbs, n) {
-    unpack! _c = signed_recode_carry(p_limbs, $0, n)
-  }.
-
-Section WithBase.
-Context (B : Z).
-Definition positional := fold_right (fun a s => a + (B)*s) 0.
-
-Lemma positional_nil :
-  positional nil = 0.
-Proof. reflexivity. Qed.
-
-Lemma positional_cons h t :
-  positional (h :: t) = h + B*(positional t).
-Proof. reflexivity. Qed.
-
-Definition positional_bytes l :=
-  positional (map byte.unsigned l).
-
-Definition positional_signed_bytes l :=
-  positional (map byte.signed l).
-
-Lemma positional_bytes_cons h t :
-  positional_bytes (h :: t) = byte.unsigned h + B*(positional_bytes t).
-Proof. constructor. Qed.
-End WithBase.
-
-#[local] Notation bytearray := (Array.array ptsto (word.of_Z 1)).
-
-#[export] Instance spec_of_ctime_ltu : spec_of "ctime_ltu" :=
-  fnspec! "ctime_ltu" a b ~> r,
-  { requires t m := True;
-    ensures T M :=
-      M = m /\ T = t /\
-      word.unsigned r < 2 /\
-      r = if word.ltu a b then word.of_Z 1 else word.of_Z 0
-  }.
-
-#[export] Instance spec_of_extract_limb_at_bit : spec_of "extract_limb_at_bit" :=
-  fnspec! "extract_limb_at_bit" (p_input total_bits i : word) / input ~> r,
-    { requires t m :=
-        m =*> bytearray p_input input /\
-        8 * (length input - 1) < total_bits <= 8 * length input /\
-        le_combine input < 2^total_bits /\
-        i < total_bits /\
-        total_bits + 7 <= (word.of_Z (-1) : word);
-      ensures T M :=
-        M = m /\
-        T = t /\
-        r = le_combine input / 2^i mod 2^w :>Z
-    }.
-
-#[export] Instance spec_of_decompose_to_limbs : spec_of "decompose_to_limbs" :=
-  fnspec! "decompose_to_limbs" (p_output p_input total_bits : word) / output input R,
-    { requires t m :=
-        m =* bytearray p_output output * bytearray p_input input * R /\
-        8 * (length input - 1) < total_bits <= 8 * length input /\
-        w * (length output - 1) < total_bits <= w * length output /\
-        le_combine input < 2^total_bits /\
-        total_bits + 7 <= (word.of_Z (-1) : word);
-      ensures T M := exists OUTPUT,
-        M =* bytearray p_output OUTPUT * bytearray p_input input * R /\
-        length output = length OUTPUT /\
-        T = t /\
-        Forall (fun b => (0 <= byte.unsigned b < 2^w)) OUTPUT /\
-        le_combine input = positional_bytes (2^w) OUTPUT
-    }.
-
-#[export] Instance spec_of_signed_recode_carry : spec_of "signed_recode_carry" :=
-  fnspec! "signed_recode_carry" (p_limbs ci n : word) / limbs R ~> CO,
-    { requires t m :=
-        m =* bytearray p_limbs limbs * R /\ length limbs = word.unsigned n :>Z /\
-        Forall (fun b => (0 <= byte.unsigned b < 2^w)) limbs /\
-        0 <= ci <= 1;
-      ensures T M := exists LIMBS,
-        M =* bytearray p_limbs LIMBS * R /\ length LIMBS = word.unsigned n :>Z /\
-        T = t /\
-        positional_signed_bytes (2^w) LIMBS + 2^(w*n)*CO = word.unsigned ci + positional_bytes (2^w) limbs /\
-        Forall (fun b => (-2^w + 2 <= 2*(byte.signed b) <= 2^w)) LIMBS /\
-        0 <= CO <= 1
-    }.
-
-#[export] Instance spec_of_signed_recode : spec_of "signed_recode" :=
-  fnspec! "signed_recode" (p_limbs n : word) / limbs R,
-  { requires t m :=
-      m =* bytearray p_limbs limbs * R /\ length limbs = word.unsigned n :>Z /\
-      Forall (fun b => (0 <= byte.unsigned b < 2^w)) limbs /\
-      2 * (positional_bytes (2^w) limbs) < 2^(w*n) /\
-      -2^(w*n) <= positional (2^w) (repeat (-2^w + 2) (Z.to_nat n));
-    ensures T M := exists LIMBS,
-      M =* bytearray p_limbs LIMBS * R /\ length LIMBS = word.unsigned n :>Z /\
-      T = t /\
-      positional_signed_bytes (2^w) LIMBS = positional_bytes (2^w) limbs /\
-      Forall (fun b => (-2^w + 2 <= 2*(byte.signed b) <= 2^w)) LIMBS
-  }.
 
 Lemma ctime_ltu_ok : program_logic_goal_for_function! ctime_ltu.
 Proof.
@@ -185,12 +47,12 @@ Qed.
 Lemma bytearray_load_of_sep addr (addr' : word) n (values : list byte) R m
   (Hsep : (sep (bytearray addr values) R m))
   (Haddr : addr' = (word.add addr (word.of_Z (Z.of_nat n))))
-  (Hlength : (n < length values)%nat) :
+  (Hlength : (n < length values)) :
   Memory.load access_size.one m addr' =
   Some (word.of_Z (byte.unsigned (nth_default Byte.x00 values n))).
 Proof.
   rewrite nth_default_eq.
-  rewrite <-(firstn_nth_skipn _ _ values Byte.x00 Hlength) in Hsep.
+  rewrite <-(firstn_nth_skipn _ n values Byte.x00) in Hsep by lia.
   do 2 seprewrite_in @bytearray_append Hsep.
   seprewrite_in @array_cons Hsep.
   seprewrite_in @array_nil Hsep.
@@ -202,7 +64,7 @@ Qed.
 Lemma bytearray_load_of_sep' (addr addr': word) (values : list byte) R m :
   (sep (bytearray addr values) R m) ->
   let offset := word.unsigned (word.sub addr' addr) in
-    (let n := Z.to_nat offset in (n < length values)%nat ->
+    (let n := Z.to_nat offset in (n < length values) ->
     Memory.load access_size.one m addr' =
     Some (word.of_Z (byte.unsigned (nth_default Byte.x00 values n)))).
 Proof.
@@ -240,12 +102,11 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma bytelist_extract_two num i b1 b2 w:
+Lemma bytelist_extract_two num i b1 b2:
   let idx := i / 8  in
   let offset := i mod 8 in
   b1 = (nth_default Byte.x00 num (Z.to_nat idx)) ->
   b2 = (nth_default Byte.x00 num (S (Z.to_nat (idx)))) ->
-  0 <= w <= 8 ->
   0 <= i < length num * 8 ->
   (Z.of_bytes num / 2 ^ i) mod 2 ^ w =
       ((byte.unsigned b1 + byte.unsigned  b2 * 2 ^ 8) / 2 ^ offset) mod 2 ^ w.
@@ -323,7 +184,7 @@ Proof.
 
     rewrite extract_limb_at_bit_zify by apply byte.unsigned_range.
 
-    erewrite bytelist_extract_two; [| reflexivity | reflexivity | lia | ZnWords ].
+    erewrite bytelist_extract_two; [| reflexivity | reflexivity | ZnWords ].
 
     rewrite <-word.add_assoc, !word.word_sub_add_l_same_l.
     subst idx.
@@ -336,7 +197,7 @@ Proof.
 
   rewrite extract_limb_at_bit_zify by (try apply byte.unsigned_range; lia).
 
-  erewrite bytelist_extract_two; [| reflexivity | reflexivity | lia | ZnWords].
+  erewrite bytelist_extract_two; [| reflexivity | reflexivity | ZnWords].
 
   subst idx.
   repeat f_equal. { ZnWords. }
@@ -548,7 +409,7 @@ Proof.
   ssplit; try ecancel_assumption; trivial.
 Qed.
 
-Lemma positional_bounded (l : list Z) L U :
+Lemma positional_bound (l : list Z) L U :
   let n := length l in
   Forall (fun b => (L <= 2*b <= U)) l ->
   positional (2^w) (List.repeat L n) <= 2 * (positional (2^w) l) <= positional (2^w) (List.repeat U n).
@@ -576,7 +437,7 @@ Proof.
   assert (word.unsigned x <> 1).
   { intros Hx.
     rewrite word.unsigned_of_Z_0, Z.add_0_l, Hx, Z.mul_1_r in *.
-    epose proof positional_bounded (map byte.signed x0) (- 2 ^ w + 2) (2 ^ w) ltac:(apply Forall_map; assumption).
+    epose proof positional_bound (map byte.signed x0) (- 2 ^ w + 2) (2 ^ w) ltac:(apply Forall_map; assumption).
     rewrite length_map in *.
     progress fold (positional_signed_bytes (2 ^ w) x0) in *.
     assert (2*positional_signed_bytes (2 ^ w) x0 < -2^(w*n)) by lia.
