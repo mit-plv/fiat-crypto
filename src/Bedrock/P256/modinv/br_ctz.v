bc96d03cd238b113af1e1db8b8a0e7f249d84c58
Klinashka <klinashka@google.com>
2026-07-28 16:16:22 +0000

    Completed proof for br_ctz.

--- src/Bedrock/P256/modinv/br_ctz.v
+++ src/Bedrock/P256/modinv/br_ctz.v
@@ -88,6 +88,169 @@ Section FunctionalCtz.
             eapply Z_div_exact_2; try eapply lctz_pos_pow2; eauto.
             eapply lctz_pos_mod; eauto. }
     Qed.
+
+    Lemma pos_range (p : positive) (n : nat) : (Z.pos p) < 2^n -> (0 < n).
+    Proof.
+        intros H. induction n; cbn [Z.pow] in *; lia.
+    Qed.
+
+    Lemma pos_testbit_z_testbit (p : positive)  (n : N) : 
+        Pos.testbit p n = Z.testbit (Z.pos p) (Z.of_N n).
+    Proof.
+        destruct p, n; eauto.
+    Qed.
+
+    Lemma lctz_pos_testbit_lt (def : Z) (z : Z) : z > 0 -> 
+        forall i , i < lctz def z -> Z.testbit z i = false.
+    Proof.
+        intros Hz.
+        destruct z as [ | p | p]; inversion Hz.
+        induction p; intros i Hi; destruct i as [ | pi | pi]; 
+        inversion Hi; cbn [Z.testbit] in *; eauto.
+        cbn [Pos.testbit].
+        replace (Pos.testbit p (Pos.pred_N pi)) with (Z.testbit (Z.pos p) (Z.of_N (Pos.pred_N pi)))
+            by (destruct pi, p; eauto).
+        eapply IHp; try lia.
+        cbn [lctz pos_ctz] in *.
+        destruct pi; lia.
+    Qed.
+
+    Lemma testbit_xO_1 (p : positive) (n : nat) : 
+        Z.testbit (Z.pos p~0) (S n) = Z.testbit (Z.pos p) n.
+    Proof.
+        rewrite Nat2Z.inj_succ, Pos2Z.pos_xO, Z.double_bits, Z.pred_succ.
+        eauto.
+    Qed.
+
+    Lemma testbit_xO_2 (p : positive) (z : Z) : 
+        Z.testbit (Z.pos (p~0)) z = Z.testbit (Z.pos p) (Z.pred z).
+        rewrite Pos2Z.pos_xO, Z.double_bits. eauto.
+    Qed.
+
+    Lemma lctz_pos_testbit_eq (def : Z) (z : Z) : z > 0 -> 
+        Z.testbit z (lctz def z) = true.
+    Proof.
+        intros Hz; destruct z; inversion Hz.
+        induction p as [p IHp | p IHp | ]; cbn [lctz pos_ctz] in *; 
+        eauto. rewrite testbit_xO_1. eapply IHp; lia.
+    Qed.
+
+
+    Lemma lctz_pos_testbit_2 (def : Z) (z : Z) (c : Z) : z > 0 -> 
+        Z.testbit z c = true /\ (forall i , i < c -> Z.testbit z i = false) ->
+            c = lctz def z
+        .
+    Proof.
+        intros Hz. destruct z as [ | p | p]; inversion Hz.
+        revert c. induction p; intros c [H1 H2]; cbn [lctz pos_ctz].
+        { 
+            destruct (Ztrichotomy c 0%nat) as [H | [H | H]];
+            destruct c; inversion H; inversion H1; eauto.
+            assert (Heq : 0%nat < Z.pos p0) by lia.
+            specialize (H2 0%nat Heq). cbv [Z.testbit Z.of_nat Z.odd] in H2.
+            inversion H2.
+        }
+        { 
+            destruct (Ztrichotomy c 0%nat) as [H | [H | H]]; 
+            destruct c; inversion H; inversion H1; eauto.
+            cbn [lctz] in IHp. 
+            rewrite Nat2Z.inj_succ.
+            rewrite <- IHp with (c := Z.pred (Z.pos p0)); try lia.
+            rewrite testbit_xO_2 in H1; split; eauto.
+            intros i Hi.
+            specialize (H2 (Z.succ i)). rewrite testbit_xO_2, Z.pred_succ in H2.
+            eapply H2; lia.
+        }
+        {
+            destruct (Ztrichotomy c 0%nat) as [H | [H | H]]; 
+            destruct c; inversion H; inversion H1; eauto.
+        }
+    Qed.
+    
+    Lemma lctz_pos_lt (def : Z) (z : Z) (n : nat) : 
+        0 < z < 2^n -> 0 <= lctz def z < n.
+    Proof.
+        intros [Hzlt Hzgt]; split; destruct z; inversion Hzlt.
+        - cbv [lctz]; lia.
+        - generalize dependent n.
+          induction p; intros; cbv [lctz] in *; cbn [pos_ctz];
+          destruct n; try lia.
+          rewrite !Nat2Z.inj_succ in *.
+          eapply Zsucc_lt_compat, IHp; try lia.
+          rewrite Pos2Z.pos_xO in Hzgt.
+          replace (Z.succ n) with (1 + n) in Hzgt by lia.
+          rewrite Z.pow_add_r in Hzgt by lia.
+          lia.
+    Qed.
+
+    Lemma lctz_pos_modpow2 (def : Z) (z : Z) (n : nat) : z > 0 -> z mod 2^n <> 0 -> 
+        lctz def (z mod 2^n) = lctz def z.
+    Proof.
+        intros Hz Hzmod.
+        apply lctz_pos_testbit_2; eauto.
+        split.
+        - destruct z; inversion Hz.
+          rewrite <-Z.mod_pow2_bits_low with (n := n).
+          { 
+            eapply lctz_pos_testbit_eq.
+            assert (0 <= Z.pos p mod 2^n) by (eapply Z_mod_lt; lia). lia.
+          }
+          {
+            eapply lctz_pos_lt.
+            assert (0 <= Z.pos p mod 2^n) by (eapply Z_mod_lt; lia); split; try lia.
+            eapply Z_mod_lt; lia.
+          }
+        - intros i H. 
+          assert (i < n).
+          { 
+            eapply (Z.lt_trans _ _ _ H).
+            eapply lctz_pos_lt.
+            assert (0 <= z mod 2^n) by (eapply Z_mod_lt; lia); split; try lia.
+            eapply Z_mod_lt; lia.
+          }
+          rewrite <-Z.mod_pow2_bits_low with (n := n); eauto.
+          eapply lctz_pos_testbit_lt; eauto.
+          assert (0 <= z mod 2^n).
+          { eapply Z_mod_lt; lia. }
+          lia.
+    Qed.
+
+    Lemma lctz_word_64 (w : word) : (w > 0) -> lctz 64 ((w * 2) mod 2^64) = 1 + lctz 64 w.
+    Proof.
+        intros H.
+        ZnWords_pre.
+        remember ((w0 * 2) mod 2^64) as a.
+        destruct (Z.eq_dec a 0).
+        { 
+            subst. rewrite e.
+            apply Z_div_exact_full_2 in e; try lia.
+            assert (w0 * 2 / 2^64 <= 1).
+            {
+                apply Zlt_succ_le.
+                replace (2^64) with (2^63 * 2 ^1) by (rewrite <- Z.pow_add_r; lia).
+                replace (2^1) with 2 by lia.
+                rewrite Zdiv_mult_cancel_r by lia.
+                eapply Z.div_lt_upper_bound; lia.
+            }
+            assert (0 < w0 * 2 / 2^64).
+            {
+                eapply Z.div_str_pos; split; lia.
+            }
+            assert (w0 * 2 / 2^64 = 1) by lia.
+            rewrite H4 in e.
+            assert (w0 = 2^63%nat) by lia.
+            rewrite H5. eauto.
+        }
+        { subst. replace 64 with (Z.of_nat 64%nat) by lia.
+          assert (w0 > 0).
+          { destruct (Z.eq_dec w0 0).
+            { subst. rewrite Z.mul_0_l, Z.mod_0_l in n; lia. } 
+            { lia. }}
+            rewrite lctz_pos_modpow2 by lia.
+            rewrite Z.mul_comm, lctz_pos_double; lia.
+        }
+    Qed.
+
 End FunctionalCtz.
 
 
@@ -97,7 +260,7 @@ End FunctionalCtz.
 #[export] Instance spec_of_br_ctz : spec_of "br_ctz" := 
     fnspec! "br_ctz" (value : word) ~> count,
     {
-        requires t m := value <> word.of_Z 0;
+        requires t m := value > 0;
         ensures T M := T = t /\ M = m /\ count = word.of_Z (lctz 64 value)
     }.
 
@@ -110,7 +273,7 @@ Definition br_ctz := func! (value) ~> count {
         count = count + $1;
         tmp = tmp << $1
     };
-    count = count - $ 64
+    count = $64 - count
 }.
 
 (** * Specification Proof *)
@@ -119,14 +282,64 @@ Proof.
     repeat straightline.
     refine ((Loops.tailrec
     (* types of ghost variables*) 
-                                  
-    (HList.polymorphic_list.nil)
+        (HList.polymorphic_list.nil)
     (* program variables *) (["value";"count";"tmp"] : list String.string))
     (fun v t m value_ count tmp => PrimitivePair.pair.mk (* precondition *)
-      (lctz 64 tmp = lctz 64 value + count)
-    (fun            T M VALUE COUNT TMP => (* postcondition *)
-      lctz 64 TMP = lctz 64 value))
-    (fun n m => m < n <= total_bits + w) (* well_founded relation *)
-    _ _ _ _ _ ); 
-    Loops.loop_simpl.
+      (v = word.unsigned count /\ word.unsigned tmp = (word.unsigned value * 2^count) mod 2^64 /\ lctz 64 (word.unsigned tmp) = lctz 64 (word.unsigned value) + (word.unsigned count) /\ value = value_)
+    (fun T M VALUE COUNT TMP => (* postcondition *)
+      T = t /\ M = m /\ 64 = lctz 64 (word.unsigned value) + word.unsigned COUNT))
+    (fun n m => m < n <= 64) (* well_founded relation *)
+    _ _ _ _ _ ); Loops.loop_simpl.
 
+    { repeat straightline. }
+    { eapply Z.gt_wf. }
+    { 
+        repeat straightline; cbv [count]; ssplit.
+        all: intuition try ZnWords.
+        rewrite Properties.word.unsigned_of_Z_0, Z.pow_0_r. ZnWords.
+    }
+    { 
+        repeat straightline; try eexists _;
+        repeat straightline; ssplit; try split;
+        repeat straightline;
+        try (erewrite H0 in H2); eauto.
+        { 
+            ZnWords_pre.
+            assert (Hw2 : w2 < 64).
+            {
+                replace 64 with (Z.of_nat 64%nat) in * by lia.
+                assert (0 <= lctz 64%nat w0 < 64%nat) by (eapply lctz_pos_lt; lia).
+                assert (0 <= lctz 64%nat w1 < 64%nat) by (eapply lctz_pos_lt; lia).
+                lia.
+            }
+            rewrite H1. rewrite Z.mul_mod_idemp_l by ZnWords.
+            rewrite !(Z.mod_small (w2 + 1)) by ZnWords.
+            rewrite Z.pow_add_r by ZnWords.
+            ZnWords.
+        }
+        {
+            assert (lctz 64 ((x1 * 2 ^ 1) mod 2^64) = 1 + lctz 64 x1).
+            { eapply lctz_word_64; ZnWords. }
+            ZnWords_pre. 
+            assert (Hw2 : w2 < 64).
+            {
+                replace 64 with (Z.of_nat 64%nat) in * by lia.
+                assert (0 <= lctz 64%nat w0 < 64%nat) by (eapply lctz_pos_lt; lia).
+                assert (0 <= lctz 64%nat w1 < 64%nat) by (eapply lctz_pos_lt; lia).
+                lia.
+            }
+            ZnWords.
+        }
+        { ZnWords_pre. 
+            assert (Hw2 : w2 < 64).
+            {
+                replace 64 with (Z.of_nat 64%nat) in * by lia.
+                assert (0 <= lctz 64%nat w0 < 64%nat) by (eapply lctz_pos_lt; lia).
+                assert (0 <= lctz 64%nat w1 < 64%nat) by (eapply lctz_pos_lt; lia).
+                lia.
+            }
+            ZnWords.
+        }
+    }
+    { repeat straightline. ZnWords. }
+Qed.
