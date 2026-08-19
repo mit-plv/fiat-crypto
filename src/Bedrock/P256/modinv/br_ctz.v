198b31ba114fac4e6139de49b362b06eea8f44a0
Klinashka <klinashka@google.com>
2026-08-19 12:22:25 +0000

    Solved review comments.

--- src/Bedrock/P256/modinv/br_ctz.v
+++ src/Bedrock/P256/modinv/br_ctz.v
@@ -12,32 +12,35 @@ From Coq Require Import ZArith Lia.
 
 Lemma lctz_word_slu (w : word) : (w > 0) -> lctz 64 (word.slu w (word.of_Z 1)) = 1 + lctz 64 w.
 Proof.
-    intros H. ZnWords_pre.
-    replace (2^1) with 2 by lia.
-    remember ((w0 * 2) mod 2^64) as a.
-    destruct (Z.eq_dec a 0) as [e | ne]; subst.
-    { 
-        rewrite e. apply Z_div_exact_full_2 in e; try lia.
-        assert (w0 * 2 / 2^64 <= 1).
-        {
-            apply Zlt_succ_le.
-            replace (2^64) with (2^63 * 2 ^1) by (rewrite <- Z.pow_add_r; lia).
-            replace (2^1) with 2 by lia.
-            rewrite Zdiv_mult_cancel_r by lia.
-            eapply Z.div_lt_upper_bound; lia.
-        }
-        assert (0 < w0 * 2 / 2^64) by (eapply Z.div_str_pos; split; lia).
-        replace (w0 * 2 / 2^64) with 1 in e by lia.
-        replace w0 with (2^63%nat) by lia.
-        eauto.
+    intros H.
+    rewrite Properties.word.unsigned_slu_shamtZ by lia.
+    cbv [word.wrap].
+    rewrite Z.shiftl_mul_pow2, Z.pow_1_r by lia.
+    match goal with |- context [lctz _ ?x] => destr (x =? 0) end.
+    { pose proof (Properties.word.unsigned_range w).
+        apply Z.mod_divide in E; [|lia].
+        destruct E.
+        replace (word.unsigned w) with (2^63) by lia.
+        reflexivity. }
+    { rewrite lctz_eq_mod_pow2 with (n := 64%nat) by lia.
+        rewrite Z.mul_comm, lctz_double by lia.
+        reflexivity. }
+Qed.
+
+Lemma word_lctz_range (w : word) : 0 <= lctz 64 w <= 64%nat.
+Proof.
+    destruct (word.eqb w (word.of_Z 0)) eqn: Heq.
+    {
+        eapply Properties.word.eqb_true in Heq. rewrite Heq in *; 
+        simpl; lia.
     }
-    { 
-        subst. replace 64 with (Z.of_nat 64%nat) by lia.
-        assert (w0 > 0) by (destruct (Z.eq_dec w0 0); lia).
-        rewrite lctz_pos_modpow2 by lia.
-        rewrite Z.mul_comm, lctz_pos_double; lia.
+    {
+        eapply Properties.word.eqb_false in Heq. 
+        pose proof (lctz_range 64 w 64).
+        ZnWords.
     }
 Qed.
+
 
 
 (** * Specification *)
@@ -45,8 +48,8 @@ Qed.
 #[export] Instance spec_of_br_ctz : spec_of "br_ctz" := 
     fnspec! "br_ctz" (value : word) ~> count,
     {
-        requires t m := value > 0;
-        ensures T M := T = t /\ M = m /\ count = word.of_Z (lctz 64 value)
+        requires t m := True;
+        ensures T M := T = t /\ M = m /\ word.unsigned count = (lctz 64 value)
     }.
 
 (** * Implementation *)
@@ -70,7 +73,9 @@ Proof.
         (HList.polymorphic_list.nil)
     (* program variables *) (["value";"count";"tmp"] : list String.string))
     (fun v t m value_ count tmp => PrimitivePair.pair.mk (* precondition *)
-      (v = word.unsigned count /\ word.unsigned tmp = (word.unsigned value * 2^count) mod 2^64 /\ lctz 64 (word.unsigned tmp) = lctz 64 (word.unsigned value) + (word.unsigned count) /\ value = value_)
+      (
+        v = word.unsigned count /\ word.unsigned tmp = (word.unsigned value * 2^count) mod 2^64 /\ 
+      lctz 64 (word.unsigned tmp) = lctz 64 (word.unsigned value) + (word.unsigned count) /\ value = value_)
     (fun T M VALUE COUNT TMP => (* postcondition *)
       T = t /\ M = m /\ 64 = lctz 64 (word.unsigned value) + word.unsigned COUNT))
     (fun n m => m < n <= 64) (* well_founded relation *)
@@ -91,8 +96,8 @@ Proof.
             | [H1 : ?x = 0 , H2 : lctz 64 ?x = _ |- _ ] => erewrite H1 in H2
             end; eauto; try ZnWords.
         all: assert (Htmp : lctz 64 tmp = lctz 64 x1 + 1) by (cbv [tmp]; rewrite lctz_word_slu; ZnWords).
-        all: assert (0 <= lctz 64 x1 < 64%nat) by (eapply lctz_pos_lt; ZnWords).
-        all: assert (0 <= lctz 64 x < 64%nat) by (eapply lctz_pos_lt; ZnWords).
+        all: assert (0 <= lctz 64 x1 < 64%nat) by (eapply lctz_range; ZnWords).
+        all: assert (0 <= lctz 64 x < 64%nat) by (eapply lctz_range; ZnWords).
         all: try ZnWords.
         { 
             ZnWords_pre.
@@ -104,5 +109,5 @@ Proof.
             all: idtac + rewrite ?Z.pow_add_r; ZnWords.
         }
     }
-    { repeat straightline. ZnWords. }
+    { repeat straightline. pose proof (word_lctz_range value). ZnWords. }
 Qed.
